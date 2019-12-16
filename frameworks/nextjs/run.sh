60ba4c8da5bec8211d158f13ee0a7297b367993d
Luca Steeb <contact@luca-steeb.com>
2019-12-16 16:17:52 +0100

    feat(framework/nextjs): add nextjs

---
+++ frameworks/nextjs/run.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+set -eux
+
+rm -rf prisma/migrations/
+rm -rf prisma/dev.db
+
+yarn install
+yarn prisma2 lift save --create-db --name init
+yarn prisma2 lift up
+yarn prisma2 generate
+
+yarn build
+yarn start &
+pid=$!
+
+sleep 5
+
+curl -v localhost:3000
+
+kill $pid
+
+yarn prisma2 lift down
