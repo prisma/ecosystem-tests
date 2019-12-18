bd683bc2e343cdae30f337072e52ceb5693cf1a0
Luca Steeb <contact@luca-steeb.com>
2019-12-18 16:55:13 +0100

    ci(github): search for run.sh instead of package.json

--- all.sh
+++ all.sh
@@ -6,7 +6,7 @@ dir=$1
 
 echo "running $dir tests"
 
-packages=$(find "$dir" -not -path "*/node_modules/*" -type f -name "package.json")
+packages=$(find "$dir" -not -path "*/node_modules/*" -type f -name "run.sh")
 
 echo "$packages" | tr ' ' '\n' | while read -r item; do
 	echo "running $item"
