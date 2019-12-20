c6ada712fba2c6d4aa20cf3a70695e0a739f37a2
Luca Steeb <contact@luca-steeb.com>
2019-12-20 16:00:26 +0100

    feat(upgrade): run each minute in each cron execution

--- upgrade.sh
+++ upgrade.sh
@@ -2,43 +2,56 @@
 
 set -eu
 
-echo "checking info..."
+# since GH actions are limited to 5 minute cron jobs, just run this for 5 minutes
 
-channel="alpha"
-v=$(yarn info prisma2@$channel --json | jq '.data["dist-tags"].alpha' | tr -d '"')
+# check each 30 seconds, so 10 times per 5 minutes
+j=0
+while [ $j -le 10 ]; do
+	echo "checking info..."
 
-packages=$(find . -not -path "*/node_modules/*" -type f -name "package.json")
+	channel="alpha"
+	v=$(yarn info prisma2@$channel --json | jq '.data["dist-tags"].alpha' | tr -d '"')
 
-dir=$(pwd)
+	packages=$(find . -not -path "*/node_modules/*" -type f -name "package.json")
 
-echo "$packages" | tr ' ' '\n' | while read -r item; do
-	echo "checking $item"
-	cd "$(dirname "$item")/"
-	yarn add "prisma2@$v" --dev
-	yarn add "@prisma/photon@$v"
-	cd "$dir"
+	dir=$(pwd)
+
+	echo "$packages" | tr ' ' '\n' | while read -r item; do
+		echo "checking $item"
+		cd "$(dirname "$item")/"
+		yarn add "prisma2@$v" --dev
+		yarn add "@prisma/photon@$v"
+		cd "$dir"
+	done
+
+	if [ -z "$(echo)" ]; then
+		echo "no changes"
+		# checking takes around ~5s
+		sleep 25
+
+		j=$(( j + 1 ))
+		continue
+	fi
+
+	echo "changes, upgrading..."
+
+	mkdir -p ~/.ssh
+	echo "$SSH_KEY" > ~/.ssh/id_rsa
+	chmod 600 ~/.ssh/id_rsa
+	ssh-keyscan github.com >> ~/.ssh/known_hosts
+
+	git config --global user.email "prismabots@gmail.com"
+	git config --global user.name "Prismo"
+
+	git remote add github "git@github.com:$GITHUB_REPOSITORY.git"
+
+	git commit -am "chore(packages): bump prisma2 to $v"
+
+	git push github HEAD:"${GITHUB_REF}"
+
+	echo "pushed commit"
+
+	j=$(( j + 1 ))
 done
-
-if [ -z "$(git status -s)" ]; then
-	echo "no changes"
-	exit 0
-fi
-
-echo "changes, upgrading..."
-
-mkdir -p ~/.ssh
-echo "$SSH_KEY" > ~/.ssh/id_rsa
-chmod 600 ~/.ssh/id_rsa
-ssh-keyscan github.com >> ~/.ssh/known_hosts
-
-git config --global user.email "prismabots@gmail.com"
-git config --global user.name "Prismo"
-
-git remote add github "git@github.com:$GITHUB_REPOSITORY.git"
-git pull github "${GITHUB_REF}" --ff-only
-
-git commit -am "chore(packages): bump prisma2 to $v"
-
-git push github HEAD:"${GITHUB_REF}"
 
 echo "done"
