7be0bda54ba08b3095fc917cdf110460efe45312
Prismo <prismabots@gmail.com>
2020-01-13 18:46:19 +0100

    config port

--- platforms/heroku/index.js
+++ platforms/heroku/index.js
@@ -7,7 +7,7 @@ const { Photon } = require('./prisma/photonjs')
 const photon = new Photon()
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.get('/', async (req, res) => {
   const createUser = await photon.users.create({
