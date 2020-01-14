56a30c6475575625b2395a65a8dc8a7c8483aa6e
Luca Steeb <contact@luca-steeb.com>
2020-01-14 23:16:28 +0100

    fix(lambda): provide postgres env variable

--- platforms/lambda/update-code.sh
+++ platforms/lambda/update-code.sh
@@ -4,5 +4,7 @@ set -eux
 
 sh zip.sh
 
+aws lambda update-function-configuration --function-name prisma2-e2e-tests --environment "Variables={POSTGRESQL_URL=$POSTGRESQL_URL}" --timeout 10
+
 aws lambda update-function-code --function-name prisma2-e2e-tests --zip-file "fileb://lambda.zip"
 yarn install --offline
