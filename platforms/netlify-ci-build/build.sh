6b412e78dbc1c20e57d2610e9502b606d36e1c81
Divyendu Singh <singh@prisma.io>
2020-01-21 18:53:02 +0100

    simplify part 1

--- platforms/netlify-ci-build/build.sh
+++ platforms/netlify-ci-build/build.sh
@@ -2,14 +2,9 @@
 
 set -e
 
-# rm -rf functions-build
-# mkdir -p functions-build
 cp -R ./prisma ./functions/prisma
-# cd functions/
-# zip -r index.zip *
-# mv index.zip ../functions-build/index.zip
-# cd ..
-yarn netlify deploy --dir=. --prod --functions=functions
+# yarn netlify build
+yarn netlify deploy --prod
 rm -rf functions/prisma
 
 sh test.sh
--- platforms/netlify-ci-build/run.sh
+++ platforms/netlify-ci-build/run.sh
@@ -5,5 +5,17 @@ set -eu
 yarn install
 yarn prisma2 generate
 
-# sh build.sh
-# sh test.sh // To be run after build.sh in the netlify CI
+# sh build.sh // Should run after the commit in netlify CI
+# sh test.sh // To be run after build.sh (via build.sh) in the netlify CI
+
+# git config --global user.email "prismabots@gmail.com"
+# git config --global user.name "Prismo"
+git config --global user.email "singh@prisma.io"
+git config --global user.name "Divyendu Singh"
+
+git init
+git remote add origin "https://ignored_user:`heroku auth:token`@git.heroku.com/e2e-platforms-heroku.git" || git remote set-url heroku "https://ignored_user:`heroku auth:token`@git.heroku.com/e2e-platforms-heroku.git" || true
+git add .
+git commit -m "push to heroku"
+git push heroku master --force
+rm -rf .git
