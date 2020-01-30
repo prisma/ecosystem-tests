154a7b858564bc2f502e265858ad260b6c46a23a
Luca Steeb <contact@luca-steeb.com>
2020-01-30 11:58:28 +0700

    feat(gcp-functions): stream logs

--- platforms/gcp-functions/run.sh
+++ platforms/gcp-functions/run.sh
@@ -17,6 +17,35 @@ yarn prisma2 generate
 
 yarn tsc
 
+set +e
+echo "function deploy start"
 gcloud functions deploy prisma-e2e --runtime nodejs10 --trigger-http --entry-point=handler --allow-unauthenticated --source . --verbosity debug --set-env-vars GCP_FUNCTIONS_PG_URL=$GCP_FUNCTIONS_PG_URL
+code=$?
+echo "function deploy end"
+set -e
 
+if [ "$code" != "0" ]; then
+	# wait for logs
+	sleep 30
+
+	echo "function logs start"
+	gcloud functions logs read prisma-e2e
+	echo "function logs end"
+
+	echo "function deploy failed, see logs above"
+	exit $code
+fi
+
+set +e
 sh test.sh
+code=$?
+set -e
+
+# wait for logs
+sleep 30
+
+echo "function logs start"
+gcloud functions logs read prisma-e2e
+echo "function logs end"
+
+exit $code
