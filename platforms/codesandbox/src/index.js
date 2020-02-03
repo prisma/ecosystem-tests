4c7b1df57499b653a659d02788d8a825b843a933
Divyendu Singh <singh@prisma.io>
2020-02-03 16:00:22 +0100

    force new csb

--- platforms/codesandbox/src/index.js
+++ platforms/codesandbox/src/index.js
@@ -46,4 +46,4 @@ app.get('/', async (req, res) => {
 })
 
 const port = process.env.PORT || 3000
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Listening on port ${port}!`))
