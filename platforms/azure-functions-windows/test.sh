3fd8fe65331d550db846f4f7d4553c70d882379c
Luca Steeb <contact@luca-steeb.com>
2020-02-05 14:20:08 +0700

    fix url

--- platforms/azure-functions-windows/test.sh
+++ platforms/azure-functions-windows/test.sh
@@ -2,7 +2,7 @@
 
 set -eux
 
-url="https://prisma-e2e-windows-test.azurewebsites.net/api/prisma-e2e-windows-test"
+url="https://prisma-e2e-windows-test.azurewebsites.net/api/prisma-e2e-windows-test?code=FVunlJhxUGTyrBG5Z3ji0Ak5OnobJDIBVXqN10Fvm8oMjSX5DBR0NA=="
 
 expected='{"createUser":{"id":"12345","email":"alice@prisma.io","name":"Alice"},"updateUser":{"id":"12345","email":"bob@prisma.io","name":"Bob"},"users":{"id":"12345","email":"bob@prisma.io","name":"Bob"},"deleteManyUsers":{"count":1}}'
 actual=$(curl -v "$url")
