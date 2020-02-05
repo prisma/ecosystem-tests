471904c0ab303e0a54d800eab9988247379296db
Luca Steeb <contact@luca-steeb.com>
2020-02-05 11:41:52 +0700

    adapt endpoint

--- platforms/azure-functions-linux/test.sh
+++ platforms/azure-functions-linux/test.sh
@@ -2,7 +2,7 @@
 
 set -eux
 
-url="https://prisma-e2e-linux-test.azurewebsites.net/api/prisma-e2e-linux-test"
+url="https://prisma-e2e-linux-test.azurewebsites.net/api/prisma-e2e-linux-test?code=2apwPxNqxtD1MnsnsQM0BeXAa8ANLI2egehrNnWIa39p6O5k6WpRJw=="
 
 expected='{"createUser":{"id":"12345","email":"alice@prisma.io","name":"Alice"},"updateUser":{"id":"12345","email":"bob@prisma.io","name":"Bob"},"users":{"id":"12345","email":"bob@prisma.io","name":"Bob"},"deleteManyUsers":{"count":1}}'
 actual=$(curl -v "$url")
