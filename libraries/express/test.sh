61f5c945039f9c246969fbfeb1388e75905a2ec4
Luca Steeb <contact@luca-steeb.com>
2020-02-11 15:23:44 +0700

    feat(libraries): add express

---
+++ libraries/express/test.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+set -eu
+
+yarn start &
+pid=$!
+
+sleep 10
+
+curl "http://localhost:3000/ensure-user"
+
+expected='[{"id":1,"email":"john@example.com","name":"John Doe"}]'
+actual=$(curl -v "http://localhost:3000/user")
+
+if [ "$expected" != "$actual" ]; then
+	echo "expected '$expected', got '$actual'"
+	kill $pid
+	exit 1
+fi
+
+echo "result: $actual"
+
+kill $pid
