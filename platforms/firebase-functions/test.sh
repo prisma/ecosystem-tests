1f9703e4a6f4cde20ad5becde053c9c4e8529a52
Luca Steeb <contact@luca-steeb.com>
2020-02-13 14:45:08 +0700

    fix(firebase-functions): create new function on each deploy (#88)

--- platforms/firebase-functions/test.sh
+++ platforms/firebase-functions/test.sh
@@ -2,6 +2,8 @@
 
 set -eux
 
+func="$1"
+
 url="https://us-central1-prisma-e2e-tests-265911.cloudfunctions.net/func"
 
 expected='{"createUser":{"id":"12345","email":"alice@prisma.io","name":"Alice"},"updateUser":{"id":"12345","email":"bob@prisma.io","name":"Bob"},"users":{"id":"12345","email":"bob@prisma.io","name":"Bob"},"deleteManyUsers":{"count":1}}'
@@ -9,7 +11,10 @@ actual=$(curl -v "$url")
 
 if [ "$expected" != "$actual" ]; then
 	echo "expected '$expected', got '$actual'"
+	sh cleanup.sh "$func"
 	exit 1
 fi
 
 echo "result: $actual"
+
+sh cleanup.sh "$func"
