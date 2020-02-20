096c1d8ace12f79cadf76b48f75f197080b85115
Divyendu Singh <singh@prisma.io>
2020-02-20 13:19:55 +0100

    correct load command

--- databases/docker-pgbouncer/run.sh
+++ databases/docker-pgbouncer/run.sh
@@ -1,6 +1,6 @@
 yarn
 docker-compose up -d
 sleep 30
-docker container exec -i $(docker-compose ps -q postgres) psql < data.sql
+docker container exec -i $(docker-compose ps -q postgres) psql -U postgres < data.sql
 yarn prisma2 generate
 yarn test
