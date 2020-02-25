af9e4b720b31f421309458d9c97f959375394f05
Luca Steeb <contact@luca-steeb.com>
2020-02-25 21:50:48 +0700

    fix(platforms): cleanup (#149)
    * fix(platforms): cleanup
    
    * fix(azure-linux): deploy new function with prisma azure account

--- platforms/azure-functions-linux/test.sh
+++ platforms/azure-functions-linux/test.sh
@@ -2,7 +2,7 @@
 
 set -eux
 
-url="https://prisma-e2e-linux-test-azure-functions-is-so-amazing.azurewebsites.net/api/prisma-e2e-linux-test?code=CPVQPpNiafsZa7alNwcjpo5pPqMyYqaG/la9m5LLhzP8w37N21v85g=="
+url="https://prisma-e2e-linux-test-azure-functions-is-so-amazing.azurewebsites.net/api/prisma-e2e-linux-test-azure-functions-is-so-amazing?code=a7HWAhCiFFMCa96cgxkVahgIjn6j15Hkdt9ah1OMSG0zamsHtwTnvw=="
 
 expected='{"createUser":{"id":"12345","email":"alice@prisma.io","name":"Alice"},"updateUser":{"id":"12345","email":"bob@prisma.io","name":"Bob"},"users":{"id":"12345","email":"bob@prisma.io","name":"Bob"},"deleteManyUsers":{"count":1}}'
 actual=$(curl -v "$url")
