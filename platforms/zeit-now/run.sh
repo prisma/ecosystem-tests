4a7a6fa2007709abba7da384b2d5a194257c40bb
Divyendu Singh <singh@prisma.io>
2020-03-02 16:57:38 +0100

    fix(zeit): move to prisma team

--- platforms/zeit-now/run.sh
+++ platforms/zeit-now/run.sh
@@ -3,12 +3,12 @@
 set -eu
 
 yarn
-yarn now --token=$ZEIT_TOKEN --prod
+yarn now --token=$ZEIT_TOKEN --prod --scope=prisma
 sleep 15
 
 set +e
 sh test.sh
 code=$?
 set -e
-now logs e2e-platforms-zeit-now.now.sh --token=$ZEIT_TOKEN
+now logs e2e-platforms-zeit-now.now.sh --token=$ZEIT_TOKEN --scope=prisma
 exit $code
