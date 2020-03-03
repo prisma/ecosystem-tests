0d931a5a1925cf41dad8b35729b36e1fd5d0ab5f
Luca Steeb <contact@luca-steeb.com>
2020-03-04 00:32:46 +0700

    fix(azure-functions-windows): remove debug

--- platforms/azure-functions-windows/prisma-e2e-windows-test-new/index.ts
+++ platforms/azure-functions-windows/prisma-e2e-windows-test-new/index.ts
@@ -1,26 +1,12 @@
 import { PrismaClient } from "@prisma/client";
 import { Context, HttpRequest } from "@azure/functions";
-import { spawnSync } from "child_process";
 
 const client = new PrismaClient();
-
-async function debug() {
-  const cmd = spawnSync(
-    "dir D:\\home\\site\\wwwroot\\node_modules\\@prisma\\client\\runtime\\query-engine-windows.exe"
-  );
-  console.log(
-    "dir D:\\home\\site\\wwwroot\\node_modules\\@prisma\\client\\runtime\\"
-  );
-  console.log("stderr", cmd.stderr);
-  console.log("stdout", cmd.stdout);
-}
 
 export default async function(
   context: Context,
   req: HttpRequest
 ): Promise<void> {
-  await debug();
-
   await client.user.deleteMany({});
 
   const id = "12345";
