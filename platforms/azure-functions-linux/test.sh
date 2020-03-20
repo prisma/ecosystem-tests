cd0b9bd962b9f78916ad3b51554fb138960f2a23
Luca Steeb <contact@luca-steeb.com>
2020-03-20 14:52:05 +0700

    fix platforms

--- platforms/azure-functions-linux/test.sh
+++ platforms/azure-functions-linux/test.sh
@@ -4,6 +4,7 @@ set -eux
 
 url="https://prisma-e2e-linux-test-azure-functions-is-so-amazing.azurewebsites.net/api/prisma-e2e-linux-test-azure-functions-is-so-amazing"
 
+prisma_version="$(cat ../../.github/prisma-version.txt)"
 expected='{"version":"'$prisma_version'","createUser":{"id":"12345","email":"alice@prisma.io","name":"Alice"},"updateUser":{"id":"12345","email":"bob@prisma.io","name":"Bob"},"users":{"id":"12345","email":"bob@prisma.io","name":"Bob"},"deleteManyUsers":{"count":1}}'
 actual=$(curl -v "$url")
 
