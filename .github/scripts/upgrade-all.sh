6bc38bd328bf3a4f77e3a3a2579b22ce9d6869ff
Luca Steeb <contact@luca-steeb.com>
2020-03-30 14:24:52 +0700

    ci(upgrade-all): revert temporary prisma2 deprecation change

--- .github/scripts/upgrade-all.sh
+++ .github/scripts/upgrade-all.sh
@@ -25,7 +25,8 @@ echo "$packages" | tr ' ' '\n' | while read -r item; do
 	cd "$(dirname "$item")/"
 
 	## ACTION
-	yarn remove prisma2 || true
+	yarn add "@prisma/cli@$channel" --dev
+	yarn add "@prisma/client@$channel"
 	## END
 
 	echo "$item done"
