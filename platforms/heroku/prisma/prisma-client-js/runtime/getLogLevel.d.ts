3f0a86a29b54aa8ab28d43784fc804092f67c054
Divyendu Singh <singh@prisma.io>
2020-04-07 14:37:10 +0530

    fix(heroku): attempt to generate client into node_modules (#295)
    * fix(heroku): attempt to generate client into node_modules
    
    * fix(heroku): fix url, actually run tests
    
    * fix(heroku): remove binary targets
    
    as in https://github.com/divyendu-test/p2-bouncer which works with
    client in `node_modules`
    
    * fix(heroku): remove redundant test.sh

--- platforms/heroku/prisma/prisma-client-js/runtime/getLogLevel.d.ts
+++
@@ -1,7 +0,0 @@
-declare type LogLevel = 'info' | 'query' | 'warn';
-declare type LogDefinition = {
-    level: LogLevel;
-    emit: 'stdout' | 'event';
-};
-export declare function getLogLevel(log: LogLevel | Array<LogLevel | LogDefinition>): LogLevel | undefined;
-export {};
--- platforms/heroku/prisma/prisma-client-js/runtime/utils/uniqueBy.d.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Returns unique elements of array
- * @param arr Array
- */
-export declare function uniqueBy<T>(arr: T[], callee: (element: T) => string): T[];
