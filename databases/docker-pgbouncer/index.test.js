b1f97254282b2c4b910d33af05a6ca59387202ca
Jan Piotrowski <piotrowski+github@gmail.com>
2020-05-20 20:58:41 +0200

    Update index.test.js

--- databases/docker-pgbouncer/index.test.js
+++ databases/docker-pgbouncer/index.test.js
@@ -1,24 +1,24 @@
 const {
-  clientWithoutFlagCall,
-  clientWithFlagCall,
+  clientWithoutQueryStringParamCall,
+  clientWithQueryStringParamCall,
   client,
-  clientWithFlag,
+  clientWithQueryStringParam,
 } = require('.')
 
 describe('should test Prisma client and pgBouncer', () => {
   afterAll(async () => {
     await client.disconnect()
-    await clientWithFlag.disconnect()
+    await clientWithQueryStringParam.disconnect()
     return
   })
 
   // TODO: Uncommenting this makes the other test fail
   // it('should fail with docker pgbouncer without the forcedTransactions flag', async () => {
-  //   expect(clientWithoutFlagCall).toThrow()
+  //   expect(clientWithoutQueryStringParamCall).toThrow()
   // })
 
   it('should work with docker pgbouncer with the forcedTransactions flag', async () => {
-    const data = await clientWithFlagCall()
+    const data = await clientWithQueryStringParamCall()
     expect(data).toMatchSnapshot()
   })
 })
