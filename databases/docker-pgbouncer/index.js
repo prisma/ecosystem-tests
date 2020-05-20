5809456c23e57964048cdb6adf53abaeaf53892c
Jan Piotrowski <piotrowski+github@gmail.com>
2020-05-20 21:05:36 +0200

    Update index.js

--- databases/docker-pgbouncer/index.js
+++ databases/docker-pgbouncer/index.js
@@ -8,13 +8,13 @@ const client = new PrismaClient({
   datasources: {
     db: "postgresql://postgres:postgres@127.0.0.1:6433/blog?schema=public",
   }
-)
+})
 
 const clientWithQueryStringParam = new PrismaClient({
   datasources: {
     db: "postgresql://postgres:postgres@127.0.0.1:6433/blog?schema=public&pgbouncer=true",
   }
-)
+})
 
 async function clientWithoutQueryStringParamCall() {
   const data = await client.user.findMany()
