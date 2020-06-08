91b64901dca3ee1cabc7fbb67c3c2902ab6bcc8c
Luca Steeb <contact@luca-steeb.com>
2020-06-08 23:13:35 +0700

    ci(check-for-update): ignore dot prisma client package

--- .github/scripts/check-for-update.sh
+++ .github/scripts/check-for-update.sh
@@ -28,7 +28,7 @@ git fetch github "master"
 git reset --hard "github/master"
 
 # prepare script: read package.json but ignore workspace package.json files
-pkg="var pkg=require('./package.json'); if (pkg.workspaces) { process.exit(0); }"
+pkg="var pkg=require('./package.json'); if (pkg.workspaces || pkg.name == '.prisma/client') { process.exit(0); }"
 
 # since GH actions are limited to 5 minute cron jobs, just run this continuously for 5 minutes
 minutes=5 # cron job runs each x minutes
