05cb88778bee7244a9667e2626b21eea4e68c916
Divyendu Singh <singh@prisma.io>
2020-06-24 16:55:00 +0530

    refactor: add fetch retry to heroku

--- platforms/heroku/test.sh
+++ platforms/heroku/test.sh
@@ -2,14 +2,4 @@
 
 set -eu
 
-url="https://e2e-platforms-heroku.herokuapp.com"
-
-prisma_version="$(cat ../../.github/prisma-version.txt)"
-
-expected='{"version":"'$prisma_version'","createUser":{"id":"12345","email":"alice@prisma.io","name":"Alice"},"updateUser":{"id":"12345","email":"bob@prisma.io","name":"Bob"},"users":{"id":"12345","email":"bob@prisma.io","name":"Bob"},"deleteManyUsers":{"count":1}}'
-actual=$(curl "$url")
-
-if [ "$expected" != "$actual" ]; then
-	echo "expected '$expected', got '$actual'"
-	exit 1
-fi
+npx ts-node ../../utils/fetch-retry.ts --url https://e2e-platforms-heroku.herokuapp.com --prisma-version $(sh ../../utils/prisma_version.sh)
