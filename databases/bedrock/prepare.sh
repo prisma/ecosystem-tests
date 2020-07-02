3fd74164ac81d45e85e6de3798d2cf75ae6a105a
Jan Piotrowski <piotrowski+github@gmail.com>
2020-07-02 19:03:01 +0200

    use migrated sqlite db to start bedrock

--- databases/bedrock/prepare.sh
+++ databases/bedrock/prepare.sh
@@ -12,8 +12,5 @@ wget -O - https://apt.bedrockdb.com/bedrock.gpg | sudo apt-key add -
 sudo apt-get update
 sudo apt-get install bedrock
 
-# Create an empty database (See: https://github.com/Expensify/Bedrock/issues/489)
-touch bedrock.db
-
 # Run it (press Ctrl^C to quit, or use -fork to make it run in the backgroud)
-bedrock -fork
+bedrock -fork -db prisma/bedrock.db
--- databases/bedrock/run.sh
+++ databases/bedrock/run.sh
@@ -7,6 +7,3 @@ export RUST_BACKTRACE=full
 
 yarn install
 yarn prisma generate
-
-yarn prisma migrate save --create-db --experimental
-yarn prisma migrate up --create-db --experimental
