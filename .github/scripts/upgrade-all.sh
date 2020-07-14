2e190733e357df45fab5c5d32caf7811a86544ad
Luca Steeb <contact@luca-steeb.com>
2020-07-14 23:56:34 +0700

    feat(upgrade): detect and handle yarn resolutions (#554)

--- .github/scripts/upgrade-all.sh
+++ .github/scripts/upgrade-all.sh
@@ -24,9 +24,18 @@ echo "$packages" | tr ' ' '\n' | while read -r item; do
 	echo "running $item"
 	cd "$(dirname "$item")/"
 
+  pkg="var pkg=require('./package.json'); if (pkg.workspaces || pkg.name == '.prisma/client') { process.exit(0); }"
+	hasResolutions="$(node -e "$pkg;console.log(!!pkg.resolutions)")"
+
 	## ACTION
-	yarn add "@prisma/cli@$channel" --dev
-	yarn add "@prisma/client@$channel"
+	if [ "$hasResolutions" = "true" ]; then
+	  v=$(sh .github/scripts/prisma-version.sh "$channel")
+	  json -I -f package.json -e "this.resolutions['@prisma/cli']='$v'"
+	  json -I -f package.json -e "this.resolutions['@prisma/client']='$v'"
+	else
+		yarn add "@prisma/cli@$channel" --dev
+		yarn add "@prisma/client@$channel"
+	fi
 	## END
 
 	echo "$item done"
