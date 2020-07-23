85bbfd92cc03d22513dd55432408d2c7ae790e42
Jason Kuhrt <jasonkuhrt@me.com>
2020-07-23 07:51:48 -0400

    fix: drop graphql-yoga

--- libraries/nexus-schema/src/server.ts
+++ libraries/nexus-schema/src/server.ts
@@ -1,9 +1,19 @@
-import { GraphQLServer } from 'graphql-yoga'
+import express from 'express'
+import { graphqlHTTP } from 'express-graphql'
 import { schema } from './schema'
-import { createContext } from './context'
 
-new GraphQLServer({ schema, context: createContext }).start(() =>
-  console.log(
-    `🚀 Server ready at: http://localhost:4000\n⭐️ See sample queries: http://pris.ly/e/ts/graphql#3-using-the-graphql-api`,
-  ),
+const app = express()
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  }),
 )
+
+app.listen(4000, () => {
+  console.log(
+    `🚀 Server ready at: http://localhost:4000/graphql\n⭐️ See sample queries: http://pris.ly/e/ts/graphql#3-using-the-graphql-api`,
+  )
+})
