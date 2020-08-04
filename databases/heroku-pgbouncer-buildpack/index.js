e75718285ba43b6626d92108cc076321382f21b0
Divyendu Singh <singh@prisma.io>
2020-08-04 17:10:22 +0530

    chore: add reconnect to heroku pgbouncer buildpack (#655)

--- databases/heroku-pgbouncer-buildpack/index.js
+++ databases/heroku-pgbouncer-buildpack/index.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv')
 dotenv.config()
 
 const { PrismaClient, prismaVersion } = require('@prisma/client')
+
 const client = new PrismaClient({
   datasources: {
     db: {
@@ -11,20 +12,46 @@ const client = new PrismaClient({
   },
 })
 
+const clientWithQueryStringParam = new PrismaClient({
+  datasources: {
+    db: {
+      url: process.env.DATABASE_HEROKU_PGBOUNCER_URL + '?pgbouncer=true',
+    },
+  },
+})
+
 const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', async (req, res) => {
-  await client.user.deleteMany({})
+  try {
+    await client.user.findMany()
+
+    /*
+    * Query engine instance names prepared statements serially s0, s1 and so on. Without the `pgbouncer=true` flag, 
+    * prepared statements are not cleaned up in PgBouncer. By doing disconnect/reconnect, we get a 
+    * new instance of query engine that starts again at s0. And we expect the next client call to throw
+    * "prepared statement s0 already exists"
+    */
+    await client.disconnect()
+    await client.connect()
+
+    await client.user.findMany()
+    process.exit(1) // The code should never reach here
+  } catch (e) {
+    console.log(e)
+  }
+
+  await clientWithQueryStringParam.user.deleteMany({})
   const id = '12345'
-  const createUser = await client.user.create({
+  const createUser = await clientWithQueryStringParam.user.create({
     data: {
       id,
       email: 'alice@prisma.io',
       name: 'Alice',
     },
   })
-  const updateUser = await client.user.update({
+  const updateUser = await clientWithQueryStringParam.user.update({
     where: {
       id,
     },
@@ -33,12 +60,18 @@ app.get('/', async (req, res) => {
       name: 'Bob',
     },
   })
-  const users = await client.user.findOne({
+  const users = await clientWithQueryStringParam.user.findOne({
     where: {
       id,
     },
   })
-  const deleteManyUsers = await client.user.deleteMany()
+
+  await clientWithQueryStringParam.user.findOne({
+    where: {
+      id,
+    },
+  })
+  const deleteManyUsers = await clientWithQueryStringParam.user.deleteMany()
   return res.send(
     JSON.stringify({
       version: prismaVersion.client,
