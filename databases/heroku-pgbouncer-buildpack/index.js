dbb4c2cd330b8d32794ba05ee405a7192069158a
Divyendu Singh <singh@prisma.io>
2020-08-04 22:09:53 +0530

    fix: remove failing test

--- databases/heroku-pgbouncer-buildpack/index.js
+++ databases/heroku-pgbouncer-buildpack/index.js
@@ -4,18 +4,10 @@ dotenv.config()
 
 const { PrismaClient, prismaVersion } = require('@prisma/client')
 
-const client = new PrismaClient({
-  datasources: {
-    db: {
-      url: process.env.DATABASE_URL_PGBOUNCER,
-    },
-  },
-})
-
 const clientWithQueryStringParam = new PrismaClient({
   datasources: {
     db: {
-      url: process.env.DATABASE_HEROKU_PGBOUNCER_URL + '?pgbouncer=true',
+      url: process.env.DATABASE_URL_PGBOUNCER + '?pgbouncer=true',
     },
   },
 })
@@ -24,26 +16,6 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', async (req, res) => {
-  try {
-    await client.user.findMany()
-
-    /*
-    * Query engine instance names prepared statements serially s0, s1 and so on. Without the `pgbouncer=true` flag, 
-    * prepared statements are not cleaned up in PgBouncer. By doing disconnect/reconnect, we get a 
-    * new instance of query engine that starts again at s0. And we expect the next client call to throw
-    * "prepared statement s0 already exists"
-    */
-    await client.disconnect()
-    await client.connect()
-
-    await client.user.findMany()
-    process.exit(1) // The code should never reach here
-  } catch (e) {
-    console.log("===START: ERROR EXPECTED===============")
-    console.log(e)
-    console.log("===END: ERROR EXPECTED===============")
-  }
-
   await clientWithQueryStringParam.user.deleteMany({})
   const id = '12345'
   const createUser = await clientWithQueryStringParam.user.create({
