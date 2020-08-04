f327f9b09e83e1e214585909ec89d2a615fb3420
Divyendu Singh <singh@prisma.io>
2020-08-04 15:36:26 +0530

    chore: improve E2E PgBouncer tests with client reconnect (#651)

--- databases/digitalocean-pgbouncer/index.js
+++ databases/digitalocean-pgbouncer/index.js
@@ -21,11 +21,15 @@ const clientWithQueryStringParam = new PrismaClient({
 })
 
 async function clientWithoutQueryStringParamCall() {
+  await client.disconnect()
+  await client.connect()
   const data = await client.user.findMany()
   return data
 }
 
 async function clientWithQueryStringParamCall() {
+  await clientWithQueryStringParam.disconnect()
+  await clientWithQueryStringParam.connect()
   const data = await clientWithQueryStringParam.user.findMany()
   return data
 }
@@ -44,7 +48,7 @@ async function main() {
 
 if (require.main === module) {
   main()
-    .then((_) => {})
+    .then((_) => { })
     .catch((e) => {
       console.log(e)
     })
