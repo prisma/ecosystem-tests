654b6c95b658e8b65363b086c4130b68257381d7
Divyendu Singh <singh@prisma.io>
2020-08-04 16:22:43 +0530

    chore: add reconnect to heroku pgbouncer buildpack

--- databases/heroku-pgbouncer-buildpack/index.js
+++ databases/heroku-pgbouncer-buildpack/index.js
@@ -38,6 +38,21 @@ app.get('/', async (req, res) => {
       id,
     },
   })
+
+  /*
+  * Query engine instance names prepared statements serially s0, s1 and so on. Without the `pgbouncer=true` flag, 
+  * prepared statements are not cleaned up in PgBouncer. By doing disconnect/reconnect, we get a 
+  * new instance of query engine that starts again at s0. And we expect the next client call to throw
+  * "prepared statement s0 already exists"
+  */
+  await client.disconnect()
+  await client.connect()
+
+  await client.user.findOne({
+    where: {
+      id,
+    },
+  })
   const deleteManyUsers = await client.user.deleteMany()
   return res.send(
     JSON.stringify({
