a706b7d1058ead96ef00aabc043b6e651b5827ec
Divyendu Singh <singh@prisma.io>
2020-08-04 16:06:15 +0530

    chore: add comments

--- databases/digitalocean-pgbouncer/index.js
+++ databases/digitalocean-pgbouncer/index.js
@@ -35,9 +35,15 @@ async function main() {
     const data1 = await client.user.findMany()
     console.log(data1)
 
+    /*
+    * Query engine instance names prepared statements serially s0, s1 and so on. Without the pgbouncer=true flag, 
+    * prepared statements are not cleaned up in PgBouncer. By doing disconnect/reconnect, we get a 
+    * new insatnce of query engine that starts again at s0. And we expect the next client call to throw
+    * prepared statement s0 already exists
+    */
     await client.disconnect()
     await client.connect()
-    
+
     const data2 = await client.user.findMany()
     console.log(data2)
   } catch (e) {
