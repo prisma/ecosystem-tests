2a28b230885dd9844e7ac31ae207371de0ebc733
Divyendu Singh <singh@prisma.io>
2020-08-27 20:57:36 +0530

    fix: strip pgbouncer tests output color

--- databases/docker-pgbouncer/index.js
+++ databases/docker-pgbouncer/index.js
@@ -5,6 +5,7 @@ dotenv.config({
 })
 
 const client = new PrismaClient({
+  errorFormat: 'colorless',
   datasources: {
     db: {
       url: 'postgresql://postgres:postgres@127.0.0.1:6433/blog?schema=public',
@@ -13,6 +14,7 @@ const client = new PrismaClient({
 })
 
 const clientWithQueryStringParam = new PrismaClient({
+  errorFormat: 'colorless',
   datasources: {
     db: {
       url:
@@ -42,11 +44,11 @@ async function main() {
     console.log({ data1 })
 
     /*
-    * Query engine instance names prepared statements serially s0, s1 and so on. Without the `pgbouncer=true` flag, 
-    * prepared statements are not cleaned up in PgBouncer. By doing disconnect/reconnect, we get a 
-    * new instance of query engine that starts again at s0. And we expect the next client call to throw
-    * "prepared statement s0 already exists"
-    */
+     * Query engine instance names prepared statements serially s0, s1 and so on. Without the `pgbouncer=true` flag,
+     * prepared statements are not cleaned up in PgBouncer. By doing disconnect/reconnect, we get a
+     * new instance of query engine that starts again at s0. And we expect the next client call to throw
+     * "prepared statement s0 already exists"
+     */
     await client.$disconnect()
     await client.$connect()
 
@@ -65,7 +67,7 @@ async function main() {
 
 if (require.main === module) {
   main()
-    .then((_) => { })
+    .then((_) => {})
     .catch((e) => {
       console.log(e)
     })
