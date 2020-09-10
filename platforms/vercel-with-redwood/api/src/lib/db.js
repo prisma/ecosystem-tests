f2cef7dd0cc8231f2bfddb5545cccf1b61f112cd
Divyendu Singh <singh@prisma.io>
2020-09-10 16:16:33 +0200

    feat(redwood): init (#808)

---
+++ platforms/vercel-with-redwood/api/src/lib/db.js
@@ -0,0 +1,8 @@
+// See https://www.prisma.io/docs/reference/tools-and-interfaces/prisma-client/constructor
+// for options.
+
+// https://redwoodjs.com/tutorial/redwood-file-structure
+
+import { PrismaClient } from '@prisma/client'
+
+export const db = new PrismaClient()
