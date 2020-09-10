d0c06f0adeb12967b6c7d25dd4d40747b5f862de
Divyendu Singh <singh@prisma.io>
2020-09-10 14:00:25 +0200

    feat(redwood): use resolutions

--- platforms/vercel-with-redwood/index.test.js
+++ platforms/vercel-with-redwood/index.test.js
@@ -1,21 +1,29 @@
 const { request, gql } = require('graphql-request')
+const pjson = require('./api/package.json')
 
-const query = gql`
-  query {
-    prismaVersion
-    users {
-      id
-      email
-      name
+const endpoint = 'https://e2e-vercel-with-redwood.vercel.app/api/graphql'
+// const endpoint = 'http://localhost:8911/graphql'
+
+test('should test prisma version', async () => {
+  const query = gql`
+    query {
+      prismaVersion
     }
-  }
-`
+  `
+  const data = await request(endpoint, query)
+  expect(data.prismaVersion).toEqual(pjson.dependencies['@prisma/client'])
+})
 
 test('should query graphql users', async () => {
-  const data = await request(
-    'https://e2e-vercel-with-redwood.vercel.app/api/graphql',
-    // 'http://localhost:8911/graphql',
-    query
-  )
+  const query = gql`
+    query {
+      users {
+        id
+        email
+        name
+      }
+    }
+  `
+  const data = await request(endpoint, query)
   expect(data).toMatchSnapshot()
 })
