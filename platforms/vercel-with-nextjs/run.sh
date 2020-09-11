6bf2177bde0ab33804f6f41ca1e112d865768867
Divyendu Singh <singh@prisma.io>
2020-09-11 17:53:59 +0200

    fix(vercel-with-nextjs): env var names

--- platforms/vercel-with-nextjs/run.sh
+++ platforms/vercel-with-nextjs/run.sh
@@ -4,8 +4,8 @@ set -eu
 
 export PRISMA_TELEMETRY_INFORMATION='e2e-tests platforms vercel-with-nextjs build'
 yarn
-export VERCEL_PROJECT_ID=$VERCEL_WITH_REDWOOD_PROJECT_ID
-export VERCEL_ORG_ID=$VERCEL_WITH_REDWOOD_ORG_ID
+export VERCEL_PROJECT_ID=$VERCEL_WITH_NEXTJS_PROJECT_ID
+export VERCEL_ORG_ID=$VERCEL_WITH_NEXTJS_ORG_ID
 echo "VERCEL_PROJECT_ID: $VERCEL_PROJECT_ID"
 echo "VERCEL_ORG_ID: $VERCEL_ORG_ID"
 yarn vercel --token=$VERCEL_TOKEN --prod --scope=prisma --confirm
