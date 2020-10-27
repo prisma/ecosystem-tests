3ac5d773465b64a03024606b45cf15596656fc99
eemmiillyy <emily.morgan@code.berlin>
2020-10-27 15:42:03 +0100

    feat(test): add postgres service back to GH actions

--- core-features/auto-recovery/run.sh
+++ core-features/auto-recovery/run.sh
@@ -3,8 +3,9 @@
 set -eu 
 
 yarn install 
-docker ps --filter name=auto-recovery* --filter status=running -aq | xargs docker stop
-docker ps --filter name=auto-recovery* --filter status=running -aq | xargs docker container rm
-docker-compose up -d
+# comment this in for testing on the locally hosted postgres service
+# docker ps --filter name=auto-recovery* --filter status=running -aq | xargs docker stop $(docker ps -aq)
+# docker ps --filter name=auto-recovery* --filter status=running -aq | xargs docker container rm
+# docker-compose up -d
 sleep 5
 yarn prisma generate
