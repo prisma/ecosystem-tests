bd3b894add3eb5434d81a10eaaf55b95ab371fe5
Divyendu Singh <singh@prisma.io>
2020-11-10 10:47:39 +0100

    feat(community-generators): add option slack channel webhooks

--- .github/slack/notify.js
+++ .github/slack/notify.js
@@ -1,23 +1,12 @@
-const arg = require('arg')
 const post = require('./post')
 
 async function run() {
-  const args = arg({
-    'message-prefix': String,
-  })
-
   const url = process.env.webhook
   let message = process.argv[2]
-
-  const messagePrefix = args['message-prefix']
 
   const startTime = process.env.START_TIME
   if (Boolean(startTime)) {
     message = `${message} (Start Time: ${startTime})`
-  }
-
-  if (Boolean(messagePrefix) && Boolean(messagePrefix.trim())) {
-    message = `${messagePrefix} ${message}`
   }
 
   await post(url, message)
