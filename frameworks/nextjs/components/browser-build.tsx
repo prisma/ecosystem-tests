d3a559ba0e882aeff404f460e5251341a54f0f43
William Luke <william@ordino.ai>
2020-12-07 12:34:37 +0300

    pretty

--- frameworks/nextjs/components/browser-build.tsx
+++ frameworks/nextjs/components/browser-build.tsx
@@ -1,13 +1,15 @@
 import { Prisma } from '@prisma/client';
 import React from 'react'
+const prettyPrintJson = require('pretty-print-json');
+
 export const BrowserBuild = () => {
   // const shouldThrow = new PrismaClient()
   // @ts-ignore
   const decimal = new Prisma.Decimal(0.213213);
   return (
-    <div>
-      {JSON.stringify(decimal)}
-      {JSON.stringify(Prisma.UserDistinctFieldEnum)}
+    <div style={{textAlign: 'left', margin: 'auto', maxWidth: 700, padding: 10}}>
+      Decimal Test: {JSON.stringify(decimal.squareRoot())}
+      {<div><pre>{JSON.stringify(Prisma, null, 2) }</pre></div>}
     </div>
   )
 }
