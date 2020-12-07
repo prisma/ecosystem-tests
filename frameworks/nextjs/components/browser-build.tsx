1793e0bbfc485f1125a25be3e4b8d43fe24b8dcb
William Luke <william@ordino.ai>
2020-12-07 12:40:58 +0300

    clean up

--- frameworks/nextjs/components/browser-build.tsx
+++ frameworks/nextjs/components/browser-build.tsx
@@ -1,14 +1,25 @@
-import { Prisma } from '@prisma/client';
+import * as Prisma from '@prisma/client';
 import React from 'react'
-const prettyPrintJson = require('pretty-print-json');
 
 export const BrowserBuild = () => {
-  // const shouldThrow = new PrismaClient()
-  // @ts-ignore
-  const decimal = new Prisma.Decimal(0.213213);
+  const [err, setErr] = React.useState()
+  const decimal = new Prisma.Prisma.Decimal(0.213213);
+  React.useEffect(() => {
+    try {
+      const client = new Prisma.PrismaClient()
+    } catch (err) {
+      console.log(err);
+      setErr(err.message)
+    }
+  }, [])
   return (
     <div style={{textAlign: 'left', margin: 'auto', maxWidth: 700, padding: 10}}>
-      Decimal Test: {JSON.stringify(decimal.squareRoot())}
+      <h2>Prisma Browser Build Test</h2>
+      <strong>Error Test:</strong> {err}
+      <br/>
+      <br/>
+      <strong>Decimal Test:</strong> {JSON.stringify(decimal.squareRoot())}
+      <strong>Browser Test:</strong>
       {<div><pre>{JSON.stringify(Prisma, null, 2) }</pre></div>}
     </div>
   )
