9fde718e87113d37fb0153a431832524f5f4500c
Divyendu Singh <singh@prisma.io>
2020-12-10 12:59:44 +0100

    feat(gcp-mysql): and ssl

--- databases/gcp-mysql-ssl/run.sh
+++ databases/gcp-mysql-ssl/run.sh
@@ -2,9 +2,12 @@
 
 set -eu
 
-echo $GCP_MYSQL_SSL_CLIENT_CERT >> client-cert.pem
-echo $GCP_MYSQL_SSL_CLIENT_KEY >> client-key.pem
-echo $GCP_MYSQL_SSL_SERVER_CA >> server-ca.pem
+pwd
+ls
+echo "$GCP_MYSQL_SSL_CLIENT_CERT" >> client-cert.pem
+echo "$GCP_MYSQL_SSL_CLIENT_KEY" >> client-key.pem
+echo "$GCP_MYSQL_SSL_SERVER_CA" >> server-ca.pem
+ls
 
 openssl pkcs12 -export -out client-identity.p12 -inkey client-key.pem -in client-cert.pem -password pass:prisma
 
