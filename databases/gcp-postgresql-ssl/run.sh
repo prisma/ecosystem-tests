eeea76c4d81bba4136b99be163e471902e7a09a0
Divyendu Singh <singh@prisma.io>
2020-12-10 14:51:37 +0100

    feat(gcp-postgres): fix chmod

--- databases/gcp-postgresql-ssl/run.sh
+++ databases/gcp-postgresql-ssl/run.sh
@@ -4,7 +4,7 @@ set -eu
 
 echo "$GCP_POSTGRESQL_SSL_CLIENT_CERT" >> client-cert.pem
 echo "$GCP_POSTGRESQL_SSL_CLIENT_KEY" >> client-key.pem
-chmod 600 ./client-key
+chmod 600 ./client-key.pem
 echo "$GCP_POSTGRESQL_SSL_SERVER_CA" >> server-ca.pem
 
 openssl pkcs12 -export -out client-identity.p12 -inkey client-key.pem -in client-cert.pem -password pass:prisma
