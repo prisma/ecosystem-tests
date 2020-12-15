40026d5c168968f091b87685567c605c77524abe
Divyendu Singh <singh@prisma.io>
2020-12-15 13:18:43 +0100

    feat(platform): aws graviton

--- platforms/aws-graviton/prepare.sh
+++ platforms/aws-graviton/prepare.sh
@@ -4,13 +4,10 @@ set -eu
 
 cp ./code/package.json ./package.json
 
-if [ "$OSTYPE" = "linux" ]; then
-    echo "Disabling StrictHostKeyChecking"
-    mkdir -p ~/.ssh
-    ssh-keyscan 54.72.209.131 >> ~/.ssh/known_hosts
-
-    echo "$SSH_KEY_GRAVITON" > ./server-key.pem
-fi
+echo "Disabling StrictHostKeyChecking"
+mkdir -p ~/.ssh
+ssh-keyscan 54.72.209.131 >> ~/.ssh/known_hosts
+echo "$SSH_KEY_GRAVITON" > ./server-key.pem
 
 if test -f "./server-key.pem"; then
     chmod 600 ./server-key.pem
