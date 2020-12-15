2c0e10573e84e88668be2e787d0bd772edea800a
Divyendu Singh <singh@prisma.io>
2020-12-15 14:02:11 +0100

    feat(platform): aws graviton (#1057)

---
+++ platforms/aws-graviton/prepare.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -eu
+
+cp ./code/package.json ./package.json
+
+echo "Disabling StrictHostKeyChecking"
+mkdir -p ~/.ssh
+ssh-keyscan 54.72.209.131 >> ~/.ssh/known_hosts
+echo "$SSH_KEY_GRAVITON" > ./server-key.pem
+
+if test -f "./server-key.pem"; then
+    chmod 600 ./server-key.pem
+fi
