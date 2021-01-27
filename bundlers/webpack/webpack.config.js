5a0b2b162fca2c943ec0b11f0748c85138c190f2
Sid Jain <sid_26@outlook.com>
2021-01-27 21:04:53 +0530

    create reproduction for webpack bunling runtime error

--- bundlers/webpack/webpack.config.js
+++ bundlers/webpack/webpack.config.js
@@ -1,6 +1,14 @@
-const nodeExternals = require('webpack-node-externals')
+// I will bundle node_modules
+// const nodeExternals = require('webpack-node-externals')
 
 module.exports = {
   target: 'node',
-  externals: [nodeExternals()],
+  
+  // I will bundle node_modules
+  // externals: [nodeExternals()],
+
+  // https://webpack.js.org/configuration/node/
+  // webpack patches __dirname which causes string_encoding/ to not be resolved
+  // uncomment to make that error go
+  // node: false
 }
