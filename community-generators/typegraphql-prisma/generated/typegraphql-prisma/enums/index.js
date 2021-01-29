a2bd3b92de42bcc3605646ad654b68c1bc0b7605
Divyendu Singh <singh@prisma.io>
2021-01-29 12:26:10 +0100

    chore: more lock file bumps

---
+++ community-generators/typegraphql-prisma/generated/typegraphql-prisma/enums/index.js
@@ -0,0 +1,18 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+exports.__esModule = true;
+exports.UserType = exports.UserScalarFieldEnum = exports.SortOrder = exports.QueryMode = void 0;
+var QueryMode_1 = require("./QueryMode");
+__createBinding(exports, QueryMode_1, "QueryMode");
+var SortOrder_1 = require("./SortOrder");
+__createBinding(exports, SortOrder_1, "SortOrder");
+var UserScalarFieldEnum_1 = require("./UserScalarFieldEnum");
+__createBinding(exports, UserScalarFieldEnum_1, "UserScalarFieldEnum");
+var UserType_1 = require("./UserType");
+__createBinding(exports, UserType_1, "UserType");
---
+++ test-runners/jest-with-multiple-generators/src/__tests__/the.test.js
@@ -0,0 +1,5 @@
+"use strict";
+exports.__esModule = true;
+it('should work', function () {
+    expect(1).toBe(1);
+});
