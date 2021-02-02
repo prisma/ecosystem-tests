64560ce9eafbf70f81a4ada8887c2bd4c1a38706
Divyendu Singh <singh@prisma.io>
2021-02-02 12:52:49 +0100

    chore: fixes

--- platforms/lambda/zip.sh
+++ platforms/lambda/zip.sh
@@ -10,6 +10,7 @@ rm -rf lambda.zip
 rm -rf node_modules/@prisma/cli
 rm -rf node_modules/typescript
 
+yarn prisma generate
 zip -r lambda.zip index.js prisma/schema.prisma node_modules/.prisma node_modules/**
 
 du -h ./lambda.zip
