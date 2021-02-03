ff650f800a76648e7f7ef0ea4133627c275142b5
Divyendu Singh <singh@prisma.io>
2021-02-03 14:48:18 +0100

    chore: remove typegraphql-prisma generated part

--- community-generators/typegraphql-prisma/generated/typegraphql-prisma/enums/index.ts
+++
@@ -1,4 +0,0 @@
-export { QueryMode } from "./QueryMode";
-export { SortOrder } from "./SortOrder";
-export { UserScalarFieldEnum } from "./UserScalarFieldEnum";
-export { UserType } from "./UserType";
