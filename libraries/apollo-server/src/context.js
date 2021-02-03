17b0eccfc861b263c6595193c649282e90782902
Divyendu Singh <singh@prisma.io>
2021-02-03 14:56:19 +0100

    chore: remove apollo-server js

--- libraries/apollo-server/src/context.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.createContext = void 0;
-var client_1 = require("@prisma/client");
-var prisma = new client_1.PrismaClient();
-function createContext() {
-    return { prisma: prisma };
-}
-exports.createContext = createContext;
