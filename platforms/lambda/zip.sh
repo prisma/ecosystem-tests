31c21be0bb0ef17de4209f5595db3d6c6415569b
Jan Piotrowski <piotrowski+github@gmail.com>
2021-02-03 13:50:49 +0100

    fix lambda

--- platforms/lambda/zip.sh
+++ platforms/lambda/zip.sh
@@ -7,7 +7,7 @@ yarn install --production
 
 rm -rf lambda.zip
 
-rm -rf node_modules/prisma
+rm -rf node_modules/@prisma/cli
 rm -rf node_modules/typescript
 
 zip -r lambda.zip index.js prisma/schema.prisma node_modules/.prisma node_modules/**
