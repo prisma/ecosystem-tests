3c14b54c6a829f3da33da83d0616d284175c5c63
William Luke <william@ordino.ai>
2021-02-05 14:06:58 +0300

    more experimenting

--- platforms/vercel-api/finally.sh
+++ platforms/vercel-api/finally.sh
@@ -2,8 +2,6 @@
 
 set -eu
 
-action_start_time = $START_TIME
 OUTPUT=$(yarn vercel logs e2e-vercel-api.vercel.app --token=$VERCEL_TOKEN --scope=prisma)
 echo "${OUTPUT}"
 echo "${OUTPUT}" | grep -q 'Generated Prisma Client' && echo 'Prisma Client Was Successfully Generated'
-echo "${action_start_time}"
--- platforms/vercel-api/run.sh
+++ platforms/vercel-api/run.sh
@@ -8,5 +8,9 @@ export VERCEL_PROJECT_ID=$VERCEL_API_PROJECT_ID
 export VERCEL_ORG_ID=$VERCEL_API_ORG_ID
 echo "VERCEL_PROJECT_ID: $VERCEL_PROJECT_ID"
 echo "VERCEL_ORG_ID: $VERCEL_ORG_ID"
-yarn vercel --token=$VERCEL_TOKEN --prod --scope=prisma --confirm --force
+DEPLOYED_URL=$(yarn vercel --token=$VERCEL_TOKEN --prod --scope=prisma --confirm --force)
+echo "${DEPLOYED_URL}"
 sleep 15
+OUTPUT=$(yarn vercel logs $DEPLOYED_URL --token=$VERCEL_TOKEN --scope=prisma)
+echo "${OUTPUT}"
+echo "${OUTPUT}" | grep -q 'Generated Prisma Client' && echo 'Prisma Client Was Successfully Generated'
