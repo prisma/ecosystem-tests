1856d01352286c420afa2a4506cf1e5e1aea1974
William Luke <william@ordino.ai>
2021-02-05 13:15:26 +0300

    feat(vercel-api): test postinstall

--- platforms/vercel-api/finally.sh
+++ platforms/vercel-api/finally.sh
@@ -2,4 +2,7 @@
 
 set -eu
 
-yarn vercel logs e2e-vercel-api.vercel.app --token=$VERCEL_TOKEN --scope=prisma
+
+OUTPUT=$(yarn vercel logs e2e-vercel-api.vercel.app --token=$VERCEL_TOKEN --scope=prisma)
+echo "${OUTPUT}"
+echo "${OUTPUT}" | grep -q 'Generated Prisma Client' && echo 'Prisma Client Was Successfully Generated'
