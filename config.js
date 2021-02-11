d3d87cd59e4b4765ec3b0296388205a9c65c41f5
Divyendu Singh <divyendu.z@gmail.com>
2021-02-11 18:02:45 +0100

    chore: add renovate + dev2 config

---
+++ config.js
@@ -0,0 +1,11 @@
+const tempDirectory = require('temp-dir')
+
+module.exports = {
+  onboarding: true, // https://docs.renovatebot.com/self-hosted-configuration/#onboarding
+  cacheDir: tempDirectory,
+  token: process.env.GH_TOKEN,
+  repositories: ['prisma/e2e-tests'],
+  requireConfig: true,
+  forceCli: true,
+  force: true,
+}
