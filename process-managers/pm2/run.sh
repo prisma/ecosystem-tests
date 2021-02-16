bd34f68a4465edab204f996e35451412ec4c57b1
Jan Piotrowski <piotrowski+github@gmail.com>
2021-02-16 17:34:18 +0100

    fix(process-managers/pm2): Add -f to force restart on second or third try, double wait time (#1441)

--- process-managers/pm2/run.sh
+++ process-managers/pm2/run.sh
@@ -5,5 +5,5 @@ set -eux
 yarn install
 yarn prisma generate
 
-yarn pm2 start --name prisma-pm2 --interpreter node "./server.js"
-sleep 5
+yarn pm2 start --name prisma-pm2 --interpreter node "./server.js" -f
+sleep 10
