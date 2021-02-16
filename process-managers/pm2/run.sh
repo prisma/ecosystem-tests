569ffdbeabbdaecb08da243da3deb01f29dbb140
Jan Piotrowski <piotrowski+github@gmail.com>
2021-02-16 16:36:55 +0100

    fix(process-managers/pm2): Add -f to force restart on second or third try
    Right now test is a bit flaky on macos, as sometimes it seems to not answer on the first try.
    Example: https://github.com/prisma/e2e-tests/runs/1911696820?check_suite_focus=true

--- process-managers/pm2/run.sh
+++ process-managers/pm2/run.sh
@@ -5,5 +5,5 @@ set -eux
 yarn install
 yarn prisma generate
 
-yarn pm2 start --name prisma-pm2 --interpreter node "./server.js"
+yarn pm2 start --name prisma-pm2 --interpreter node "./server.js" -f
 sleep 5
