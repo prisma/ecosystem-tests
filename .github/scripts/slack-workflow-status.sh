ba5541e34db2dd7ac1fae59d7ca8eb78301273c8
Jan Piotrowski <piotrowski+github@gmail.com>
2021-03-05 16:06:41 +0100

    rework slack shell script

--- .github/scripts/slack-workflow-status.sh
+++ .github/scripts/slack-workflow-status.sh
@@ -3,18 +3,22 @@
 set -eu
 shopt -s inherit_errexit || true
 
-(cd .github/slack/ && yarn install --silent)
-
 emoji="$1"
 
-export webhook="$SLACK_WEBHOOK_URL_WORKFLOWS"
-version="$(cat .github/prisma-version.txt)"
-branch="$(git rev-parse --abbrev-ref HEAD)"
-sha="$(git rev-parse HEAD)"
-short_sha="$(echo "$sha" | cut -c -7)"
-message="$(git log -1 --pretty=%B | head -n 1)"
+if [ "$GITHUB_REF" = "refs/heads/dev" ] || [ "$GITHUB_REF" = "refs/heads/integration" ] || [ "$GITHUB_REF" = "refs/heads/patch-dev" ] || [ "$GITHUB_REF" = "refs/heads/latest" ]; then
 
-commit_link="\`<https://github.com/prisma/e2e-tests/commit/$sha|$branch@$short_sha>\`"
-workflow_link="<https://github.com/prisma/e2e-tests/actions/runs/$GITHUB_RUN_ID|$message>"
+  (cd .github/slack/ && yarn install --silent)
 
-node .github/slack/notify.js "prisma@$version: $emoji $workflow_link (via $commit_link)"
+  version="$(cat .github/prisma-version.txt)"
+  branch="$(git rev-parse --abbrev-ref HEAD)"
+  sha="$(git rev-parse HEAD)"
+  short_sha="$(echo "$sha" | cut -c -7)"
+  message="$(git log -1 --pretty=%B | head -n 1)"
+
+  commit_link="\`<https://github.com/prisma/e2e-tests/commit/$sha|$branch@$short_sha>\`"
+  workflow_link="<https://github.com/prisma/e2e-tests/actions/runs/$GITHUB_RUN_ID|$message>"
+
+  export webhook="$SLACK_WEBHOOK_URL_WORKFLOWS"
+  node .github/slack/notify.js "prisma@$version: $emoji $workflow_link (via $commit_link)"
+
+fi
--- .github/slack/notify-failure.sh
+++ .github/slack/notify-failure.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 set -eu
+shopt -s inherit_errexit || true
 
 dir=$1
 project=$2
@@ -9,21 +10,18 @@ matrix=$3
 set -u
 
 if [ "$GITHUB_REF" = "refs/heads/dev" ] || [ "$GITHUB_REF" = "refs/heads/integration" ] || [ "$GITHUB_REF" = "refs/heads/patch-dev" ] || [ "$GITHUB_REF" = "refs/heads/latest" ]; then
+
   (cd .github/slack/ && yarn install --silent)
 
+  version="$(cat .github/prisma-version.txt)"
   branch="${GITHUB_REF##*/}"
   sha="$(git rev-parse HEAD | cut -c -7)"
   short_sha="$(echo "$sha" | cut -c -7)"
   commit_link="\`<https://github.com/prisma/e2e-tests/commit/$sha|$branch@$short_sha>\`"
   workflow_link="<https://github.com/prisma/e2e-tests/actions/runs/$GITHUB_RUN_ID|$project $matrix>"
 
-  export webhook="$SLACK_WEBHOOK_URL"
-  version="$(cat .github/prisma-version.txt)"
-  sha="$(git rev-parse HEAD | cut -c -7)"
-
-  emoji=":x:"
-
-  export webhook="$SLACK_WEBHOOK_URL_FAILING"
   echo "notifying failing slack channel"
-  node .github/slack/notify.js "prisma@$version: ${emoji} $workflow_link failed (via $commit_link)"
+  export webhook="$SLACK_WEBHOOK_URL_FAILING"
+  node .github/slack/notify.js "prisma@$version: :x: $workflow_link failed (via $commit_link)"
+
 fi
