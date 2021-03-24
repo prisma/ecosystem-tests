3cd6e8c220e363f58021ec294556f34440199e78
Jan Piotrowski <piotrowski+github@gmail.com>
2021-03-24 21:21:58 +0000

    copy other way

--- platforms/lambda/zip.sh
+++ platforms/lambda/zip.sh
@@ -13,7 +13,8 @@ rm -rf node_modules/typescript
 
 # zip -r lambda.zip index.js prisma/schema.prisma node_modules/.prisma node_modules/**
 
-npx copyfiles index.js prisma/schema.prisma node_modules/.prisma node_modules/** temp
+npx copyfiles index.js prisma/schema.prisma temp
+npx cpr node_modules temp/node_modules 
 npx --package=cross-zip-cli cross-zip temp lambda.zip
 
 du -b ./lambda.zip
