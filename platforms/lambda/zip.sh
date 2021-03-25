e1c9f4069f9d5863c06f30b8990896f4f934e4db
Jan Piotrowski <piotrowski+github@gmail.com>
2021-03-25 17:54:07 +0100

    Update zip.sh

--- platforms/lambda/zip.sh
+++ platforms/lambda/zip.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
 
 set -eux
 
@@ -13,18 +14,25 @@ rm -rf node_modules/typescript
 
 UNAME=$(uname)
 
-if [[ "$UNAME" == "Linux" ]] ; then
-  zip -r lambda.zip index.js prisma/schema.prisma node_modules/.prisma node_modules/**
-elif [[ "$UNAME" == "Darwin" ]] ; then
-  echo "Darwin"
-elif [[ "$UNAME" == CYGWIN* || "$UNAME" == MINGW* || "$UNAME" == MSYS* ]] ; then
-  rm -rf temp
-  npx copyfiles index.js prisma/schema.prisma temp
-  npx cpr node_modules/.prisma temp/node_modules/.prisma
-  npx cpr node_modules/@prisma temp/node_modules/@prisma
-  npx cpr node_modules/@types temp/node_modules/@types
+case $( "${UNAME}" | tr '[:upper:]' '[:lower:]') in
+  linux*)
+    zip -r lambda.zip index.js prisma/schema.prisma node_modules/.prisma node_modules/**
+    ;;
+  darwin*)
+    printf 'darwin\n'
+    ;;
+  msys*|cygwin*|mingw*|nt|win*)
+    rm -rf temp
+    npx copyfiles index.js prisma/schema.prisma temp
+    npx cpr node_modules/.prisma temp/node_modules/.prisma
+    npx cpr node_modules/@prisma temp/node_modules/@prisma
+    npx cpr node_modules/@types temp/node_modules/@types
 
-  powershell.exe -nologo -noprofile -command "& { param([String]$sourceDirectoryName, [String]$destinationArchiveFileName, [Boolean]$includeBaseDirectory); Add-Type -A 'System.IO.Compression.FileSystem'; Add-Type -A 'System.Text.Encoding'; [IO.Compression.ZipFile]::CreateFromDirectory($sourceDirectoryName, $destinationArchiveFileName, [IO.Compression.CompressionLevel]::Fastest, $includeBaseDirectory, [System.Text.Encoding]::UTF8); exit !$?;}" -sourceDirectoryName temp -destinationArchiveFileName lambda.zip -includeBaseDirectory $false
-fi
+    powershell.exe -nologo -noprofile -command "& { param([String]$sourceDirectoryName, [String]$destinationArchiveFileName, [Boolean]$includeBaseDirectory); Add-Type -A 'System.IO.Compression.FileSystem'; Add-Type -A 'System.Text.Encoding'; [IO.Compression.ZipFile]::CreateFromDirectory($sourceDirectoryName, $destinationArchiveFileName, [IO.Compression.CompressionLevel]::Fastest, $includeBaseDirectory, [System.Text.Encoding]::UTF8); exit !$?;}" -sourceDirectoryName temp -destinationArchiveFileName lambda.zip -includeBaseDirectory $false
+    ;;
+  *)
+    printf 'unknown\n'
+    ;;
+esac
 
 du -b ./lambda.zip
