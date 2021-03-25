78bcdfa4d016b3814c8a33ddce6f835aa4c74d57
Jan Piotrowski <piotrowski+github@gmail.com>
2021-03-25 21:43:38 +0100

    Update index.ts

--- platforms/lambda/index.ts
+++ platforms/lambda/index.ts
@@ -1,4 +1,5 @@
 const process = require('process');
+
 const measure_start = process.hrtime.bigint()
 
 import { PrismaClient, Prisma } from '@prisma/client'
@@ -12,8 +13,6 @@ export async function handler() {
   const measure_handler = process.hrtime.bigint()
   
   await client.user.deleteMany({})
-
-  const measure_delete_1 = process.hrtime.bigint()
   
   const id = '12345'
 
@@ -24,8 +23,6 @@ export async function handler() {
       name: 'Alice',
     },
   })
-
-  const measure_create = process.hrtime.bigint()
   
   const updateUser = await client.user.update({
     where: {
@@ -36,20 +33,16 @@ export async function handler() {
       name: 'Bob',
     },
   })
-
-  const measure_update = process.hrtime.bigint()
   
   const users = await client.user.findUnique({
     where: {
       id,
     },
   })
-
-  const measure_find = process.hrtime.bigint()
   
   const deleteManyUsers = await client.user.deleteMany({})
 
-  const measure_delete_2 = process.hrtime.bigint()
+  const measure_end = process.hrtime.bigint()
   
   return {
     version: Prisma.prismaVersion.client,
@@ -58,15 +51,9 @@ export async function handler() {
     users,
     deleteManyUsers,
     measurements: {
-      client: Number(measure_client-measure_start) / 1000000000,
-      handler: Number(measure_handler-measure_client) / 1000000000,
-      delete_1: Number(measure_delete_1-measure_handler) / 1000000000,
-      create: Number(measure_create-measure_delete_1) / 1000000000,
-      update:Number( measure_update-measure_create) / 1000000000,
-      find: Number(measure_find-measure_update) / 1000000000,
-      delete_2: Number(measure_delete_2-measure_find) / 1000000000,
-      total_1: Number(measure_delete_2-measure_start) / 1000000000,
-      total_2: Number(measure_delete_2-measure_handler) / 1000000000
+      outside_handler: Number(measure_client-measure_start) / 1000000000,
+      inside_handler: Number(measure_end-measure_handler) / 1000000000,
+      total: Number(measure_end-measure_start) / 1000000000,
     }
   }
 }
