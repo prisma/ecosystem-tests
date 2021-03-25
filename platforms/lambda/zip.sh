922d9f290ee482b969d32374424720b38b5f556e
Jan Piotrowski <piotrowski+github@gmail.com>
2021-03-25 15:06:22 +0100

    Update zip.sh

--- platforms/lambda/zip.sh
+++ platforms/lambda/zip.sh
@@ -18,7 +18,7 @@ npx copyfiles index.js prisma/schema.prisma temp
 npx cpr node_modules/.prisma temp/node_modules/.prisma --overwrite
 npx cpr node_modules/@prisma temp/node_modules/@prisma --overwrite
 npx cpr node_modules/@types temp/node_modules/@types --overwrite
-npx --package=@janpio/cross-zip-cli@0.0.4 cross-zip temp ../lambda.zip
+npx --package=@janpio/cross-zip-cli@0.0.4 cross-zip temp lambda.zip
 
 
 du -b ./lambda.zip
