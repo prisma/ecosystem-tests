375672810c13170746db69dd9878ca77762413f3
Jan Piotrowski <piotrowski+github@gmail.com>
2021-03-25 17:39:49 +0100

    Update zip.sh

--- platforms/lambda/zip.sh
+++ platforms/lambda/zip.sh
@@ -11,9 +11,14 @@ rm -rf node_modules/prisma
 rm -rf node_modules/@prisma/engines
 rm -rf node_modules/typescript
 
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-  zip -r lambda.zip index.js prisma/schema.prisma node_modules/.prisma node_modules/**
-elif [[ "$OSTYPE" == "win"* ]]; then
+UNAME=$(uname)
+
+if [ "$UNAME" == "Linux" ] ; then
+	  zip -r lambda.zip index.js prisma/schema.prisma node_modules/.prisma node_modules/**
+
+elif [ "$UNAME" == "Darwin" ] ; then
+	echo "Darwin"
+elif [[ "$UNAME" == CYGWIN* || "$UNAME" == MINGW* || "$UNAME" == MSYS* ]] ; then
   rm -rf temp
   npx copyfiles index.js prisma/schema.prisma temp
   npx cpr node_modules/.prisma temp/node_modules/.prisma
@@ -21,6 +26,10 @@ elif [[ "$OSTYPE" == "win"* ]]; then
   npx cpr node_modules/@types temp/node_modules/@types
 
   powershell.exe -nologo -noprofile -command "& { param([String]$sourceDirectoryName, [String]$destinationArchiveFileName, [Boolean]$includeBaseDirectory); Add-Type -A 'System.IO.Compression.FileSystem'; Add-Type -A 'System.Text.Encoding'; [IO.Compression.ZipFile]::CreateFromDirectory($sourceDirectoryName, $destinationArchiveFileName, [IO.Compression.CompressionLevel]::Fastest, $includeBaseDirectory, [System.Text.Encoding]::UTF8); exit !$?;}" -sourceDirectoryName temp -destinationArchiveFileName lambda.zip -includeBaseDirectory $false
+fi
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+elif [[ "$OSTYPE" == "win"* ]]; then
 fi
 
 du -b ./lambda.zip
