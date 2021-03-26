e25178e6095b972f2764eddb35e808f48eaac665
Jan Piotrowski <piotrowski+github@gmail.com>
2021-03-26 19:30:47 +0100

    Update finally.sh

--- platforms/gcp-functions/finally.sh
+++ platforms/gcp-functions/finally.sh
@@ -6,5 +6,13 @@ sleep 30
 
 func="$(cat func-tmp.txt)"
 
+UNAME=$(uname)
+case "${UNAME}" in
+  Darwin*)
+    source "$(brew --prefix)/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.bash.inc"
+    source "$(brew --prefix)/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.bash.inc"
+    ;;
+esac
+
 gcloud functions logs read "$func"
 gcloud functions delete "$func" --quiet
