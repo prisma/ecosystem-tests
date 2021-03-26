cea8ddb3dbcc66734952e09eaafa8342a950bb7e
Jan Piotrowski <piotrowski+github@gmail.com>
2021-03-26 17:32:02 +0100

    install gcloud SDK on macOS only

--- platforms/gcp-functions/prepare.sh
+++ platforms/gcp-functions/prepare.sh
@@ -2,6 +2,13 @@
 
 set -eux
 
+# gcloud CLI is not installed on macOS image of GH Actions
+case "${UNAME}" in
+  Darwin*)
+    brew install --cask google-cloud-sdk
+    ;;
+esac
+
 printf "%s" "$GCP_FUNCTIONS_SECRET" > key.json
 gcloud auth activate-service-account --key-file key.json
 gcloud config set project "$GCP_FUNCTIONS_PROJECT"
