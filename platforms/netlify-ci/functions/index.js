d2de663a2bf78b9f83e9fa6079474a3f59ed889b
Jan Piotrowski <piotrowski+github@gmail.com>
2021-04-10 21:44:25 +0000

    logging of dir contents and return empty additional array

--- platforms/netlify-ci/functions/index.js
+++ platforms/netlify-ci/functions/index.js
@@ -33,6 +33,24 @@ exports.handler = async function (event, context, callback) {
 
   const deleteManyUsers = await client.user.deleteMany()
 
+
+  // node_modules\.prisma\client
+  console.log("process.env.LAMBDA_TASK_ROOT", process.env.LAMBDA_TASK_ROOT)
+  fs.readdir(process.env.LAMBDA_TASK_ROOT + "", function(err, items) {
+    console.log("# content of process.env.LAMBDA_TASK_ROOT")
+    console.log(items);
+  })
+  fs.readdir(process.env.LAMBDA_TASK_ROOT + "/src", function(err, items) {
+    console.log("# content of process.env.LAMBDA_TASK_ROOT/src")
+    console.log(items);
+  })
+  fs.readdir(process.env.LAMBDA_TASK_ROOT + "/src/functions", function(err, items) {
+    console.log("# content of process.env.LAMBDA_TASK_ROOT/src/functions")
+    console.log(items);
+  })
+
+  const files = []
+
   return {
     statusCode: 200,
     body: JSON.stringify({
@@ -41,6 +59,7 @@ exports.handler = async function (event, context, callback) {
       updateUser,
       users,
       deleteManyUsers,
+      files
     }),
     headers: {
       'Access-Control-Allow-Origin': '*',
