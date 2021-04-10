bb135db3e2e6e2f8f08626aa06d77f614d79764f
Jan Piotrowski <piotrowski+github@gmail.com>
2021-04-10 23:54:43 +0200

    chore: rename file to fetch-retry-and-confirm-version.ts for clarity (#1612)

--- platforms/netlify-cli/build.sh
+++ platforms/netlify-cli/build.sh
@@ -4,10 +4,13 @@ set -e
 
 rm -rf functions-build
 mkdir -p functions-build
+
 cp -R ./prisma ./functions/prisma
+
 cd functions/
 zip -r index.zip *
 mv index.zip ../functions-build/index.zip
 cd ..
+
 yarn netlify deploy --dir=. --prod --functions=functions-build
 rm -rf functions/prisma
