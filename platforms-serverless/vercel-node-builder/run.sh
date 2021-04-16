856b86c27f0b46020a9956d38147a30ff0af4083
Jan Piotrowski <piotrowski+github@gmail.com>
2021-04-16 20:58:15 +0000

    fix

--- platforms-serverless/vercel-node-builder/run.sh
+++ platforms-serverless/vercel-node-builder/run.sh
@@ -10,7 +10,8 @@ export VERCEL_ORG_ID=$VERCEL_NODE_BUILDER_ORG_ID
 echo "VERCEL_PROJECT_ID: $VERCEL_PROJECT_ID"
 echo "VERCEL_ORG_ID: $VERCEL_ORG_ID"
 
-yarn vercel --token=$VERCEL_TOKEN --prod --scope=prisma --confirm --force 1> deployment-url.txt
+yarn -s vercel --token=$VERCEL_TOKEN --prod --scope=prisma --confirm --force 1> deployment-url.txt
+echo ''
 cat deployment-url.txt
 DEPLOYED_URL=$( tail -n 1 deployment-url.txt )
 echo "Deployed to ${DEPLOYED_URL}"
