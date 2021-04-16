7f33cea85f5bcc52126b8617ef4e916977e4df65
Jan Piotrowski <piotrowski+github@gmail.com>
2021-04-16 20:50:31 +0000

    combine vercel run.sh

--- platforms-serverless/vercel-api/run.sh
+++ platforms-serverless/vercel-api/run.sh
@@ -10,7 +10,8 @@ export VERCEL_ORG_ID=$VERCEL_API_ORG_ID
 echo "VERCEL_PROJECT_ID: $VERCEL_PROJECT_ID"
 echo "VERCEL_ORG_ID: $VERCEL_ORG_ID"
 
-npx vercel --token=$VERCEL_TOKEN --prod --scope=prisma --confirm --force 1> deployment-url.txt
+yarn vercel --token=$VERCEL_TOKEN --prod --scope=prisma --confirm --force 1> deployment-url.txt
+cat deployment-url.txt
 DEPLOYED_URL=$( tail -n 1 deployment-url.txt )
 echo "Deployed to ${DEPLOYED_URL}"
 
