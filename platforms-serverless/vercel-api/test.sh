f68a790649cf743bdc90d943aa3351066285453b
Jan Piotrowski <piotrowski+github@gmail.com>
2021-04-16 20:21:50 +0000

    updated expected files for vercel-api

--- platforms-serverless/vercel-api/test.sh
+++ platforms-serverless/vercel-api/test.sh
@@ -2,4 +2,4 @@
 
 set -eu
 
-npx ts-node ../../utils/fetch-retry-and-confirm-version.ts --url https://e2e-vercel-api.vercel.app/api --prisma-version $(sh ../../utils/prisma_version.sh) --binary-string ',"files":["index-browser.js","index.d.ts","index.js","package.json","query-engine-rhel-openssl-1.0.x","schema.prisma"]'
+npx ts-node ../../utils/fetch-retry-and-confirm-version.ts --url https://e2e-vercel-api.vercel.app/api --prisma-version $(sh ../../utils/prisma_version.sh) --binary-string ',"files":["index.js","package.json","query-engine-rhel-openssl-1.0.x","schema.prisma"]'
