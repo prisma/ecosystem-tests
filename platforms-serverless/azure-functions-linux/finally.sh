26c31b426668ae84ff1456b35d7a67c824c127cd
Jan Piotrowski <piotrowski+github@gmail.com>
2021-04-26 21:29:30 +0000

    also clean up app insights

--- platforms-serverless/azure-functions-linux/finally.sh
+++ platforms-serverless/azure-functions-linux/finally.sh
@@ -7,3 +7,4 @@ app="$(cat func-tmp.txt)"
 group="prisma-e2e-linux"
 
 az functionapp delete --name "$app" --resource-group "$group"
+az monitor app-insights component delete --app "$app" --resource-group "$group"
--- platforms-serverless/azure-functions-windows/finally.sh
+++ platforms-serverless/azure-functions-windows/finally.sh
@@ -7,3 +7,4 @@ app="$(cat func-tmp.txt)"
 group="prisma-e2e-windows"
 
 az functionapp delete --name "$app" --resource-group "$group"
+az monitor app-insights component delete --app "$app" --resource-group "$group"
