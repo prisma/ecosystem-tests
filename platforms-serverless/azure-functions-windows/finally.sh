42ba1d5feb669d2fee67c75eca36d7e1151eedad
Jan Piotrowski <piotrowski+github@gmail.com>
2021-04-26 21:57:20 +0000

    allow auto install of app-insights extension

--- platforms-serverless/azure-functions-windows/finally.sh
+++ platforms-serverless/azure-functions-windows/finally.sh
@@ -7,4 +7,6 @@ app="$(cat func-tmp.txt)"
 group="prisma-e2e-windows"
 
 az functionapp delete --name "$app" --resource-group "$group"
+
+az config set extension.use_dynamic_install=yes_without_prompt
 az monitor app-insights component delete --app "$app" --resource-group "$group"
