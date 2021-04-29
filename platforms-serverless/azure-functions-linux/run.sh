da5da1c6960b39df621c84b3e6a0c22f7d9df211
Jan Piotrowski <piotrowski+github@gmail.com>
2021-04-29 16:16:59 +0200

    change order and remove sleep

--- platforms-serverless/azure-functions-linux/run.sh
+++ platforms-serverless/azure-functions-linux/run.sh
@@ -9,15 +9,16 @@ yarn tsc
 app="azure-function-linux-e2e-test-$(date "+%Y-%m-%d-%H%M%S")"
 echo "$app" > func-tmp.txt
 
+# TODO Do not copy, move it (otherwise we deploy both each time)
 cp -r "func-placeholder" "$app"
 
 group="prisma-e2e-linux"
 storage="prismae2elinuxstorage"
 
 az functionapp create --resource-group "$group" --consumption-plan-location westeurope --name "$app" --storage-account "$storage" --runtime "node" --os-type Linux
-sleep 60
-yarn func azure functionapp publish "$app" --force
 az functionapp config appsettings set --name "$app" --resource-group "$group" --settings "DEBUG=*"
 az functionapp config appsettings set --name "$app" --resource-group "$group" --settings "AZURE_FUNCTIONS_LINUX_PG_URL=$AZURE_FUNCTIONS_LINUX_PG_URL"
+#sleep 60
+yarn func azure functionapp publish "$app" --force
 
-sleep 30
+#sleep 30
