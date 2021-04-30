3b5cf72a2546333a0704b9abf77cb2f3d72dafeb
Jan Piotrowski <piotrowski+github@gmail.com>
2021-04-30 11:48:53 +0200

    fix(azure): improve function creation logic (#1673)

--- platforms-serverless/azure-functions-linux/run.sh
+++ platforms-serverless/azure-functions-linux/run.sh
@@ -9,15 +9,15 @@ yarn tsc
 app="azure-function-linux-e2e-test-$(date "+%Y-%m-%d-%H%M%S")"
 echo "$app" > func-tmp.txt
 
-cp -r "func-placeholder" "$app"
+# give function folder our new app name
+mv "func-placeholder" "$app"
 
 group="prisma-e2e-linux"
 storage="prismae2elinuxstorage"
 
 az functionapp create --resource-group "$group" --consumption-plan-location westeurope --name "$app" --storage-account "$storage" --runtime "node" --os-type Linux
-sleep 60
-yarn func azure functionapp publish "$app" --force
 az functionapp config appsettings set --name "$app" --resource-group "$group" --settings "DEBUG=*"
 az functionapp config appsettings set --name "$app" --resource-group "$group" --settings "AZURE_FUNCTIONS_LINUX_PG_URL=$AZURE_FUNCTIONS_LINUX_PG_URL"
 
 sleep 30
+yarn func azure functionapp publish "$app" --force
