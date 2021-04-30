d8fc0efc6948bb1776b5bf3a137b3643aefd5bfe
Jan Piotrowski <piotrowski+github@gmail.com>
2021-04-30 21:41:29 +0000

    other param

--- databases/bedrock/prepare.sh
+++ databases/bedrock/prepare.sh
@@ -16,6 +16,6 @@ sudo apt-get install bedrock
 #bedrock -fork -db prisma/bedrock.db
 
 cd prisma 
-bedrock -controlPort localhost:3000 -nodeHost localhost:3001 &
+bedrock -serverHost localhost:3000 -nodeHost localhost:3001 &
 
 cat /var/log/syslog
