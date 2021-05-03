8742175697f4b3183031cc6f4d9b3c779bc84e12
William Luke <william@ordino.ai>
2021-05-03 17:05:42 +0300

    fix serverless-lambda test

--- platforms-serverless/serverless-framework-lambda/test.ts
+++ platforms-serverless/serverless-framework-lambda/test.ts
@@ -1,4 +1,3 @@
-import { Prisma } from '@prisma/client'
 import { deepEqual, invokeLambdaSync } from './utils'
 
 const name = 'e2e-tests-serverless-lambda' // TODO rename to match project name again in AWS account
@@ -9,12 +8,28 @@ async function main() {
   console.log({ data: data.$response.data })
 
   const actual = (data.$response.data as any).Payload
-  const binaryName = process.env.PRISMA_FORCE_NAPI === 'true' ? 'libquery_engine_napi-rhel-openssl-1.0.x.so.node' : 'query-engine-rhel-openssl-1.0.x'
-  const binaryString = `,"files":["index-browser.js","index.d.ts","index.js","package.json","${binaryName}","schema.prisma"]`
+  const binaryName =
+    process.env.PRISMA_FORCE_NAPI === 'true'
+      ? 'libquery_engine_napi-rhel-openssl-1.0.x.so.node'
+      : 'query-engine-rhel-openssl-1.0.x'
 
-  const expect = `{"version":"${Prisma.prismaVersion.client}","createUser":{"id":"12345","email":"alice@prisma.io","name":"Alice"},"updateUser":{"id":"12345","email":"bob@prisma.io","name":"Bob"},"users":{"id":"12345","email":"bob@prisma.io","name":"Bob"},"deleteManyUsers":{"count":1}${binaryString}}`
+  const expect = {
+    version: '${Prisma.prismaVersion.client}',
+    createUser: { id: '12345', email: 'alice@prisma.io', name: 'Alice' },
+    updateUser: { id: '12345', email: 'bob@prisma.io', name: 'Bob' },
+    users: { id: '12345', email: 'bob@prisma.io', name: 'Bob' },
+    deleteManyUsers: { count: 1 },
+    files: [
+      'index-browser.js',
+      'index.d.ts',
+      'index.js',
+      'package.json',
+      binaryName,
+      'schema.prisma',
+    ],
+  }
 
-  if (deepEqual(actual, expect)) {
+  if (deepEqual(JSON.parse(actual), expect)) {
     console.log('Expected:\n ', expect, '\nBut got:\n', actual)
     process.exit(1)
   }
