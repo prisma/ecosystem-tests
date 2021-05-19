29211e298342158c741e53e6061025b8f8306389
Jan Piotrowski <piotrowski+github@gmail.com>
2021-05-19 21:28:54 +0000

    build first before running

--- platforms-serverless/vercel-with-nextjs-and-nexus-plugin-prisma/run.sh
+++ platforms-serverless/vercel-with-nextjs-and-nexus-plugin-prisma/run.sh
@@ -5,6 +5,8 @@ set -eu
 export PRISMA_TELEMETRY_INFORMATION='e2e-tests platforms-serverless vercel-with-nextjs-and-nexus-plugin-prisma build'
 yarn
 
+yarn build
+
 export VERCEL_PROJECT_ID=$VERCEL_WITH_NEXTJS_AND_NEXUS_PLUGIN_PRISMA_PROJECT_ID
 export VERCEL_ORG_ID=$VERCEL_WITH_NEXTJS_AND_NEXUS_PLUGIN_PRISMA_ORG_ID
 echo "VERCEL_PROJECT_ID: $VERCEL_PROJECT_ID"
