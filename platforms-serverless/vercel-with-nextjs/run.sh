c0948d387a3c8b31a15c52e03c0f35e3b26fa2cb
Jan Piotrowski <piotrowski+github@gmail.com>
2021-05-19 20:18:59 +0000

    formatting

--- platforms-serverless/vercel-with-nextjs/run.sh
+++ platforms-serverless/vercel-with-nextjs/run.sh
@@ -2,11 +2,13 @@
 
 set -eu
 
-export PRISMA_TELEMETRY_INFORMATION='e2e-tests platforms vercel-with-nextjs build'
+export PRISMA_TELEMETRY_INFORMATION='e2e-tests platforms-serverless vercel-with-nextjs build'
 yarn
+
 export VERCEL_PROJECT_ID=$VERCEL_WITH_NEXTJS_PROJECT_ID
 export VERCEL_ORG_ID=$VERCEL_WITH_NEXTJS_ORG_ID
 echo "VERCEL_PROJECT_ID: $VERCEL_PROJECT_ID"
 echo "VERCEL_ORG_ID: $VERCEL_ORG_ID"
 yarn vercel --token=$VERCEL_TOKEN --prod --scope=prisma --confirm
+
 sleep 15
