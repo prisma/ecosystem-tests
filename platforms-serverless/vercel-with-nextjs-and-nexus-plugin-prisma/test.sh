51fc10c485825c2a129d508d25f9405ce1785b20
Jan Piotrowski <piotrowski+github@gmail.com>
2021-05-19 21:43:08 +0000

    fix url

--- platforms-serverless/vercel-with-nextjs-and-nexus-plugin-prisma/test.sh
+++ platforms-serverless/vercel-with-nextjs-and-nexus-plugin-prisma/test.sh
@@ -6,7 +6,7 @@ pid=$!
 
 sleep 10
 
-actual="$(curl 'https://e2e-vercel-with-nextjs-and-nexus-plugin-prisma.vercel.app/api/graphql/' -H 'Accept-Encoding: gzip, deflate, br' -H 'Content-Type: application/json' -H 'Accept: application/json' -H 'Connection: keep-alive' -H 'DNT: 1' -H 'Origin: https://e2e-vercel-with-nextjs-and-nexus-plugin-prisma.vercel.app' --data-binary '{"query":"query {\n  users {\n    id, name\n  }\n}"}' --compressed)"
+actual="$(curl 'https://e2e-vercel-with-nextjs-and-nexus-plugin-prisma.vercel.app/api/graphql' -H 'Accept-Encoding: gzip, deflate, br' -H 'Content-Type: application/json' -H 'Accept: application/json' -H 'Connection: keep-alive' -H 'DNT: 1' -H 'Origin: https://e2e-vercel-with-nextjs-and-nexus-plugin-prisma.vercel.app' --data-binary '{"query":"query {\n  users {\n    id, name\n  }\n}"}' --compressed)"
 
 expected='{"data":{"users":{"result":"[{\"id\":7,\"email\":\"john@example.com\",\"name\":\"John Doe\"}]"}}}'
 
