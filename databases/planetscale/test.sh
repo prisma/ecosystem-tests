f2fedf57283b20739731cd6c2eb46382fa091238
Jan Piotrowski <piotrowski+github@gmail.com>
2021-05-23 00:29:54 +0200

    Update test.sh

--- databases/planetscale/test.sh
+++ databases/planetscale/test.sh
@@ -2,7 +2,9 @@
 
 set -eu
 
-pscale connect e2e-tests main --execute-protocol 'mysql' --execute 'yarn test' #<-- Does not work because of "Query engine exited with code 1"
+pscale connect e2e-tests main --execute-protocol 'mysql' --debug --execute 'test-wrapper.sh' 
+
+#<-- Does not work because of "Query engine exited with code 1"
 
 #pscale connect e2e-tests main &
 #pid=$!
