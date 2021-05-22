2ed8134238791ea992e1ebe7a019a080b3f16f33
Jan Piotrowski <piotrowski+github@gmail.com>
2021-05-23 01:57:39 +0200

    Update test.sh

--- databases/planetscale/test.sh
+++ databases/planetscale/test.sh
@@ -2,7 +2,4 @@
 
 set -eu
 
-pscale connect e2e-tests main --execute-protocol 'mysql' --debug --execute './test-wrapper.sh' 
-
-# have to exit 0 manually, as `pscale` above is killed from the inside
-exit 0
+pscale connect e2e-tests main --execute-protocol 'mysql' --debug --execute './test-wrapper.sh'
