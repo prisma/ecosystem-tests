b1ec7b3d19bfe079ec6c1906944c027364453ef3
Jan Piotrowski <piotrowski+github@gmail.com>
2021-05-22 23:13:57 +0000

    exit 0

--- databases/planetscale/test.sh
+++ databases/planetscale/test.sh
@@ -4,13 +4,5 @@ set -eu
 
 pscale connect e2e-tests main --execute-protocol 'mysql' --debug --execute './test-wrapper.sh' 
 
-#<-- Does not work because of "Query engine exited with code 1"
-
-#pscale connect e2e-tests main &
-#pid=$!
-
-#sleep 5
-
-#yarn test
-
-#kill $pid
+# have to exit 0 manually, as `pscale` above is killed from the inside
+exit 0
