abd0eb6433fb1c85dc6a7bbc9f528f4eda7168af
Jan Piotrowski <piotrowski+github@gmail.com>
2021-05-25 21:46:40 +0000

    use correct package to install

--- platforms-serverless/lambda/zip.sh
+++ platforms-serverless/lambda/zip.sh
@@ -11,6 +11,6 @@ rm -rf node_modules/prisma
 rm -rf node_modules/@prisma/engines
 rm -rf node_modules/typescript
 
-zip -r lambda.zip index.js prisma/schema.prisma node_modules/.prisma node_modules/** pscale_0.40.0_linux_amd64.deb
+zip -r lambda.zip index.js prisma/schema.prisma node_modules/.prisma node_modules/** pscale_0.40.0_linux_amd64.rpm
 
 du -b ./lambda.zip
