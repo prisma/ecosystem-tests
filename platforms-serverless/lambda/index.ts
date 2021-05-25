2674d349fd240b32834c3165496809928f20ef0f
Jan Piotrowski <piotrowski+github@gmail.com>
2021-05-25 21:37:39 +0000

    include CLI in archive

--- platforms-serverless/lambda/index.ts
+++ platforms-serverless/lambda/index.ts
@@ -12,7 +12,6 @@ function wait(ms: number) {
 }
 
 try {
-  execa.sync('wget', ['https://github.com/planetscale/cli/releases/download/v0.40.0/pscale_0.40.0_linux_amd64.deb'])
   execa.sync('sudo', ['dpkg -i pscale_0.40.0_linux_amd64.deb'])
 
 	execa.sync('pscale', ['connect', 'e2e-tests', 'main'], { detached: true })
