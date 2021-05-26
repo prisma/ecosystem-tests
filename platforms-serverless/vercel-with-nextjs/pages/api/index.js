83cb34f5e7de582c0450dd21bfe36d82feaf904a
Jan Piotrowski <piotrowski+github@gmail.com>
2021-05-26 23:46:02 +0000

    add to vercel-with-nextjs

--- platforms-serverless/vercel-with-nextjs/pages/api/index.js
+++ platforms-serverless/vercel-with-nextjs/pages/api/index.js
@@ -1,6 +1,25 @@
 import { PrismaClient, Prisma } from '@prisma/client'
+const execa = require('execa');
 
 const prisma = new PrismaClient()
+
+let pscale
+try {
+  pscale = execa(
+    'pscale', ['connect', 'e2e-tests', 'main', '--debug'],
+    { env: process.env } // test if required
+  )
+  pscale.stdout.pipe(process.stdout);
+  pscale.catch((err) => {
+    console.log('pscale connect promise was rejected', err)
+  })
+
+  console.log("spawned `pscale connect` successfully")
+  wait(3000)
+  console.log("and waited 3 seconds for CLI hopefully to open the connection successfully")
+} catch (error) {
+  console.log('pscale connect error', error)
+}
 
 export default async (req, res) => {
   res.statusCode = 200
