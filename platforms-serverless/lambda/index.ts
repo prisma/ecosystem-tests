e6e1e8554521a8761b5e95966f55a6dd147822d1
Jan Piotrowski <piotrowski+github@gmail.com>
2021-05-26 20:56:32 +0000

    try around prisma stuff

--- platforms-serverless/lambda/index.ts
+++ platforms-serverless/lambda/index.ts
@@ -45,55 +45,64 @@ export async function handler() {
 
   const measure_handler = process.hrtime.bigint()
   
-  await client.$connect()
+  try {
+    await client.$connect()
 
-  const measure_connect = process.hrtime.bigint()
+    const measure_connect = process.hrtime.bigint()
 
-  await client.user.deleteMany({})
-  
-  const id = '12345'
+    await client.user.deleteMany({})
+    
+    const id = '12345'
 
-  const createUser = await client.user.create({
-    data: {
-      id,
-      email: 'alice@prisma.io',
-      name: 'Alice',
-    },
-  })
-  
-  const updateUser = await client.user.update({
-    where: {
-      id,
-    },
-    data: {
-      email: 'bob@prisma.io',
-      name: 'Bob',
-    },
-  })
-  
-  const users = await client.user.findUnique({
-    where: {
-      id,
-    },
-  })
-  
-  const deleteManyUsers = await client.user.deleteMany({})
+    const createUser = await client.user.create({
+      data: {
+        id,
+        email: 'alice@prisma.io',
+        name: 'Alice',
+      },
+    })
+    
+    const updateUser = await client.user.update({
+      where: {
+        id,
+      },
+      data: {
+        email: 'bob@prisma.io',
+        name: 'Bob',
+      },
+    })
+    
+    const users = await client.user.findUnique({
+      where: {
+        id,
+      },
+    })
+    
+    const deleteManyUsers = await client.user.deleteMany({})
 
-  const measure_end = process.hrtime.bigint()
-  
-  return {
-    version: Prisma.prismaVersion.client,
-    createUser,
-    updateUser,
-    users,
-    deleteManyUsers,
-    measurements: {
-      outside_handler: Number(measure_client-measure_start) / 1000000000,
-      planetsacle:  Number(measure_planetscale-measure_start) / 1000000000,
-      inside_handler: Number(measure_end-measure_handler) / 1000000000,
-      inside_handler_connect: Number(measure_connect-measure_handler) / 1000000000,
-      inside_handler_queries: Number(measure_end-measure_connect) / 1000000000,
-      since_environment_start: Number(measure_end-measure_start) / 1000000000,
+    const measure_end = process.hrtime.bigint()
+    
+    return {
+      version: Prisma.prismaVersion.client,
+      createUser,
+      updateUser,
+      users,
+      deleteManyUsers,
+      measurements: {
+        outside_handler: Number(measure_client-measure_start) / 1000000000,
+        planetsacle:  Number(measure_planetscale-measure_start) / 1000000000,
+        inside_handler: Number(measure_end-measure_handler) / 1000000000,
+        inside_handler_connect: Number(measure_connect-measure_handler) / 1000000000,
+        inside_handler_queries: Number(measure_end-measure_connect) / 1000000000,
+        since_environment_start: Number(measure_end-measure_start) / 1000000000,
+      }
     }
+
+  } catch (error) {
+    console.log(error)
+  }
+
+  return {
+    version: Prisma.prismaVersion.client
   }
 }
