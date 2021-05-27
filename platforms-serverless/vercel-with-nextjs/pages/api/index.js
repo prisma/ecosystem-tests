9f420758b41d488ff6ef1b4db17ae1b8e81cdfc1
Jan Piotrowski <piotrowski+github@gmail.com>
2021-05-27 00:57:19 +0000

    inside the handler

--- platforms-serverless/vercel-with-nextjs/pages/api/index.js
+++ platforms-serverless/vercel-with-nextjs/pages/api/index.js
@@ -11,25 +11,28 @@ function wait(ms) {
 
 const prisma = new PrismaClient()
 
-let pscale
-try {
-  pscale = execa(
-    'pscale', ['connect', 'e2e-tests', 'main', '--debug'],
-    { env: process.env } // test if required
-  )
-  pscale.stdout.pipe(process.stdout);
-  pscale.catch((err) => {
-    console.log('pscale connect promise was rejected', err)
-  })
 
-  console.log("spawned `pscale connect` successfully")
-  wait(3000)
-  console.log("and waited 3 seconds for CLI hopefully to open the connection successfully")
-} catch (error) {
-  console.log('pscale connect error', error)
-}
 
 export default async (req, res) => {
+
+  let pscale
+  try {
+    pscale = execa(
+      'pscale', ['connect', 'e2e-tests', 'main', '--debug'],
+      { env: process.env } // test if required
+    )
+    pscale.stdout.pipe(process.stdout);
+    pscale.catch((err) => {
+      console.log('pscale connect promise was rejected', err)
+    })
+
+    console.log("spawned `pscale connect` successfully")
+    wait(3000)
+    console.log("and waited 3 seconds for CLI hopefully to open the connection successfully")
+  } catch (error) {
+    console.log('pscale connect error', error)
+  }
+
   res.statusCode = 200
   res.setHeader('Content-Type', 'application/json')
   const users = await prisma.user.findMany()
