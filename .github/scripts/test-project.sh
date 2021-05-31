cab535fa4344220cdee36cd35710372451ee88a2
Jan Piotrowski <piotrowski+github@gmail.com>
2021-05-31 10:56:39 +0200

    Update test-project.sh

--- .github/scripts/test-project.sh
+++ .github/scripts/test-project.sh
@@ -110,6 +110,8 @@ if [ $code -eq 0 ]; then
   fi
 fi
 
+// TODO parse output of npx prisma -v --json for correct file/path
+
 if [ -f "finally.sh" ]; then
   echo "-----------------------------"
   echo ""
