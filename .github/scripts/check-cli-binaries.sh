b16648dbff8e4312cb50cb25aa761ea8e648287e
William Luke <william@ordino.ai>
2021-06-01 14:12:12 +0300

    switch if to case

--- .github/scripts/check-cli-binaries.sh
+++ .github/scripts/check-cli-binaries.sh
@@ -12,44 +12,52 @@ case $(uname | tr '[:upper:]' '[:lower:]') in
     OS_NAME=notset
     ;;
 esac
-
 echo "Using $OS_NAME"
+
 if [ -z ${PRISMA_FORCE_NAPI+x} ]; then
   # binary
   echo "N-API Disabled"
-  if [ $OS_NAME=='windows' ]; then
-  QE_LOCATION="node_modules\@prisma\engines\query-engine-windows.exe"
-  QE_LOCATION2="node_modules\prisma\node_modules\engines\query-engine-windows.exe"
-
-  fi
-  if [ $OS_NAME=='osx' ]; then
-  QE_LOCATION="node_modules/@prisma/engines/query-engine-darwin"
-  QE_LOCATION2="node_modules/prisma/node_modules/@prisma/engines/query-engine-darwin"
-
-  fi
-  if [ $OS_NAME=='linux' ]; then
-  QE_LOCATION="node_modules/@prisma/engines/query-engine-debian-openssl-1.1.x"
-  QE_LOCATION2="node_modules/prisma/node_modules/@prisma/engines/query-engine-debian-openssl-1.1.x"
-
-  fi
+  case $OS_NAME in
+    linux)
+      QE_LOCATION="node_modules/@prisma/engines/query-engine-debian-openssl-1.1.x"
+      QE_LOCATION2="node_modules/prisma/node_modules/@prisma/engines/query-engine-debian-openssl-1.1.x"
+      ;;
+    osx)
+      QE_LOCATION="node_modules/@prisma/engines/query-engine-darwin"
+      QE_LOCATION2="node_modules/prisma/node_modules/@prisma/engines/query-engine-darwin"
+      ;;
+    windows)
+      QE_LOCATION="node_modules\@prisma\engines\query-engine-windows.exe"
+      QE_LOCATION2="node_modules\prisma\node_modules\engines\query-engine-windows.exe"
+      ;;
+    *)
+      OS_NAME=notset
+      ;;
+  esac
 else
   # library
+
   echo "N-API Enabled"
-  if [ $OS_NAME=='windows' ]; then
-  QE_LOCATION="node_modules\@prisma\engines\query_engine_napi-windows.dll.node"
-  QE_LOCATION2="node_modules\prisma\node_modules\engines\query_engine_napi-windows.dll.node"
-
-  fi
-  if [ $OS_NAME=='osx' ]; then
-  QE_LOCATION="node_modules/@prisma/engines/libquery_engine_napi-darwin.dylib.node"
-  QE_LOCATION2="node_modules/prisma/node_modules/@prisma/engines/libquery_engine_napi-darwin.dylib.node"
-
-  fi
-  if [ $OS_NAME=='linux' ]; then
-  QE_LOCATION="node_modules/@prisma/engines/libquery_engine_napi-debian-openssl-1.1.x.so.node"
-  QE_LOCATION2="node_modules/prisma/node_modules/@prisma/engines/libquery_engine_napi-debian-openssl-1.1.x.so.node"
-
-  fi
+  case $OS_NAME in
+    linux)
+      echo "Linux"
+      QE_LOCATION="node_modules/@prisma/engines/libquery_engine_napi-debian-openssl-1.1.x.so.node"
+      QE_LOCATION2="node_modules/prisma/node_modules/@prisma/engines/libquery_engine_napi-debian-openssl-1.1.x.so.node"
+      ;;
+    osx)
+      echo "Osx"
+      QE_LOCATION="node_modules/@prisma/engines/libquery_engine_napi-darwin.dylib.node"
+      QE_LOCATION2="node_modules/prisma/node_modules/@prisma/engines/libquery_engine_napi-darwin.dylib.node"
+      ;;
+    windows*)
+      echo "Windows"
+      QE_LOCATION="node_modules\@prisma\engines\query_engine_napi-windows.dll.node"
+      QE_LOCATION2="node_modules\prisma\node_modules\engines\query_engine_napi-windows.dll.node"
+      ;;
+    *)
+      OS_NAME=notset
+      ;;
+  esac
 fi
 
 echo "---"
