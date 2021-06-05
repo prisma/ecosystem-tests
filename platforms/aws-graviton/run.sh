447c0b26431cc42ecff5196ef73af14689547764
William Luke <william@atto-byte.com>
2021-06-05 13:31:12 +0300

    chore: remove napi exclusions (#1745)
    Co-authored-by: Jan Piotrowski <piotrowski+github@gmail.com>

--- platforms/aws-graviton/run.sh
+++ platforms/aws-graviton/run.sh
@@ -4,8 +4,21 @@ set -eu
 
 sh sync-to-remote.sh
 
-ssh -i ./server-key.pem ec2-user@54.72.209.131 -tt "
-    cd /home/ec2-user/aws-graviton/; 
-    yarn;
-    yarn prisma generate;
-"
+if [ -z ${PRISMA_FORCE_NAPI+x} ]; then
+  echo "N-API:  Disabled"
+  ssh -i ./server-key.pem ec2-user@54.72.209.131 -tt "
+      cd /home/ec2-user/aws-graviton/;
+      yarn;
+      yarn prisma generate;
+  "
+else
+  echo "N-API: Enabled"
+  ssh -i ./server-key.pem ec2-user@54.72.209.131 -tt "
+      cd /home/ec2-user/aws-graviton/;
+      export PRISMA_FORCE_NAPI=\"true\";
+      yarn;
+      yarn prisma generate;
+  "
+fi
+
+
