d370480d8704a5375d8930ae2e4e4be748baf0f9
Jan Piotrowski <piotrowski+github@gmail.com>
2021-06-06 10:33:47 +0000

    empty commit to make PR possible

--- platforms/heroku/test.sh
+++ platforms/heroku/test.sh
@@ -4,3 +4,4 @@ set -eu
 
 npx ts-node ../../utils/fetch-retry-and-confirm-version.ts --url https://e2e-platforms-heroku.herokuapp.com --prisma-version $(sh ../../utils/prisma_version.sh)
 # TODO Add binary string to script call so it confirms engine files
+
