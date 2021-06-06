8325b14340f1e07b5c0fbdf972e5486b7151bd43
Jan Piotrowski <piotrowski+github@gmail.com>
2021-06-06 10:56:04 +0000

    more comments where binary check should go

--- platforms-serverless/firebase-functions/test.sh
+++ platforms-serverless/firebase-functions/test.sh
@@ -8,6 +8,7 @@ url="https://us-central1-prisma-e2e-tests-265911.cloudfunctions.net/$func"
 prisma_version="$(cat ../../.github/prisma-version.txt)"
 
 expected='{"version":"'$prisma_version'","createUser":{"id":"12345","email":"alice@prisma.io","name":"Alice"},"updateUser":{"id":"12345","email":"bob@prisma.io","name":"Bob"},"users":{"id":"12345","email":"bob@prisma.io","name":"Bob"},"deleteManyUsers":{"count":1}}'
+# TODO Add engineString to check for engine files
 actual=$(curl "$url")
 
 firebase functions:log --only "$func"
--- platforms-serverless/gcp-functions/test.sh
+++ platforms-serverless/gcp-functions/test.sh
@@ -8,6 +8,7 @@ prisma_version="$(cat ../../.github/prisma-version.txt)"
 url="https://us-central1-prisma-e2e-tests-265911.cloudfunctions.net/$func"
 
 expected='{"version":"'$prisma_version'","createUser":{"id":"12345","email":"alice@prisma.io","name":"Alice"},"updateUser":{"id":"12345","email":"bob@prisma.io","name":"Bob"},"users":{"id":"12345","email":"bob@prisma.io","name":"Bob"},"deleteManyUsers":{"count":1}}'
+# TODO Add engineString to check for engine files
 actual=$(curl "$url")
 
 if [ "$expected" != "$actual" ]; then
