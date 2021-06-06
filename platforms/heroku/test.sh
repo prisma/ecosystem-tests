9e6af8b728ff54d644402ef2f4aaba6a550eedd6
Jan Piotrowski <piotrowski+github@gmail.com>
2021-06-06 12:31:40 +0200

    Revert "platforms: add comments where engine file checking should be"
    This reverts commit 35b599091793342d32be147b8ec5bc65a6d2de69.

--- platforms/heroku/test.sh
+++ platforms/heroku/test.sh
@@ -3,4 +3,3 @@
 set -eu
 
 npx ts-node ../../utils/fetch-retry-and-confirm-version.ts --url https://e2e-platforms-heroku.herokuapp.com --prisma-version $(sh ../../utils/prisma_version.sh)
-# TODO Add binary string to script call so it confirms engine files
