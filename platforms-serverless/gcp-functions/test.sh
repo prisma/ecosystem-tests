97663edfd6e6c3756795107da92b66780e1393c1
William Luke <william@ordino.ai>
2021-06-08 15:25:15 +0300

    gcp

--- platforms-serverless/gcp-functions/test.sh
+++ platforms-serverless/gcp-functions/test.sh
@@ -6,8 +6,14 @@ func="$(cat func-tmp.txt)"
 prisma_version="$(cat ../../.github/prisma-version.txt)"
 
 url="https://us-central1-prisma-e2e-tests-265911.cloudfunctions.net/$func"
+# checks whether PRISMA_FORCE_NAPI has length equal to zero
+if [[ -z "${PRISMA_FORCE_NAPI+x}" ]]; then
+  files=',"files":["index-browser.js","index.d.ts","index.js","package.json","query-engine-debian-openssl-1.1.x","schema.prisma"]'
+else
+  files=',"files":["index-browser.js","index.d.ts","index.js","package.json","libquery_engine_napi-debian-openssl-1.1.x.so.node","schema.prisma"]'
+fi
 
-expected='{"version":"'$prisma_version'","createUser":{"id":"12345","email":"alice@prisma.io","name":"Alice"},"updateUser":{"id":"12345","email":"bob@prisma.io","name":"Bob"},"users":{"id":"12345","email":"bob@prisma.io","name":"Bob"},"deleteManyUsers":{"count":1}}'
+expected='{"version":"'$prisma_version'","createUser":{"id":"12345","email":"alice@prisma.io","name":"Alice"},"updateUser":{"id":"12345","email":"bob@prisma.io","name":"Bob"},"users":{"id":"12345","email":"bob@prisma.io","name":"Bob"},"deleteManyUsers":{"count":1}'${files}'}'
 actual=$(curl "$url")
 
 if [ "$expected" != "$actual" ]; then
