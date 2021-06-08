b170738f102c1bba13649f36365cf84f444500ae
William Luke <william@ordino.ai>
2021-06-08 16:03:33 +0300

    fix heroku

--- platforms/heroku/test.sh
+++ platforms/heroku/test.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
 set -eu
-
-npx ts-node ../../utils/fetch-retry-and-confirm-version.ts --url https://e2e-platforms-heroku.herokuapp.com --prisma-version $(sh ../../utils/prisma_version.sh)
+if [[ -z "${PRISMA_FORCE_NAPI+x}" ]]; then
+  files=',"files":["index-browser.js","index.d.ts","index.js","package.json","query-engine-debian-openssl-1.1.x","schema.prisma"]'
+else
+  files=',"files":["index-browser.js","index.d.ts","index.js","package.json","libquery_engine_napi-debian-openssl-1.1.x.so.node","schema.prisma"]'
+fi
+npx ts-node ../../utils/fetch-retry-and-confirm-version.ts --url https://e2e-platforms-heroku.herokuapp.com --prisma-version $(sh ../../utils/prisma_version.sh) --binary-string $files
