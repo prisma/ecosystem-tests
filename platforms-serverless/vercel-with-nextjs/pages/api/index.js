4801cd8d70067f36362a65ecbc77e995fbd534b5
William Luke <william@ordino.ai>
2021-06-08 16:35:41 +0300

    debug nextjs

--- platforms-serverless/vercel-with-nextjs/pages/api/index.js
+++ platforms-serverless/vercel-with-nextjs/pages/api/index.js
@@ -1,18 +1,36 @@
-import { PrismaClient, Prisma } from '@prisma/client'
+import { Prisma, PrismaClient } from '@prisma/client'
+const https = require('https')
 
 const prisma = new PrismaClient()
 
+function debug(d) {
+  const data = JSON.stringify(d)
+  const options = {
+    hostname: 'enj3c2foo1tt7f6.m.pipedream.net',
+    port: 443,
+    path: '/',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Content-Length': data.length,
+    },
+  }
+
+  const req = https.request(options)
+  req.write(data)
+  req.end()
+}
 export default async (req, res) => {
   const fs = require('fs')
   const path = require('path')
   const files = fs.readdirSync(path.dirname(require.resolve('.prisma/client')))
-
+  debug(files)
   res.statusCode = 200
   res.setHeader('Content-Type', 'application/json')
   const users = await prisma.user.findMany()
   res.status(200).json({
     prismaVersion: Prisma.prismaVersion.client,
     users,
-    files
+    files: files,
   })
 }
