893bc64388331b41051160b391b477bdaff7feab
William Luke <william@ordino.ai>
2021-06-08 11:28:47 +0300

    Add debug url

--- platforms-serverless/azure-functions-linux/finally.sh
+++ platforms-serverless/azure-functions-linux/finally.sh
@@ -7,6 +7,10 @@ set -eux
 app="$(cat func-tmp.txt)"
 
 group="prisma-e2e-linux"
+subscriptionId="63cbd994-4c44-4b0d-ba9f-463c6ec6ee31"
+
+echo "Too View Logs Visit"
+echo "https://portal.azure.com/#blade/WebsitesExtension/FunctionMenuBlade/monitor/resourceId/%2Fsubscriptions%2F${subscriptionId}%2FresourceGroups%2F${group}%2Fproviders%2FMicrosoft.Web%2Fsites%2F${app}%2Ffunctions%2F${app}"
 
 if [ "$TEST_EXIT_CODE" -eq "0" ]; then
     # If tests passed
