49738396ade5edee41d6e0594ead40832bcba958
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2021-06-09 15:09:54 -0500

    fix(packagers, npm-global): eaccess errors

--- packagers/npm-global/run.sh
+++ packagers/npm-global/run.sh
@@ -6,6 +6,11 @@ prisma_version="$(cat ../../.github/prisma-version.txt)"
 
 # explicitly ignore the yarn lockfile here, npm will install the latest packages
 # from the lockfile which will already ensure the correct prisma version gets fetched
+
+mkdir ~/.npm-global
+npm config set prefix '~/.npm-global'
+NPM_CONFIG_PREFIX=~/.npm-global
+
 npm install
 npm install -g prisma@${prisma_version}
 prisma generate
