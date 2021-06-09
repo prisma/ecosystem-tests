06ee28c554c96857cb5d7928396bbb01f535ac85
William Luke <william@ordino.ai>
2021-06-09 15:42:58 +0300

    fix files for vercel-cli

--- platforms-serverless/vercel-cli/test.sh
+++ platforms-serverless/vercel-cli/test.sh
@@ -2,5 +2,11 @@
 
 set -eu
 
+# checks whether PRISMA_FORCE_NAPI has length equal to zero
+if [[ -z "${PRISMA_FORCE_NAPI+x}" ]]; then
+  files=',"files":["index.js","package.json","query-engine-rhel-openssl-1.0.x","schema.prisma"]'
+else
+  files=',"files":["index.js","libquery_engine_napi-rhel-openssl-1.0.x.so.node","package.json","schema.prisma"]'
+fi
 
-npx ts-node ../../utils/fetch-retry-and-confirm-version.ts --url https://e2e-vercel-api.vercel.app/api --prisma-version $(sh ../../utils/prisma_version.sh) --binary-string ',"files":["index.js","package.json","query-engine-rhel-openssl-1.0.x","schema.prisma"]'
+npx ts-node ../../utils/fetch-retry-and-confirm-version.ts --url https://e2e-vercel-api.vercel.app/api --prisma-version $(sh ../../utils/prisma_version.sh) --binary-string $files
