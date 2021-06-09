458510947cbcf4c17c6eff47dd9965c6cd0fe991
William Luke <william@ordino.ai>
2021-06-09 09:45:45 +0300

    return error

--- platforms-serverless/vercel-with-nextjs/pages/api/index.js
+++ platforms-serverless/vercel-with-nextjs/pages/api/index.js
@@ -1,30 +1,16 @@
 import { Prisma, PrismaClient } from '@prisma/client'
-const https = require('https')
 
 const prisma = new PrismaClient()
 
-function debug(d) {
-  const data = JSON.stringify(d)
-  const options = {
-    hostname: 'enj3c2foo1tt7f6.m.pipedream.net',
-    port: 443,
-    path: '/',
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Content-Length': data.length,
-    },
-  }
-
-  const req = https.request(options)
-  req.write(data)
-  req.end()
-}
 export default async (req, res) => {
   const fs = require('fs')
   const path = require('path')
-  const files = fs.readdirSync(path.dirname(require.resolve('.prisma/client')))
-  debug(files)
+  let files;
+  try {
+    files = fs.readdirSync(path.dirname(require.resolve('.prisma/client')))
+  } catch (e) {
+    files = e.message
+  }
   res.statusCode = 200
   res.setHeader('Content-Type', 'application/json')
   const users = await prisma.user.findMany()
