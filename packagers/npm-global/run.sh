fbad93fe9a5bab09f7891dd5bd24a217c05c7b5d
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2021-06-09 16:38:48 -0500

    chore(packagers, npm-global): add prepare script

--- packagers/npm-global/run.sh
+++ packagers/npm-global/run.sh
@@ -6,12 +6,6 @@ prisma_version="$(cat ../../.github/prisma-version.txt)"
 
 # explicitly ignore the yarn lockfile here, npm will install the latest packages
 # from the lockfile which will already ensure the correct prisma version gets fetched
-
-mkdir -p ~/.npm-global
-npm config set prefix '~/.npm-global'
-NPM_CONFIG_PREFIX=~/.npm-global
-PATH=~/.npm-global/bin:$PATH
-
 npm install
 npm install -g prisma@${prisma_version}
 prisma generate
