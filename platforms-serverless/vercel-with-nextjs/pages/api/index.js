44e806f2eec840fe85550c38d03317777f57a318
William Luke <william@ordino.ai>
2021-06-09 11:01:13 +0300

    remove nextjs check

--- platforms-serverless/vercel-with-nextjs/pages/api/index.js
+++ platforms-serverless/vercel-with-nextjs/pages/api/index.js
@@ -1,24 +1,13 @@
 import { Prisma, PrismaClient } from '@prisma/client'
-import execa from 'execa'
 
 const prisma = new PrismaClient()
 
 export default async (req, res) => {
-  const fs = require('fs')
-  const path = require('path')
-  let files
-  const { stdout } = await execa('tree')
-  try {
-    files = fs.readdirSync(path.dirname(require.resolve('.prisma/client')))
-  } catch (e) {
-    files = e.message + '\n' + stdout
-  }
   res.statusCode = 200
   res.setHeader('Content-Type', 'application/json')
   const users = await prisma.user.findMany()
   res.status(200).json({
     prismaVersion: Prisma.prismaVersion.client,
     users,
-    files: files,
   })
 }
