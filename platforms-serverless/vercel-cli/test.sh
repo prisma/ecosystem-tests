ed1f163ec0307aab6d5ab4922e5eaf1103cade55
William Luke <william@ordino.ai>
2021-06-09 14:59:41 +0300

    revert vercel url change for vercel-cli

--- platforms-serverless/vercel-cli/test.sh
+++ platforms-serverless/vercel-cli/test.sh
@@ -2,6 +2,5 @@
 
 set -eu
 
-DEPLOYED_URL=$( tail -n 1 deployment-url.txt )
 
-npx ts-node ../../utils/fetch-retry-and-confirm-version.ts --url $DEPLOYED_URL --prisma-version $(sh ../../utils/prisma_version.sh) --binary-string ',"files":["index.js","package.json","query-engine-rhel-openssl-1.0.x","schema.prisma"]'
+npx ts-node ../../utils/fetch-retry-and-confirm-version.ts --url https://e2e-vercel-api.vercel.app/api --prisma-version $(sh ../../utils/prisma_version.sh) --binary-string ',"files":["index.js","package.json","query-engine-rhel-openssl-1.0.x","schema.prisma"]'
