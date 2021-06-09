3c2dee5e654a257336d9bd0edb5d3e5373d04086
William Luke <william@ordino.ai>
2021-06-09 13:03:47 +0300

    vercel test using deployed url

--- platforms-serverless/vercel-cli/test.sh
+++ platforms-serverless/vercel-cli/test.sh
@@ -2,4 +2,6 @@
 
 set -eu
 
-npx ts-node ../../utils/fetch-retry-and-confirm-version.ts --url https://e2e-vercel-api.vercel.app/api --prisma-version $(sh ../../utils/prisma_version.sh) --binary-string ',"files":["index.js","package.json","query-engine-rhel-openssl-1.0.x","schema.prisma"]'
+DEPLOYED_URL=$( tail -n 1 deployment-url.txt )
+
+npx ts-node ../../utils/fetch-retry-and-confirm-version.ts --url $DEPLOYED_URL --prisma-version $(sh ../../utils/prisma_version.sh) --binary-string ',"files":["index.js","package.json","query-engine-rhel-openssl-1.0.x","schema.prisma"]'
--- platforms-serverless/vercel-node-builder/test.sh
+++ platforms-serverless/vercel-node-builder/test.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 set -eu
-
-npx ts-node ../../utils/fetch-retry-and-confirm-version.ts --url https://e2e-vercel-node-builder.prisma.vercel.app/ --prisma-version $(sh ../../utils/prisma_version.sh) --binary-string ',"files":["index.js","package.json","query-engine-rhel-openssl-1.0.x","schema.prisma"]'
+DEPLOYED_URL=$( tail -n 1 deployment-url.txt )
+npx ts-node ../../utils/fetch-retry-and-confirm-version.ts --url $DEPLOYED_URL --prisma-version $(sh ../../utils/prisma_version.sh) --binary-string ',"files":["index.js","package.json","query-engine-rhel-openssl-1.0.x","schema.prisma"]'
