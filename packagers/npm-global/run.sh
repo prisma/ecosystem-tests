f3bde12e5f353786f170412a7fa99b5c0a6bf509
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2021-06-09 15:21:13 -0500

    fix(packagers, npm-global): soft directory creation

--- packagers/npm-global/run.sh
+++ packagers/npm-global/run.sh
@@ -7,7 +7,7 @@ prisma_version="$(cat ../../.github/prisma-version.txt)"
 # explicitly ignore the yarn lockfile here, npm will install the latest packages
 # from the lockfile which will already ensure the correct prisma version gets fetched
 
-mkdir ~/.npm-global
+mkdir -p ~/.npm-global
 npm config set prefix '~/.npm-global'
 NPM_CONFIG_PREFIX=~/.npm-global
 
