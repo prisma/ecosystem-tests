ee580f8b2977aa30629c12a7835f4370ed860988
Jan Piotrowski <piotrowski+github@gmail.com>
2021-06-10 23:40:42 +0200

    Update test.sh

--- platforms-serverless/netlify-ci/test.sh
+++ platforms-serverless/netlify-ci/test.sh
@@ -2,7 +2,6 @@
 
 set -eu
 
-# Note: THIS ALREADY USES THE binary string, so we can copy this for other tests of the same pattern!
-# TODO This is not dynamic based on Napi env var, so should technically not even work... wtf?
+# TODO This is not dynamic based on Napi env var, so should not even work... wtf?
 npx ts-node ../../utils/fetch-retry-and-confirm-version.ts --url https://prisma2-e2e-tests-netlify-ci.netlify.app/.netlify/functions/index --prisma-version $(sh ../../utils/prisma_version.sh) --binary-string ',"files":["index-browser.js","index.d.ts","index.js","package.json","query-engine-rhel-openssl-1.0.x","schema.prisma"]'
 
