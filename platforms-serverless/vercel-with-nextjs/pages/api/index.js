986a2b543e3f28ebf7dcb6fc7bb45f6cae66ccc5
William Luke <william@ordino.ai>
2021-06-10 10:49:45 +0300

    return files for vercel-with-nextjs

--- platforms-serverless/vercel-with-nextjs/pages/api/index.js
+++ platforms-serverless/vercel-with-nextjs/pages/api/index.js
@@ -6,8 +6,18 @@ export default async (req, res) => {
   res.statusCode = 200
   res.setHeader('Content-Type', 'application/json')
   const users = await prisma.user.findMany()
+  const fs = require('fs')
+  let files
+  try {
+    files = fs.readdirSync(
+      process.env.LAMBDA_TASK_ROOT + '/node_modules/.prisma/client',
+    )
+  } catch (e) {
+    files = e.message
+  }
   res.status(200).json({
     prismaVersion: Prisma.prismaVersion.client,
     users,
+    files
   })
 }
