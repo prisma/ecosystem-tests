17e30d1853b8ba03acff9c784f2e42b3cb617355
Jan Piotrowski <piotrowski+github@gmail.com>
2021-06-12 22:08:32 +0000

    remove from local binary checker scripts

--- .github/scripts/check-cli-binaries.sh
+++ .github/scripts/check-cli-binaries.sh
@@ -4,7 +4,7 @@ PROJECT=$2
 
 # These are skipping because they have different project structures
 # TODO Adapt tests so they also work here, or adapt project to fit into the mold
-skipped_projects=(aws-graviton firebase-functions)
+skipped_projects=(aws-graviton firebase-functions m1-macstadium)
 
 case "${skipped_projects[@]}" in  *$2*)
   echo "Skipping as $2 is present in skipped_projects"
