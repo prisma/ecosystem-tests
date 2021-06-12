6c4f6d50685429727fc9c99cfe707ed21972fc2b
Jan Piotrowski <piotrowski+github@gmail.com>
2021-06-13 00:17:59 +0200

    Restore print-version.sh

--- .github/scripts/print-version.sh
+++ .github/scripts/print-version.sh
@@ -10,7 +10,6 @@ echo ""
 
 echo "node: $(node -v)"
 echo "npm: $(npm -v)"
-echo "uname -m: $(uname -m)"
 
 echo "prisma-version.txt: $(cat .github/prisma-version.txt)"
 echo "prisma (package.json): $(jq .devDependencies[\"prisma\"] < $packageJSONDir)"
