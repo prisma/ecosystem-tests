f1c61e16cb20b1b40a7f2fc5e556384b22b2645e
Jan Piotrowski <piotrowski+github@gmail.com>
2021-06-12 21:18:21 +0000

    workaround ssh weirdness

--- platforms/m1-macstadium/run.sh
+++ platforms/m1-macstadium/run.sh
@@ -8,6 +8,10 @@ if [ -z ${PRISMA_FORCE_NAPI+x} ]; then
   echo "N-API: Disabled"
   ssh -i ./server-key.pem administrator@207.254.29.83 -tt "
       cd /Users/administrator/e2e-tests;
+      
+      # to get around https://serverfault.com/questions/351731/why-does-the-path-of-an-ssh-remote-command-differ-from-that-of-an-interactive-s
+      export PATH=/opt/homebrew/bin:/opt/homebrew/sbin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Users/administrator/.cargo/bin 
+      
       yarn;
       yarn prisma generate;
   "
@@ -16,6 +20,10 @@ else
   ssh -i ./server-key.pem administrator@207.254.29.83 -tt "
       cd /Users/administrator/e2e-tests;
       export PRISMA_FORCE_NAPI=\"true\";
+
+      # to get around https://serverfault.com/questions/351731/why-does-the-path-of-an-ssh-remote-command-differ-from-that-of-an-interactive-s
+      export PATH=/opt/homebrew/bin:/opt/homebrew/sbin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Users/administrator/.cargo/bin 
+
       yarn;
       yarn prisma generate;
   "
