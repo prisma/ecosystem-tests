f5a149af7583328dfd76be10b39e0c059c4c22bd
Jan Piotrowski <piotrowski+github@gmail.com>
2021-06-12 21:34:26 +0000

    fix remote folder

--- platforms/m1-macstadium/sync-to-remote.sh
+++ platforms/m1-macstadium/sync-to-remote.sh
@@ -7,10 +7,10 @@ echo "Sync: Syncing code to EC2"
 echo "Sync: Removing local node_modules"
 rm -rf ./code/node_modules
 
-echo "Sync: Making sure remote folder does not exist"
-ssh -tt -i ./server-key.pem administrator@207.254.29.83 'rm -rf /Users/administrator/e2e-tests/$GITHUB_JOB'
+echo "Sync: Create remote folder"
+ssh -tt -i ./server-key.pem administrator@207.254.29.83 'mkdir /Users/administrator/e2e-tests/$GITHUB_JOB/$GITHUB_RUN_ID'
 
 echo "Sync: Copying new code to unique folder"
-scp -i ./server-key.pem -rp ./code administrator@207.254.29.83:/Users/administrator/e2e-tests/$GITHUB_JOB
+scp -i ./server-key.pem -rp ./code administrator@207.254.29.83:/Users/administrator/e2e-tests/$GITHUB_JOB/$GITHUB_RUN_ID
 
 echo "sync-to-remote done"
--- platforms/m1-macstadium/test.sh
+++ platforms/m1-macstadium/test.sh
@@ -3,6 +3,6 @@
 set -eux
 
 ssh -i ./server-key.pem administrator@207.254.29.83 -tt "
-    cd /Users/administrator/e2e-tests/$GITHUB_JOB; 
+    cd /Users/administrator/e2e-tests/$GITHUB_JOB/$GITHUB_RUN_ID; 
     yarn m1;
 "
