f842547d4cc51052fc185b61e9540b368aa0a6f7
Jan Piotrowski <piotrowski+github@gmail.com>
2021-06-12 21:40:24 +0000

    recursive folder creation

--- platforms/m1-macstadium/sync-to-remote.sh
+++ platforms/m1-macstadium/sync-to-remote.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-set -eu
+set -eux
 
 echo "Sync: Syncing code to EC2"
 
@@ -8,7 +8,7 @@ echo "Sync: Removing local node_modules"
 rm -rf ./code/node_modules
 
 echo "Sync: Create remote folder"
-ssh -tt -i ./server-key.pem administrator@207.254.29.83 'mkdir /Users/administrator/e2e-tests/$GITHUB_JOB/$GITHUB_RUN_ID'
+ssh -tt -i ./server-key.pem administrator@207.254.29.83 'mkdir -p /Users/administrator/e2e-tests/$GITHUB_JOB/$GITHUB_RUN_ID'
 
 echo "Sync: Copying new code to unique folder"
 scp -i ./server-key.pem -rp ./code administrator@207.254.29.83:/Users/administrator/e2e-tests/$GITHUB_JOB/$GITHUB_RUN_ID
