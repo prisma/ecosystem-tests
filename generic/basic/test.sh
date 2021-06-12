810b6668e4c22ffaad301178c5f11a3504386fbc
Jan Piotrowski <piotrowski+github@gmail.com>
2021-06-13 00:18:41 +0200

    Restore test.sh

--- generic/basic/test.sh
+++ generic/basic/test.sh
@@ -1,15 +1,5 @@
 #!/bin/sh
 
-set -eux
+set -eu
 
-yarn -s m1
-arch -arm64e yarn -s m1
-yarn -s m1-arm64
-arch -arm64e yarn -s m1-arm64
-node ./m1.js
-arch -arm64e node ./m1.js
-
-yarn -s cmd
-
-uname -a
-arch -arm64e uname -a
+yarn cmd
