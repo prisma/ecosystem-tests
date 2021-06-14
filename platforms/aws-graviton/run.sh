94a8ccfc3c3b1d9398eae3c9cf6f4c1324d675df
Jan Piotrowski <piotrowski+github@gmail.com>
2021-06-14 20:43:43 +0200

    fix(aws-graviton): Make tests more resilient by using individual folder per run (#1800)

--- platforms/aws-graviton/run.sh
+++ platforms/aws-graviton/run.sh
@@ -1,23 +1,25 @@
 #!/bin/sh
 
-set -eu
+set -eux
 
 sh sync-to-remote.sh
 
 if [ -z ${PRISMA_FORCE_NAPI+x} ]; then
-  echo "N-API:  Disabled"
+  echo "N-API: Disabled"
   ssh -i ./server-key.pem ec2-user@54.72.209.131 -tt "
-      cd /home/ec2-user/aws-graviton/;
+      cd /home/ec2-user/aws-graviton/$GITHUB_JOB/$GITHUB_RUN_ID;
       yarn;
       yarn prisma generate;
+      yarn prisma -v;
   "
 else
   echo "N-API: Enabled"
   ssh -i ./server-key.pem ec2-user@54.72.209.131 -tt "
-      cd /home/ec2-user/aws-graviton/;
+      cd /home/ec2-user/aws-graviton/$GITHUB_JOB/$GITHUB_RUN_ID;
       export PRISMA_FORCE_NAPI=\"true\";
       yarn;
       yarn prisma generate;
+      yarn prisma -v;
   "
 fi
 
