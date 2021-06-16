a4b70777fa4e91eb2e235af99dd3ebf307963a2f
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2021-06-15 20:31:35 -0500

    chore(serverless, redwood): upgrade project to standard

--- platforms-serverless/vercel-with-redwood/api/src/functions/graphql.js
+++ platforms-serverless/vercel-with-redwood/api/src/functions/graphql.js
@@ -3,15 +3,19 @@ import {
   makeMergedSchema,
   makeServices,
 } from '@redwoodjs/api'
-import schemas from 'src/graphql/**/*.{js,ts}'
-import services from 'src/services/**/*.{js,ts}'
 
+import schemas from 'src/graphql/**/*.{js,ts}'
 import { db } from 'src/lib/db'
+import services from 'src/services/**/*.{js,ts}'
 
 export const handler = createGraphQLHandler({
   schema: makeMergedSchema({
     schemas,
     services: makeServices({ services }),
   }),
-  db,
+
+  onException: () => {
+    // Disconnect from your database with an unhandled exception.
+    db.$disconnect()
+  },
 })
