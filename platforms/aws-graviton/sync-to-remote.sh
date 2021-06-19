008755572d65749948160d9aa955905e458de0f5
Jan Piotrowski <piotrowski+github@gmail.com>
2021-06-19 11:30:07 +0000

    clean up and comment some shell scripts

--- platforms/aws-graviton/sync-to-remote.sh
+++ platforms/aws-graviton/sync-to-remote.sh
@@ -7,8 +7,8 @@ echo "Sync: Syncing code to EC2"
 echo "Sync: Removing local node_modules"
 rm -rf ./code/node_modules
 
-echo "Sync: Removing existing code"
+echo "Sync: Removing existing remote ode"
 ssh -tt -i ./server-key.pem ec2-user@54.72.209.131 'rm -rf /home/ec2-user/aws-graviton'
 
-echo "Sync: Copying new code"
+echo "Sync: Copying new local code to remote"
 scp -i ./server-key.pem -rp ./code ec2-user@54.72.209.131:/home/ec2-user/aws-graviton/
