acaf6c054fbfcfdfd049db81f3d43f6f87eb77c8
Jan Piotrowski <piotrowski+github@gmail.com>
2021-06-19 20:41:12 +0000

    protect config:set from unset variable

--- platforms-serverless/firebase-functions/run.sh
+++ platforms-serverless/firebase-functions/run.sh
@@ -8,6 +8,10 @@ echo "$func" > func-tmp.txt
 cd functions/ && sh prepare_in_project.sh "$func" && cd ..
 
 # Use project config to transmit env vars, that are set as env vars in index.ts of project
-firebase functions:config:set prisma.db="$FIREBASE_FUNCTIONS_PG_URL" prisma.napi="$PRISMA_FORCE_NAPI"
+if [[ -z "${PRISMA_FORCE_NAPI+x}" ]]; then
+  firebase functions:config:set prisma.db="$FIREBASE_FUNCTIONS_PG_URL"
+else
+  firebase functions:config:set prisma.db="$FIREBASE_FUNCTIONS_PG_URL" prisma.napi="$PRISMA_FORCE_NAPI"
+fi
 
 firebase deploy --token "$FIREBASE_TOKEN" --only "functions:$func"
