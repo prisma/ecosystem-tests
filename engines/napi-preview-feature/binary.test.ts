f8cf529a813488d682edfdc2679bf4db3d2c7bb1
Jan Piotrowski <piotrowski+github@gmail.com>
2021-06-20 21:59:53 +0200

    binary tests pass

--- engines/napi-preview-feature/binary.test.ts
+++ engines/napi-preview-feature/binary.test.ts
@@ -1,31 +1,33 @@
+import path from 'path';
 import { runTest } from './utils'
 const os = require('os');
+const OS_BINARY = ((os.type() == 'Windows_NT') ? 'query-engine-windows.exe' : 'query-engine')
 
 //describe('Binary (on ' + os.type() + ')', () => {
 describe('Binary (on WindowsNT)', () => {
 
-  test('no options', async () => {
+  test('no options, uses default binary', async () => {
     const options = {}
     await runTest(options)
-  }, 100000)
+  })
 
-  test('PRISMA_QUERY_ENGINE_BINARY uses supplied binary', async () => {
+  test('PRISMA_QUERY_ENGINE_BINARY, uses supplied binary', async () => {
     const options = {
       env: {
-        PRISMA_QUERY_ENGINE_BINARY: 'foo.query-engine'
+        // Using absolute path because of https://github.com/prisma/prisma/issues/7779
+        PRISMA_QUERY_ENGINE_BINARY: path.resolve('.', 'custom-engines', os.type(), OS_BINARY)
       },
     }
     await runTest(options)
-  }, 100000)
+  })
 
-  test('PRISMA_QUERY_ENGINE_LIBRARY (!) uses default binary', async () => {
-    // should just normally use binary
+  test('PRISMA_QUERY_ENGINE_LIBRARY (!), uses default binary (and ignores env var)', async () => {
     const options = {
       env: {
         PRISMA_QUERY_ENGINE_LIBRARY: 'foo.node'
       },
     }
     await runTest(options)
-  }, 100000)
+  })
 
 })
