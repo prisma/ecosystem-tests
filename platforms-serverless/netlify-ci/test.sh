54004cd16a814c25acdaea0e31be744280e1a23b
Jan Piotrowski <piotrowski+github@gmail.com>
2021-06-22 16:33:43 +0000

    actually fix netlify

--- platforms-serverless/netlify-ci/test.sh
+++ platforms-serverless/netlify-ci/test.sh
@@ -3,9 +3,9 @@
 set -eu
 
 if [[ -z "${PRISMA_FORCE_NAPI+x}" ]]; then
-  files=',"files":["index-browser.js","index.d.ts","index.js","package.json","query-engine-debian-openssl-1.1.x","schema.prisma"]'
+  files=',"files":["index-browser.js","index.d.ts","index.js","package.json","query-engine-rhel-openssl-1.0.x","schema.prisma"]'
 else
-  files=',"files":["index-browser.js","index.d.ts","index.js","package.json","libquery_engine_napi-debian-openssl-1.1.x.so.node","schema.prisma"]'
+  files=',"files":["index-browser.js","index.d.ts","index.js","package.json","libquery_engine_napi-rhel-openssl-1.0.x.so.node","schema.prisma"]'
 fi
 
 npx ts-node ../../utils/fetch-retry-and-confirm-version.ts --url https://prisma2-e2e-tests-netlify-ci.netlify.app/.netlify/functions/index --prisma-version $(sh ../../utils/prisma_version.sh) --binary-string $files
