3246e2afbd77040559d7288f7c01e21a88288b44
Jan Piotrowski <piotrowski+github@gmail.com>
2021-06-22 16:05:39 +0000

    test netlify-ci for engine files

--- platforms-serverless/netlify-ci/test.sh
+++ platforms-serverless/netlify-ci/test.sh
@@ -2,6 +2,10 @@
 
 set -eu
 
-# TODO This is not dynamic based on Napi env var, so should not even work... wtf?
-npx ts-node ../../utils/fetch-retry-and-confirm-version.ts --url https://prisma2-e2e-tests-netlify-ci.netlify.app/.netlify/functions/index --prisma-version $(sh ../../utils/prisma_version.sh) --binary-string ',"files":["index-browser.js","index.d.ts","index.js","package.json","query-engine-rhel-openssl-1.0.x","schema.prisma"]'
+if [[ -z "${PRISMA_FORCE_NAPI+x}" ]]; then
+  files=',"files":["index-browser.js","index.d.ts","index.js","package.json","query-engine-debian-openssl-1.1.x","schema.prisma"]'
+else
+  files=',"files":["index-browser.js","index.d.ts","index.js","package.json","libquery_engine_napi-debian-openssl-1.1.x.so.node","schema.prisma"]'
+fi
 
+npx ts-node ../../utils/fetch-retry-and-confirm-version.ts --url https://prisma2-e2e-tests-netlify-ci.netlify.app/.netlify/functions/index --prisma-version $(sh ../../utils/prisma_version.sh) --binary-string $files
