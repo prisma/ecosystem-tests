56c446e056f92af4a97860e6c751fb6759c5e0fe
Jan Piotrowski <piotrowski+github@gmail.com>
2021-06-24 21:55:39 +0000

    fix

--- core-features/generate-client-and-cli-install/run.sh
+++ core-features/generate-client-and-cli-install/run.sh
@@ -6,6 +6,7 @@ VERSION=$(cat ../../.github/prisma-version.txt)
 
 # no install, instead rename file
 #yarn install
+mv package.json package.json.backup
 
 # global instead instead
 npm install -g prisma@$VERSION
