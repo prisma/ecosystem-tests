546630d98ee285304f50234211fda9ae6ae4a080
Jan Piotrowski <piotrowski+github@gmail.com>
2021-06-24 17:23:10 +0000

    aws graviton

--- platforms/aws-graviton/code/index.test.js
+++ platforms/aws-graviton/code/index.test.js
@@ -2,23 +2,37 @@ const { PrismaClient, Prisma } = require('@prisma/client')
 
 const prisma = new PrismaClient()
 
-describe('Prisma version', () => {
+describe('Prisma', () => {
   afterAll(() => {
     prisma.$disconnect()
   })
 
-  it('should test Prisma version', () => {
+  it('should return Prisma version', () => {
     const pjson = require('./package.json')
     expect(Prisma.prismaVersion.client).toBe(
       pjson.dependencies['@prisma/client'],
     )
   })
 
-  it('should query the database', async () => {
+  it('should be able to query the database', async () => {
     const data = await prisma.user.findMany()
     expect(data).toMatchObject([])
   })
 
-  // TODO Check for engine files depending on env var
-  
+  it('should use the correct engine files', async () => {
+    const fs = require('fs')
+    const path = require('path')
+    const files = fs.readdirSync(path.dirname(require.resolve('.prisma/client')))
+    expect(files).toMatchInlineSnapshot(`
+Array [
+  "index-browser.js",
+  "index.d.ts",
+  "index.js",
+  "package.json",
+  "query-engine-debian-openssl-1.1.x",
+  "schema.prisma",
+]
+`)
+  })
+
 })
