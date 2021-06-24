99c96a1d8cfc918d1780e10f459f07b28ab00f06
Jan Piotrowski <piotrowski+github@gmail.com>
2021-06-24 17:33:48 +0000

    jest-with-multiple-generators

--- test-runners/jest-with-multiple-generators/src/__tests__/the.test.ts
+++ test-runners/jest-with-multiple-generators/src/__tests__/the.test.ts
@@ -1,8 +1,61 @@
-import { PrismaClient as PC } from '@prisma/client'
-import { PrismaClient as PCA } from '../../generated/database/client'
+import {
+  PrismaClient as PCA,
+  Prisma as PA,
+} from '../../generated/database/client'
+import {
+  PrismaClient as PCB,
+  Prisma as PB,
+} from '../../generated/database/client2'
 
-it('should work', () => {
-  expect(1).toBe(1)
+const prismaA = new PCA()
+const prismaB = new PCB()
+
+describe('Prisma in jest with multiple generators', () => {
+  it('should work', () => {
+    expect(1).toBe(1)
+  })
+
+  it('should return Prisma version', () => {
+    const pjson = require('../../package.json')
+    expect(PA.prismaVersion.client).toBe(pjson.dependencies['@prisma/client'])
+    expect(PB.prismaVersion.client).toBe(pjson.dependencies['@prisma/client'])
+  })
+
+  it('should be able to query the database', async () => {
+    const data = await prismaA.user.findMany()
+    expect(data).toMatchObject([])
+    const dataB = await prismaB.user.findMany()
+    expect(dataB).toMatchObject([])
+  })
+
+  it('should use the correct engine files', async () => {
+    const fs = require('fs')
+    const path = require('path')
+    const filesA = fs.readdirSync(
+      path.dirname(require.resolve('../../generated/database/client')),
+    )
+    const filesB = fs.readdirSync(
+      path.dirname(require.resolve('../../generated/database/client2')),
+    )
+    expect(filesA).toMatchInlineSnapshot(`
+      Array [
+        "index-browser.js",
+        "index.d.ts",
+        "index.js",
+        "query-engine-debian-openssl-1.1.x",
+        "runtime",
+        "schema.prisma",
+      ]
+    `)
+    expect(filesB).toMatchInlineSnapshot(`
+      Array [
+        "index-browser.js",
+        "index.d.ts",
+        "index.js",
+        "query-engine-debian-openssl-1.1.x",
+        "runtime",
+        "schema.prisma",
+      ]
+    `)
+  })
 })
-
-// TODO Test for correct engine file here as well
