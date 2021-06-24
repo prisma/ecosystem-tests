c8fd9f0be1011f16b587fd3094cf1952f2ac6676
Jan Piotrowski <piotrowski+github@gmail.com>
2021-06-24 17:34:33 +0200

    Update test.sh

--- migrate/db-seed/test.sh
+++ migrate/db-seed/test.sh
@@ -2,6 +2,9 @@
 
 set -eux
 
+ID=$( tail -n 1 id.txt )
+export CORE_FEATURES_DB_SEED_URL="$CORE_FEATURES_DB_SEED_URL$ID"
+
 yarn prisma db pull --print
 yarn prisma db seed --preview-feature
 yarn test
