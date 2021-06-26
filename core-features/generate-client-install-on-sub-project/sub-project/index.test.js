20abe30dc2d4209846224f89324efb0d2066ebfb
Jan Piotrowski <piotrowski+github@gmail.com>
2021-06-26 10:08:45 +0200

    feat: Install Client and CLI on generate with both (and package.json) missing (#1844)
    Co-authored-by: Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>

--- core-features/generate-client-install-on-sub-project/sub-project/index.test.js
+++ core-features/generate-client-install-on-sub-project/sub-project/index.test.js
@@ -1,5 +1,5 @@
 describe('prisma generate', () => {
-  it('adds @prisma/client to package.json', async () => {
+  it('adds @prisma/client to correct package.json', async () => {
     const pkgjson = require('./package.json')
     expect(pkgjson.dependencies["@prisma/client"]).toBeTruthy()
   })
