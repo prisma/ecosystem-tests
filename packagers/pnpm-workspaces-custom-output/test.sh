b0fece3085d4ba01396acd6f431da151300808e1
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2021-06-29 23:53:18 -0500

    chore: cleanup

--- packagers/pnpm-workspaces-custom-output/test.sh
+++ packagers/pnpm-workspaces-custom-output/test.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+FILE1=sub-project-1/node_modules/.prisma/client/index.js
+FILE2=sub-project-2/node_modules/.prisma/client/index.js
+
 set -eux
+
+if [ ! -f "$FILE1" ] || [ ! -f "$FILE2" ]; then
+    echo "Client did not generate"; exit 1;
+fi;
 
 pnpm -r run cmd
--- packagers/pnpm-workspaces-standard-output/test.sh
+++ packagers/pnpm-workspaces-standard-output/test.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
+FILE1=sub-project-1/node_modules/.prisma/client/index.js
+
 set -eux
 
-test -f node_modules/.prisma/client/index.js
+if [ ! -f "$FILE1" ]; then
+    echo "Client did not generate"; exit 1;
+fi;
 
 pnpm -r run cmd
