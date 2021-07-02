161a77e7af4264a3ad0e6966fc532092c248c7bf
Jan Piotrowski <piotrowski+github@gmail.com>
2021-07-02 22:01:01 +0000

    db push

--- platforms-serverless/azure-functions-linux/run.sh
+++ platforms-serverless/azure-functions-linux/run.sh
@@ -9,6 +9,9 @@ yarn install
 yarn prisma generate
 yarn tsc
 
+# make sure database exists
+npx prisma db push --force-reset --accept-data-loss
+
 app="azure-function-linux-e2e-test-$(date "+%Y-%m-%d-%H%M%S")"
 echo "$app" > func-tmp.txt
 
@@ -20,7 +23,7 @@ storage="prismae2elinuxstorage"
 
 az functionapp create --resource-group "$group" --consumption-plan-location westeurope --name "$app" --storage-account "$storage" --runtime "node" --os-type Linux
 az functionapp config appsettings set --name "$app" --resource-group "$group" --settings "DEBUG=*"
-az functionapp config appsettings set --name "$app" --resource-group "$group" --settings "AZURE_FUNCTIONS_LINUX_PG_URL=$AZURE_FUNCTIONS_LINUX_PG_URL"
+az functionapp config appsettings set --name "$app" --resource-group "$group" --settings "DATABASE_URL=$DATABASE_URL"
 
 sleep 30
 yarn func azure functionapp publish "$app" --force
