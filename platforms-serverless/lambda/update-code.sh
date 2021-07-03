397bd34568eed8853a594be62f259a4b3b188829
Jan Piotrowski <piotrowski+github@gmail.com>
2021-07-03 00:35:28 +0000

    fix some platforms-serverless

--- platforms-serverless/lambda/update-code.sh
+++ platforms-serverless/lambda/update-code.sh
@@ -4,7 +4,7 @@ set -eux
 
 sh zip.sh
 
-aws lambda update-function-configuration --function-name prisma2-e2e-tests --environment "Variables={LAMBDA_PG_URL=$LAMBDA_PG_URL}" --timeout 10
+aws lambda update-function-configuration --function-name prisma2-e2e-tests --environment "Variables={DATABASE_URL=$DATABASE_URL}" --timeout 10
 
 aws lambda update-function-code --function-name prisma2-e2e-tests --zip-file "fileb://lambda.zip"
 yarn install
