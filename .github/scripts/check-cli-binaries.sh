60f3b664ca7b66e63904c266528bf3e2a3f3ef58
Jan Piotrowski <piotrowski+github@gmail.com>
2021-07-08 00:50:23 +0200

    feat(platform/m1-macstadium): Run tests on M1 Mac (#1798)

--- .github/scripts/check-cli-binaries.sh
+++ .github/scripts/check-cli-binaries.sh
@@ -9,6 +9,7 @@ PROJECT=$2
 skipped_projects=(
   aws-graviton        # No local project at all (everything happens on server), so no `prisma` or `node_modules
   firebase-functions  # No local project at expected location (but in `functions` subfolder)
+  m1-macstadium       # No local project at all (everything happens on server), so no `prisma` or `node_modules
 )
 
 case "${skipped_projects[@]}" in  *$2*)
