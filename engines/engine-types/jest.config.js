90ac306e44c0b710a1ed7b956068958fe1a2de75
William Luke <william@ordino.ai>
2021-08-02 18:20:44 +0300

    update snapshots

--- engines/engine-types/jest.config.js
+++ engines/engine-types/jest.config.js
@@ -2,5 +2,6 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   snapshotResolver: './jestSnapshotResolver.js',
-  testTimeout: 100000
-};
+  testTimeout: 100000,
+  modulePathIgnorePatterns: ['custom-engines'],
+}
