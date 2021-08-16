fb84105c2e0686d71afa78ec9d8fce6b39063f47
William Luke <william@atto-byte.com>
2021-08-16 09:24:48 +0100

    fix(aws-graviton): rm node_modules before install (#2002)

--- platforms/aws-graviton/run.sh
+++ platforms/aws-graviton/run.sh
@@ -9,6 +9,7 @@ if [ "$PRISMA_CLIENT_ENGINE_TYPE" == "binary" ]; then
   ssh -i ./server-key.pem ec2-user@54.72.209.131 -tt "
       cd /home/ec2-user/aws-graviton/$GITHUB_JOB/$GITHUB_RUN_ID;
       export PRISMA_CLIENT_ENGINE_TYPE=\"binary\"
+      rm -rf ./node_modules;
       yarn;
       yarn prisma generate;
       yarn prisma -v;
@@ -18,6 +19,7 @@ else
   ssh -i ./server-key.pem ec2-user@54.72.209.131 -tt "
       cd /home/ec2-user/aws-graviton/$GITHUB_JOB/$GITHUB_RUN_ID;
       export PRISMA_CLIENT_ENGINE_TYPE=\"library\"
+      rm -rf ./node_modules;
       yarn;
       yarn prisma generate;
       yarn prisma -v;
