7f5d8f369f760faf774efbcfd7a970ba318ff30d
Joël Galeran <Jolg42@users.noreply.github.com>
2021-09-22 16:52:03 +0200

    Update run.sh

--- packagers/yarn2-without-pnp/run.sh
+++ packagers/yarn2-without-pnp/run.sh
@@ -3,5 +3,9 @@
 set -eu
 
 yarn set version berry
-yarn install
+
+# To avoid this error 
+# YN0028: │ The lockfile would have been modified by this install, which is explicitly forbidden.
+YARN_ENABLE_IMMUTABLE_INSTALLS=0 yarn install
+
 yarn prisma generate
