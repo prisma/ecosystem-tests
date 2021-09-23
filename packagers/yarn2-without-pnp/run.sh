4416c7fdfe91d44a1db96559096f13e1c4c41a05
Joël Galeran <Jolg42@users.noreply.github.com>
2021-09-23 09:55:50 +0200

    yarn config set nodeLinker node-modules

--- packagers/yarn2-without-pnp/run.sh
+++ packagers/yarn2-without-pnp/run.sh
@@ -2,7 +2,11 @@
 
 set -eu
 
-yarn set version 2
+# Set Yarn to Berry (latest Yarn 2 version known to Yarn 1)
+yarn set version berry
+
+# Set nodeLinker to node-modules
+yarn config set nodeLinker node-modules
 
 # To avoid this error 
 # YN0028: │ The lockfile would have been modified by this install, which is explicitly forbidden.
