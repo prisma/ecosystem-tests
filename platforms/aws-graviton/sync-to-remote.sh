34fc449c15b1b2f2da6ef19d96e96d241ede6254
Jan Piotrowski <piotrowski+github@gmail.com>
2021-10-01 22:32:34 +0200

    fix(aws-graviton): Use PRISMA_CLIENT_ENGINE_TYPE env var in unique folder name to avoid overlaps (#2103)

--- platforms/aws-graviton/sync-to-remote.sh
+++ platforms/aws-graviton/sync-to-remote.sh
@@ -8,9 +8,9 @@ echo "Sync: Removing local node_modules"
 rm -rf ./code/node_modules
 
 echo "Sync: Create remote folder"
-ssh -tt -i ./server-key.pem ec2-user@54.72.209.131 "mkdir -p /home/ec2-user/aws-graviton/$GITHUB_JOB/$GITHUB_RUN_ID"
+ssh -tt -i ./server-key.pem ec2-user@54.72.209.131 "mkdir -p /home/ec2-user/aws-graviton/$GITHUB_JOB/$GITHUB_RUN_ID/$PRISMA_CLIENT_ENGINE_TYPE"
 
 echo "Sync: Copy code to unique folder"
-scp -i ./server-key.pem -rp ./code/* ec2-user@54.72.209.131:/home/ec2-user/aws-graviton/$GITHUB_JOB/$GITHUB_RUN_ID/
+scp -i ./server-key.pem -rp ./code/* ec2-user@54.72.209.131:/home/ec2-user/aws-graviton/$GITHUB_JOB/$GITHUB_RUN_ID/$PRISMA_CLIENT_ENGINE_TYPE/
 
 echo "sync-to-remote done"
