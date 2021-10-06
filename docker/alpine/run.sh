0448b4e7dcb245b5448628227589e7a45e1f2ed5
Harshit Pant <pantharshit00@gmail.com>
2021-10-06 20:14:00 +0530

    fix: add env vars in the conatiner

--- docker/alpine/run.sh
+++ docker/alpine/run.sh
@@ -4,5 +4,11 @@ set -eux
 export DEBUG="*"
 docker build -t prisma_alpine .
 
-docker run -p 3000:3000 -e DATABASE_URL=${DATABASE_URL} prisma_alpine
+docker run -p 3000:3000 \ 
+-e DEBUG=${DEBUG} \
+-e DATABASE_URL=${DATABASE_URL} \
+-e CI=${CI} \ 
+-e PRISMA_CLIENT_ENGINE_TYPE=${PRISMA_CLIENT_ENGINE_TYPE} \ 
+-e PRISMA_TELEMETRY_INFORMATION=${PRISMA_TELEMETRY_INFORMATION}  prisma_alpine
+
 sleep 3
