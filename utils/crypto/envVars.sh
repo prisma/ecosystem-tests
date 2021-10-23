b17451a1a835b57846362a9cff191a866cf4fccd
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2021-10-22 22:28:42 -0500

    fix(crypto): yarn ts-node calls

--- utils/crypto/envVars.sh
+++ utils/crypto/envVars.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-# equivalent of dirname in node.js
+# equivalent of a __dirname in node.js
 DIR=$(dirname "${BASH_SOURCE[0]}")
 
-echo $DIR
+# makes sure which node_modules to use
+MOD_DIR="$DIR/../../node_modules"
 
-# execute what ts-node has printed
-eval $(yarn --cwd $DIR ts-node $DIR/envVars.ts "$@")
+# execute what ts-node has printed out
+eval $(yarn --modules-folder $MOD_DIR ts-node $DIR/envVars.ts "$@")
