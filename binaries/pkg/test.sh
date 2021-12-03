f7a140dc303c19f71f7b4d67b53b73d258cdecb0
Jan Piotrowski <piotrowski+github@gmail.com>
2021-12-03 22:24:41 +0100

    Update test.sh

--- binaries/pkg/test.sh
+++ binaries/pkg/test.sh
@@ -30,7 +30,18 @@ yarn pkg node_modules/prisma -t node12-$os
 
 ./$filename init --datasource-provider sqlite
 
+# add model to schema file
+cat <> prisma/schema.prisma
+
+model User {
+  id    Int     @id @default(autoincrement())
+  email String  @unique
+  name  String?
+}
+EOF
+
 #export DEBUG="*"
+set DOTENV_CONFIG_DEBUG=true
 export DOTENV_CONFIG_DEBUG="true"
 export DATABASE_URL="file:./dev.db"
 ./$filename db push --skip-generate
