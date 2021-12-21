ad9036047fe07067df02b71f643b1ea1248522db
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2021-12-21 19:20:59 +0100

    chore(bundlers): log stdout

--- bundlers/webpack/index.test.js
+++ bundlers/webpack/index.test.js
@@ -4,6 +4,7 @@ test('test Prisma Client CRUD after bundling with webpack', async () => {
   const p = spawnSync('node', ['dist/main.js'], {
     encoding: 'utf8',
   })
+  console.log(p.stdout)
   const data = JSON.parse(p.stdout)
   expect(data.createUser.name).toBe('Alice')
   expect(data.updateUser.name).toBe('Bob')
