1245ff1ef115856691d85ff4090c24b3de875a0a
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2022-01-10 15:54:57 +0100

    fix(utils): do not print cmds in setEnv

--- utils/crypto/setEnv.sh
+++ utils/crypto/setEnv.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+# disable cmd printitng in this script
+# we don't want sensitive info to leak
+if [[ $- == *x* ]]; then # if -x is on
+    set +x # then we disable it
+    HAS_X_FLAG=true
+fi
+
 # equivalent of a __dirname in node.js
 DIR=$(dirname "${BASH_SOURCE[0]}")
 
@@ -11,3 +18,8 @@ TS_NODE=$(yarn --modules-folder $MOD_DIR bin ts-node)
 
 # execute what ts-node has printed out
 eval $($TS_NODE $DIR/setEnv.ts "$@")
+
+# reactivate -x flag for cmd printing
+if [[ $HAS_X_FLAG == true ]]; then
+    set -x
+fi
