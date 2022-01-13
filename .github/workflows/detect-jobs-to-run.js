8bdf0e167e0351c6649aa2c646e3be3cfd22ed76
Joël Galeran <galeran@prisma.io>
2022-01-13 10:01:45 +0100

    run multiple tests and add tests

--- .github/workflows/detect-jobs-to-run.js
+++ .github/workflows/detect-jobs-to-run.js
@@ -26,91 +26,108 @@ async function getStdin() {
   return result
 }
 
-async function main() {
-  const testYamlString = fs.readFileSync(
-    path.join(process.cwd(), '.github/workflows/test.yaml'),
-    { encoding: 'utf8' },
-  )
+async function detectJobsTorun({ filesChanged, GITHUB_REF }) {
+  const testYamlString = fs.readFileSync(path.join(process.cwd(), '.github/workflows/test.yaml'), { encoding: 'utf8' })
   const testYaml = yaml.parse(testYamlString)
 
   // ['process-managers', 'docker', 'core-features', ...]
-  const testDirectories = Object.keys(testYaml['jobs'])
-    .concat(Object.keys(testYaml['jobs']))
-    .filter((key) => {
-      const jobsToIgnore = [
-        'start-time', // Not a test but a job that fills an env var with the job start time
-        'report-to-slack-success', // Not a test but a job that posts to slack
-        'report-to-slack-failure', // Not a test but a job that posts to slack
-        'detect_jobs_to_run', // Not a test but a job that decides which tests should run
-        'cleanup-runs', // Not a test but a job that cancels previous runs
-      ]
-      return !jobsToIgnore.includes(key)
-    })
+  const testDirectories = Object.keys(testYaml['jobs']).filter((key) => {
+    const jobsToIgnore = [
+      'start-time', // Not a test but a job that fills an env var with the job start time
+      'report-to-slack-success', // Not a test but a job that posts to slack
+      'report-to-slack-failure', // Not a test but a job that posts to slack
+      'detect_jobs_to_run', // Not a test but a job that decides which tests should run
+      'cleanup-runs', // Not a test but a job that cancels previous runs
+    ]
+    return !jobsToIgnore.includes(key)
+  })
   console.debug(testDirectories)
-
-  const { GITHUB_REF } = process.env
 
   // Object used as output
   const jobsToRun = {}
 
-  // If true we will run all tests
-  let fallbackToAll = false
-
-  // Get which files changed from the action via stdin
-  const stdinData = await getStdin()
-  console.debug('stdin:', stdinData)
-  const filesChanged = JSON.parse(stdinData)
-  console.debug('filesChanged:', filesChanged)
+  // creates an object with all values set to true to be used a fallback to run all tests
+  const fallbackRunAllJobs = testDirectories.reduce((acc, curr) => ((acc[curr] = true), acc), {})
 
   // If we are in one of our special branches we always run all tests
-  if (
-    [
-      'refs/heads/dev',
-      'refs/heads/patch-dev',
-      'refs/heads/latest',
-      'refs/heads/integration',
-    ].includes(GITHUB_REF)
-  ) {
-    console.debug(
-      `GITHUB_REF=${GITHUB_REF} - special branch detected, settting fallbackToAll to true`,
-    )
-    fallbackToAll = true
+  if (['refs/heads/dev', 'refs/heads/patch-dev', 'refs/heads/latest', 'refs/heads/integration'].includes(GITHUB_REF)) {
+    console.debug(`GITHUB_REF=${GITHUB_REF} - special branch detected`)
+    console.debug('Fallback to run all tests.')
+    return fallbackRunAllJobs
   }
+
+  // If no files changed let's fallback and run all tests
+  if (filesChanged.length === 0) {
+    console.debug(`filesChanged = 0, no change detected`)
+    console.debug('Fallback to run all tests.')
+    return fallbackRunAllJobs
+  }
+
+  // Used to know if files outside the directories have changed if so we will fallback to all
+  let totalNumberOfFilesChangedInsideDirectories = 0
 
   /**
    * @type string[]
    **/
-
   for (const directoryName of testDirectories) {
     // If changes are located only in one of the directories
-    if (
-      filesChanged.every((fileChanged) => fileChanged.startsWith(directoryName))
-    ) {
+    const filesChangedInsideDirectory = filesChanged.filter((fileChanged) =>
+      fileChanged.startsWith(`${directoryName}/`),
+    )
+    console.debug({ directoryName })
+    console.debug({ filesChangedInsideDirectory })
+
+    if (filesChangedInsideDirectory.length > 0) {
+      // Add to total
+      totalNumberOfFilesChangedInsideDirectories += filesChangedInsideDirectory.length
+
+      // we need to run the test
       jobsToRun[directoryName] = true
     } else {
+      // we don't need to run the test
       jobsToRun[directoryName] = false
-      fallbackToAll = true
     }
   }
 
-  console.debug('fallbackToAll:', fallbackToAll)
-  console.debug('jobsToRun:', jobsToRun)
+  console.debug({ jobsToRun })
+  console.debug('filesChanged.length', filesChanged.length)
+  console.debug({ totalNumberOfFilesChangedInsideDirectories })
 
-  if (fallbackToAll) {
-    console.debug('Fallback! All directories must be tested.')
-    // creates an object with all values set to true
-    const fallbackRunAll = testDirectories.reduce(
-      (acc, curr) => ((acc[curr] = true), acc),
-      {},
-    )
-    console.debug('fallbackRunAll:', fallbackRunAll)
-    console.log('::set-output name=jobs::' + JSON.stringify(fallbackRunAll))
+  if (totalNumberOfFilesChangedInsideDirectories != filesChanged.length) {
+    console.debug('totalNumberOfFilesChangedInsideDirectories != filesChanged.length')
+    console.debug('Fallback to run all tests.')
+    return fallbackRunAllJobs
   } else {
-    console.debug('Only one directory will be tested')
-    console.log('::set-output name=jobs::' + JSON.stringify(jobsToRun))
+    console.log('Only the following directory/ies will be tested:')
+    console.log(jobsToRun)
+    return jobsToRun
   }
 }
 
-main().then(function () {
-  console.log('Done!')
-})
+async function main() {
+  // Get which files changed from the action via stdin
+  const stdinData = await getStdin()
+  console.debug('stdin:', stdinData)
+
+  const filesChanged = JSON.parse(stdinData)
+  console.debug('filesChanged:', filesChanged)
+
+  const { GITHUB_REF } = process.env
+
+  const jobsToRun = await detectJobsTorun({
+    filesChanged,
+    GITHUB_REF,
+  })
+  console.log('::set-output name=jobs::' + JSON.stringify(jobsToRun))
+}
+
+// Only run if from command line
+if (require.main === module) {
+  main().then(function () {
+    console.log('Done!')
+  })
+}
+
+module.exports = {
+  detectJobsTorun,
+}
