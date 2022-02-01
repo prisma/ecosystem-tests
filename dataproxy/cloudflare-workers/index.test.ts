1c16356eec27e387b3a9b1e5f7be3e5e7fc19e55
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2022-02-01 18:30:46 +0100

    test(dataproxy): fix timeout (#2384)

--- dataproxy/cloudflare-workers/index.test.ts
+++ dataproxy/cloudflare-workers/index.test.ts
@@ -4,8 +4,6 @@ import fetch from 'node-fetch'
 
 describe('use data proxy', () => {
   test('fetch response', async () => {
-    jest.setTimeout(30000)
-
     const cloudflareWorkersDeploymentUrl = process.env.DEPLOYMENT_URL!
     console.debug(cloudflareWorkersDeploymentUrl)
     
@@ -78,5 +76,5 @@ Object {
   ],
 }
 `)
-  })
+  }, 30000)
 })
