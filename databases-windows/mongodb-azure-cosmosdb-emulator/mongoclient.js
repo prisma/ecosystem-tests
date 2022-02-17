1a37200512387455ee4ab7c793ea9c2f942ce57a
Jan Piotrowski <piotrowski+github@gmail.com>
2022-02-17 19:43:19 +0000

    disable ssl validation in mongo client

--- databases-windows/mongodb-azure-cosmosdb-emulator/mongoclient.js
+++ databases-windows/mongodb-azure-cosmosdb-emulator/mongoclient.js
@@ -1,7 +1,10 @@
 const { MongoClient } = require('mongodb')
 
 const uri = "mongodb://localhost:C2y6yDjf5%2FR%2Bob0N8A7Cgv30VRDJIWEHLM%2B4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw%2FJw%3D%3D@localhost:10255/e2e-tests?ssl=true";
-const client = new MongoClient(uri);
+var options = {
+  sslValidate: false
+}
+const client = new MongoClient(uri, options);
 async function run() {
   try {
     await client.connect();
