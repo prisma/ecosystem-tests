a7dad374da4789b902d41314279947a1a7676de4
Jan Piotrowski <piotrowski+github@gmail.com>
2022-02-19 19:44:42 +0000

    retryWrites=false

--- databases-windows/mongodb-azure-cosmosdb-emulator/mongoclient.js
+++ databases-windows/mongodb-azure-cosmosdb-emulator/mongoclient.js
@@ -1,6 +1,6 @@
 const { MongoClient } = require('mongodb')
 
-const uri = "mongodb://localhost:C2y6yDjf5%2FR%2Bob0N8A7Cgv30VRDJIWEHLM%2B4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw%2FJw%3D%3D@localhost:10255/e2e-tests?ssl=true";
+const uri = "mongodb://localhost:C2y6yDjf5%2FR%2Bob0N8A7Cgv30VRDJIWEHLM%2B4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw%2FJw%3D%3D@localhost:10255/e2e-tests?ssl=true&retryWrites=false";
 var options = {
   sslValidate: false
 }
