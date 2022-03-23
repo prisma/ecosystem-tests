3de6bb1a35b59fa4baec367d7815e91d7860bf34
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2022-03-23 20:46:13 +0100

    fix(packagers): check real outdir for pnpm (#2586)

--- packagers/pnpm-workspaces-default-output/test.sh
+++ packagers/pnpm-workspaces-default-output/test.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
-FILE0=node_modules/.prisma/client/index.js
 FILE1=sub-project-1/node_modules/.prisma/client/index.js
 FILE2=sub-project-2/node_modules/.prisma/client/index.js
 
 set -eux
 
-cd workspace
+cd workspace/sub-project-1
+FILE0=$(node -e "console.log(path.join(require.resolve('@prisma/client/package.json'), '..', '..', '..', '.prisma'))")
 
 if [ -f "$FILE1" ] || [ -f "$FILE2" ]; then
     echo "Client should not generate in sub-folder"; exit 1;
 fi;
 
-if [ ! -f "$FILE0" ]; then
+if [ ! -d "$FILE0" ]; then
     echo "Client did not generate"; exit 1;
 fi;
 
