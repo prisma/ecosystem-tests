676c73d8a766e2f753b6bb70506a1baa5c0e612f
Jan Piotrowski <piotrowski+github@gmail.com>
2022-05-03 16:32:43 +0200

    Split out Vercel jobs to be able to remove retry (#2729)
    * Split out Vercel jobs to be able to remove retry
    
    * Update test.yaml
    
    * Update check-folders.js
    
    * fix test assertions
    
    * actually move vercel projects
    
    * fix
    
    * fix
    
    * Apply suggestions from code review
    
    Co-authored-by: Joël Galeran <Jolg42@users.noreply.github.com>
    
    Co-authored-by: Joël Galeran <Jolg42@users.noreply.github.com>

--- .github/scripts/check-folders.js
+++ .github/scripts/check-folders.js
@@ -4,51 +4,74 @@ const yaml = require('yaml')
 const fs = require('fs')
 const path = require('path')
 
+/**
+ * This script checks if there are folders (= tests) that are not "run" via test.yaml and test.yaml
+ *  It gets all relevant folders, then all the jobs from both workflows, and extract those from the list of folder and checks if nothing is left 
+ */
 async function main() {
+  
+  // Files and folders to skip during checking
+  const ignoreFiles = [
+    'package.json', // package.json at root
+    'generic/basic', // generic/basic doesn't use Github action matrix feature which we parse to find out the differences
+    'platforms-serverless-vercel/vercel-with-redwood/api', // Redwood uses workspaces but is included
+    'platforms-serverless-vercel/vercel-with-redwood/web', // Redwood uses workspaces but is included
+    'platforms-serverless/firebase-functions/functions', // Firebase root doesn't have package.json but is included
+    'packagers/yarn-workspaces/prisma-project', // Yarn workspaces root doesn't have package.json but is included
+    'packagers/yarn3-workspaces-pnp/packages/sub-project-1', // We don't want to include the workspace folders in the matrix
+    'packagers/yarn3-workspaces-pnp/packages/sub-project-2', // We don't want to include the workspace folders in the matrix
+    'platforms/aws-graviton/code', // aws-graviton doesn't have package.json at root but is included
+    'platforms/m1-macstadium/code', // m1-macstadium doesn't have package.json at root but is included
+  ]
+  
+  // Jobs in the workflow files that are not relevant and can be skipped
+  const jobsToIgnore = [
+    'start-time', // Not a test but a job that fills an env var with the job start time
+    'report-to-slack-success', // Not a test but a job that posts to slack
+    'report-to-slack-failure', // Not a test but a job that posts to slack
+    'detect_jobs_to_run', // Not a test but a job that decides which tests should run
+    'cleanup-runs', // Not a test but a job that cancels previous runs
+  ]
+  
+  // Keys to ignore
+  // TODO better description what this actually means
+  const keysToIgnore = [
+    'os', // We want to count folders vs references in the yaml file not when something is run across differnt OSes
+    'node', // We want to count folders vs references in the yaml file not when something is run across differnt node versions
+  ]
+  
+  // Get all relevant folders that _should_ appear in workflows
   const folders = glob
     .sync('**/package.json', {
       ignore: ['**/node_modules/**', '**/custom-engines/**'],
     })
     .map((file) => file.replace('/package.json', ''))
     .filter((file) => {
-      const ignoreFiles = [
-        'package.json', // package.json at root
-        'platforms-serverless/vercel-with-redwood/api', // Redwood uses workspaces but is included
-        'platforms-serverless/vercel-with-redwood/web', // Redwood uses workspaces but is included
-        'platforms-serverless/firebase-functions/functions', // Firebase root doesn't have package.json but is included
-        'generic/basic', // generic/basic doesn't use Github action matrix feature which we parse to find out the differences
-        'packagers/yarn-workspaces/prisma-project', // Yarn workspaces root doesn't have package.json but is included
-        'packagers/yarn3-workspaces-pnp/packages/sub-project-1', // We don't want to include the workspace folders in the matrix
-        'packagers/yarn3-workspaces-pnp/packages/sub-project-2', // We don't want to include the workspace folders in the matrix
-        'platforms/aws-graviton/code', // aws-graviton doesn't have package.json at root but is included
-        'platforms/m1-macstadium/code', // m1-macstadium doesn't have package.json at root but is included
-      ]
       return !ignoreFiles.includes(file)
     })
 
+  // get optional tests workflow
   const optionalTestYamlString = fs.readFileSync(
     path.join(process.cwd(), '.github/workflows/optional-test.yaml'),
     { encoding: 'utf8' },
   )
   const optionalTestYaml = yaml.parse(optionalTestYamlString)
 
+  // get tests workflow
   const testYamlString = fs.readFileSync(
     path.join(process.cwd(), '.github/workflows/test.yaml'),
     { encoding: 'utf8' },
   )
   const testYaml = yaml.parse(testYamlString)
 
+  // get job keys from workflow files
   const jobKeys = Object.keys(optionalTestYaml['jobs'])
     .concat(Object.keys(testYaml['jobs']))
     .filter((key) => {
-      const jobsToIgnore = [
-        'report-to-slack-success', // Not a test but a job that posts to slack
-        'report-to-slack-failure', // Not a test but a job that posts to slack
-        'detect_jobs_to_run', // Not a test but a job that decides which tests should run
-        'cleanup-runs', // Not a test but a job that cancels previous runs
-      ]
       return !jobsToIgnore.includes(key)
     })
+  
+  // Create an array of job names, from reading the `jobs` keys of the .yml file
   const jobs = jobKeys
     .map((key) => {
       const job = testYaml['jobs'][key] || optionalTestYaml['jobs'][key]
@@ -56,10 +79,6 @@ async function main() {
         Boolean(job) && Boolean(job.strategy) ? job.strategy.matrix : {}
       const folders = Object.keys(matrix)
         .filter((key) => {
-          const keysToIgnore = [
-            'os', // We want to count folders vs references in the yaml file not when something is run across differnt OSes
-            'node', // We want to count folders vs references in the yaml file not when something is run across differnt node versions
-          ]
           return !keysToIgnore.includes(key)
         })
         .reduce((acc, key) => acc.concat(...matrix[key]), [])
@@ -69,6 +88,7 @@ async function main() {
       return acc.concat([...folders])
     }, [])
 
+  // Transform the shape to return something that can be easy to use from GitHub Actions if conditionals
   const foldersObj = folders.reduce((acc, folder) => {
     return {
       ...acc,
