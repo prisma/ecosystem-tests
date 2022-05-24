d6d85353cf57235cb032fb19185a6a29c46d5dbd
Sergey Tatarintsev <sergey@tatarintsev.me>
2022-05-24 14:46:32 +0200

    Update platforms-serverless/netlify-cli/test.sh
    Co-authored-by: Jan Piotrowski <piotrowski+github@gmail.com>

--- platforms-serverless/netlify-cli/test.sh
+++ platforms-serverless/netlify-cli/test.sh
@@ -3,7 +3,7 @@
 set -eux
 
 if [ "$PRISMA_CLIENT_ENGINE_TYPE" == "binary" ]; then
-  BINARY_STRING=',"files":["index-browser.js","index.d.ts","index.js","query-engine-rhel-openssl-1.0.x","package.json","runtime","schema.prisma"]'
+  BINARY_STRING=',"files":["index-browser.js","index.d.ts","index.js","package.json","query-engine-rhel-openssl-1.0.x","runtime","schema.prisma"]'
 else
   BINARY_STRING=',"files":["index-browser.js","index.d.ts","index.js","libquery_engine-rhel-openssl-1.0.x.so.node","package.json","runtime","schema.prisma"]'
 fi
