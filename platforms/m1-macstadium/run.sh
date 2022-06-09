d5172eb880910f363dd897e36107c8acce65fe90
Joël Galeran <Jolg42@users.noreply.github.com>
2022-06-09 15:11:50 +0200

    chore: m1 fix path to use nvm node version (16.10.0)

--- platforms/m1-macstadium/run.sh
+++ platforms/m1-macstadium/run.sh
@@ -11,12 +11,7 @@ if [ "$PRISMA_CLIENT_ENGINE_TYPE" == "binary" ]; then
       export PRISMA_CLIENT_ENGINE_TYPE=\"binary\";
 
       # to get around https://serverfault.com/questions/351731/why-does-the-path-of-an-ssh-remote-command-differ-from-that-of-an-interactive-s
-      export PATH=/opt/homebrew/bin:/opt/homebrew/sbin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Users/administrator/.cargo/bin
-
-      # Load NVM, normally loaded in `./.zshrc` but the `export PATH` removes it, so we load it again here
-      # From https://github.com/nvm-sh/nvm#installing-and-updating
-      export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-      [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+      export PATH=/Users/administrator/.nvm/versions/node/v16.10.0/bin:/opt/homebrew/bin:/opt/homebrew/sbin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Users/administrator/.cargo/bin
 
       yarn;
       yarn prisma generate;
@@ -29,13 +24,8 @@ else
       export PRISMA_CLIENT_ENGINE_TYPE=\"library\";
 
       # to get around https://serverfault.com/questions/351731/why-does-the-path-of-an-ssh-remote-command-differ-from-that-of-an-interactive-s
-      export PATH=/opt/homebrew/bin:/opt/homebrew/sbin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Users/administrator/.cargo/bin
+      export PATH=/Users/administrator/.nvm/versions/node/v16.10.0/bin:/opt/homebrew/bin:/opt/homebrew/sbin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Users/administrator/.cargo/bin
 
-      # Load NVM, normally loaded in `./.zshrc` but the `export PATH` removes it, so we load it again here
-      # From https://github.com/nvm-sh/nvm#installing-and-updating
-      export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-      [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-      
       yarn;
       yarn prisma generate;
       yarn prisma -v;
