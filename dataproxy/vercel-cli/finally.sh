56ef000ab267a7bcdcf4a527a25dac6aaf59e107
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2022-06-15 16:27:51 +0200

    fix vercel log url and token

--- dataproxy/vercel-cli/finally.sh
+++ dataproxy/vercel-cli/finally.sh
@@ -2,4 +2,4 @@
 
 set -eu
 
-yarn vercel logs https://e2e-dataproxy-vercel-cli.vercel.app/api--token=$VERCEL_TOKEN --scope=$VERCEL_ORG_ID
+yarn vercel logs https://e2e-dataproxy-vercel-cli.vercel.app --token=$VERCEL_TOKEN --scope=$VERCEL_ORG_ID
