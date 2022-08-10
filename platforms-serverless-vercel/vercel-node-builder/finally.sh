dc46a6d0f5e39a372733d97c99f04aadc41ba017
Joël Galeran <Jolg42@users.noreply.github.com>
2022-08-10 16:40:50 +0200

    isolate vercel-node-builder

--- platforms-serverless-vercel/vercel-node-builder/finally.sh
+++ platforms-serverless-vercel/vercel-node-builder/finally.sh
@@ -2,4 +2,10 @@
 
 set -eu
 
-yarn vercel logs e2e-vercel-node-builder.prisma.vercel.app --token=$VERCEL_TOKEN --scope=$VERCEL_ORG_ID
+if [ $PRISMA_CLIENT_ENGINE_TYPE == "binary" ]; then
+  echo "Binary"
+  yarn vercel logs e2e-vercel-node-builder-binary.prisma.vercel.app --token=$VERCEL_TOKEN --scope=$VERCEL_ORG_ID
+else
+  echo "Library (Default)"
+  yarn vercel logs e2e-vercel-node-builder.prisma.vercel.app --token=$VERCEL_TOKEN --scope=$VERCEL_ORG_ID
+fi
