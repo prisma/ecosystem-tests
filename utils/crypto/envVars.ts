620381eadeea5c938f03ed63c74905846c9de656
Yoshiya Hinosawa <stibium121@gmail.com>
2022-10-06 17:02:54 +0900

    add DENO_DATA_PROXY_URL entry in utils/crypto/envVars.ts

--- utils/crypto/envVars.ts
+++ utils/crypto/envVars.ts
@@ -15,7 +15,10 @@ export const envVars = {
 
     // dataproxy > vercel-cli
     DATAPROXY_VERCEL_CLI_SERVERLESS_FUNCTIONS_DB_URL: "U2FsdGVkX187qRBSpPYoBu+9kcycYm/7BvLMBftPQbzKid8w0ZXamlDLESDjjlWZZoq40pyYNZxyOa7Y9jXul6U7lf2NPP5EZ41LS1kAW1nUGfSkFf/5Lgek5kbxoaOChuhboHmydnb/Ok5ztaVtLcTzYP4HmSeOAPEwDid9M7sD1CXcsmfA+1bDDpKmggFB",
-    DATAPROXY_VERCEL_CLI_SERVERLESS_FUNCTIONS_PROJECT_ID: "U2FsdGVkX1/xZUhTjXe9Tucxq3169wuYkJxaPgQfZqvZba8/7AwRqi5QGVV6bKl2CS0D4ix2CQ8W4+ZC5JsAqQ=="
+    DATAPROXY_VERCEL_CLI_SERVERLESS_FUNCTIONS_PROJECT_ID: "U2FsdGVkX1/xZUhTjXe9Tucxq3169wuYkJxaPgQfZqvZba8/7AwRqi5QGVV6bKl2CS0D4ix2CQ8W4+ZC5JsAqQ==",
+
+    // dataproxy > deno
+    DENO_DATA_PROXY_URL: "U2FsdGVkX187qRBSpPYoBu+9kcycYm/7BvLMBftPQbzKid8w0ZXamlDLESDjjlWZZoq40pyYNZxyOa7Y9jXul6U7lf2NPP5EZ41LS1kAW1nUGfSkFf/5Lgek5kbxoaOChuhboHmydnb/Ok5ztaVtLcTzYP4HmSeOAPEwDid9M7sD1CXcsmfA+1bDDpKmggFB",
 
     // TODO: rename the env vars to a decent standard before it gets out of hand
 }
