ce42a790f2861088862c926206b232e7bfa8f75e
Jan Piotrowski <piotrowski+github@gmail.com>
2022-10-15 00:21:11 +0000

    chore:: manipulate generated client to make unknown-to-dataproxy preview feature flag invisible

--- dataproxy/deno/run.sh
+++ dataproxy/deno/run.sh
@@ -3,3 +3,6 @@
 set -eu
 
 deno run -A --unstable npm:prisma@4.4.0-integration-feat-deno-deploy.1 generate --data-proxy
+
+# manipulate generated client so Data Proxy does not stumble over new preview feature flag
+node manipulate.js
