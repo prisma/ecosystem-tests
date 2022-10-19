90baf5c614bed73fd29400068f5d963564d305e1
Jan Piotrowski <piotrowski+github@gmail.com>
2022-10-19 17:46:51 +0200

    chore(deno): Remove manipulate.js (#3160)
    Co-authored-by: Joël Galeran <Jolg42@users.noreply.github.com>

--- dataproxy/deno/run.sh
+++ dataproxy/deno/run.sh
@@ -5,6 +5,3 @@ set -eu
 version=$( jq -r  '.devDependencies.prisma' package.json ) 
 
 deno run -A --unstable npm:prisma@$version generate --data-proxy
-
-# manipulate generated client so Data Proxy does not stumble over new preview feature flag
-node manipulate.js
