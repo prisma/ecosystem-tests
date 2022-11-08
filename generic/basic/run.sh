6f9f5fe3847be6cc246e99bd65d45fc538166dae
Joël Galeran <Jolg42@users.noreply.github.com>
2022-11-08 11:24:51 +0100

    brew / nvm

--- generic/basic/run.sh
+++ generic/basic/run.sh
@@ -7,8 +7,13 @@ export RUST_BACKTRACE=full
 
 openssl version
 
+# Needed for GitHub Linux runner image
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
 brew install nvm
 
+export NVM_DIR="$HOME/.nvm"
+[ -s "/usr/local/opt/nvm/nvm.sh" ] && \. "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
 nvm install --lts
 
 brew install openssl@1.1
