a92e73a91d14553ad58f824cfe5ae1ae361a72e9
Joël Galeran <Jolg42@users.noreply.github.com>
2022-11-08 11:44:07 +0100

    Update run.sh

--- generic/basic/run.sh
+++ generic/basic/run.sh
@@ -12,15 +12,14 @@ then
     # Linux
     # Needed for GitHub Linux runner image
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    echo '# Set PATH, MANPATH, etc., for Homebrew.' >> /home/runner/.bash_profile
-    echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/runner/.bash_profile
     eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+    
+    brew install nvm
+
+    export NVM_DIR="$HOME/.nvm"
+    [ -s "/usr/local/opt/nvm/nvm.sh" ] && \. "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
 fi
 
-brew install nvm
-
-export NVM_DIR="$HOME/.nvm"
-[ -s "/usr/local/opt/nvm/nvm.sh" ] && \. "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
 nvm install --lts
 
 brew install openssl@1.1
