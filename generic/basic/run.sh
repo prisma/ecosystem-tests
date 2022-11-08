ef1e2f2143c4ae406843ae58bcc0dfb5cd3d1f67
Joël Galeran <Jolg42@users.noreply.github.com>
2022-11-08 11:32:18 +0100

    brew for linux

--- generic/basic/run.sh
+++ generic/basic/run.sh
@@ -7,9 +7,19 @@ export RUST_BACKTRACE=full
 
 openssl version
 
-# Needed for GitHub Linux runner image
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
+if [[ "$OSTYPE" == "darwin"* ]]; then
+        # Nothing
+elif [[ "$OSTYPE" == "win32" ]]; then
+        # Nothing
+else
+        # Linux
+        # Needed for GitHub Linux runner image
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+        echo '# Set PATH, MANPATH, etc., for Homebrew.' >> /home/runner/.bash_profile
+        echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/runner/.bash_profile
+        eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+fi
+    
 brew install nvm
 
 export NVM_DIR="$HOME/.nvm"
