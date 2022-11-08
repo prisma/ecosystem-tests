acb7f1bb6690e3e83aac793c08910566501fc8b9
Joël Galeran <Jolg42@users.noreply.github.com>
2022-11-08 11:57:03 +0100

    source $(brew --prefix nvm)/nvm.sh

--- generic/basic/run.sh
+++ generic/basic/run.sh
@@ -16,8 +16,7 @@ then
 fi
 
 brew install nvm
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+source $(brew --prefix nvm)/nvm.sh
 
 nvm install --lts
 
