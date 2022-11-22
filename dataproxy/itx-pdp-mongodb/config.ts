afee2bac97946445a463b344eb4a3035027315e7
daniel starns <danielstarns@hotmail.com>
2022-11-22 17:53:55 +0000

    lower mongodb config

--- dataproxy/itx-pdp-mongodb/config.ts
+++ dataproxy/itx-pdp-mongodb/config.ts
@@ -1,7 +1,7 @@
 export const config = {
   globalTimeout: 900_000_000,
   'lots-of-activities': {
-    amount: 500, // Amount of read and writes inside a transaction
+    amount: 200, // Amount of read and writes inside a transaction
   },
   'long-running': {
     transactionDelay: 12000, // How long to delay the transaction
@@ -11,11 +11,11 @@ export const config = {
     transactionDelay: 12000, // How long to delay the transaction
   },
   concurrent: {
-    amount: 20, // How many concurrent to run at once
+    amount: 10, // How many concurrent to run at once
   },
   'burst-load': {
-    bursts: 20, // How many bursts to perform
-    children: 20, // How many ITX to do in a burst
+    bursts: 10, // How many bursts to perform
+    children: 10, // How many ITX to do in a burst
     backoff: 1000, // How long to wait in-between each burst
   },
 }
