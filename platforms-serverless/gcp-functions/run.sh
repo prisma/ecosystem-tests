58a587e9d5fae0f63c360e361f0186cea05810de
Joël Galeran <Jolg42@users.noreply.github.com>
2022-12-13 08:55:31 +0000

    try nodejs18

--- platforms-serverless/gcp-functions/run.sh
+++ platforms-serverless/gcp-functions/run.sh
@@ -24,4 +24,4 @@ yarn tsc
 func="e2e-test-$(date "+%Y-%m-%d-%H%M%S")"
 echo "$func" > func-tmp.txt
 
-gcloud functions deploy "$func" --runtime nodejs14 --trigger-http --entry-point=handler --allow-unauthenticated --verbosity debug --set-env-vars DATABASE_URL=$DATABASE_URL,PRISMA_TELEMETRY_INFORMATION='ecosystem-tests platforms azure functions linux gcp functions env'
+gcloud functions deploy "$func" --runtime nodejs18 --trigger-http --entry-point=handler --allow-unauthenticated --verbosity debug --set-env-vars DATABASE_URL=$DATABASE_URL,PRISMA_TELEMETRY_INFORMATION='ecosystem-tests platforms azure functions linux gcp functions env'
--- platforms-serverless/lambda/update-config.sh
+++ platforms-serverless/lambda/update-config.sh
@@ -2,4 +2,4 @@
 
 set -eux
 
-aws lambda update-function-configuration --function-name prisma2-e2e-tests --runtime nodejs14.x --handler index.handler --timeout 10
+aws lambda update-function-configuration --function-name prisma2-e2e-tests --runtime nodejs18.x --handler index.handler --timeout 10
