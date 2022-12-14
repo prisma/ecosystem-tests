5c1612a5a4e2a02716dbf7243e373a6fa0179f6e
jkomyno <skiabo97@gmail.com>
2022-12-14 14:22:54 +0100

    feat(os-support): add examples of running Prisma CLI and Client in Alpine

---
+++ os-support/docker/linux-node-alpine-x64/README.md
@@ -0,0 +1,11 @@
+# OS support: linux-node-alpine-x64
+
+- Docker image: `node:18.12.1-alpine`
+- OS: Linux Alpine
+- Arch: x64
+
+## Build & Run
+
+```bash
+./run.sh
+```
---
+++ os-support/docker/linux-node-alpine-x64/prisma/seed.ts
@@ -0,0 +1,38 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+
+  /* truncate tables */
+  await prisma.post.deleteMany()
+  await prisma.someUser.deleteMany()
+
+  // create users
+  const N_USERS = 2
+  const userIds = Array.from({ length: N_USERS }, (_, i) => i + 1)
+
+  // create posts per user
+  const N_POSTS_PER_USER = 100
+  const postsToCreate = Array.from({ length: N_USERS * N_POSTS_PER_USER }, (_, i) => ({
+    id: i + 1,
+    userId: Math.floor(i / N_POSTS_PER_USER) + 1
+  }))
+
+  console.log('postsToCreate', postsToCreate)
+
+  console.log('creating users...')
+  await prisma.someUser.createMany({
+    data: userIds.map((id) => ({ id })),
+  })
+
+  console.log('creating posts...')
+  await prisma.post.createMany({
+    data: postsToCreate,
+  })
+}
+
+main()
+  .finally(() => {
+    prisma.$disconnect()
+  })
