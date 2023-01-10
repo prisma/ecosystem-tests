37882a0ab9a97b5ce85cc0a44f69f19c185e16c7
Jan Piotrowski <piotrowski+github@gmail.com>
2023-01-10 21:58:06 +0100

    -x for shell, --verbose for cp

--- binaries/pkg/run.sh
+++ binaries/pkg/run.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-set -eu
+set -eux
 
 yarn install
 
 # This is a hack to work around a problem that pkg would otherwise have with the Engines binaries missing in the package
 mkdir -p node_modules/prisma/node_modules/@prisma/engines
-cp -R node_modules/@prisma/engines/* node_modules/prisma/node_modules/@prisma/engines
+cp --verbose -R node_modules/@prisma/engines/* node_modules/prisma/node_modules/@prisma/engines
