31188d7554fc70c86cff34ad321dc23c71a283de
jkomyno <skiabo97@gmail.com>
2023-01-14 18:40:57 +0100

    chore: remove --follow

--- platforms-serverless-vercel/vercel-cli/run.sh
+++ platforms-serverless-vercel/vercel-cli/run.sh
@@ -30,7 +30,7 @@ echo "Deployed to ${DEPLOYED_URL}"
 
 # sleep 15
 
-OUTPUT=$(yarn -s vercel logs --follow $DEPLOYED_URL --token=$VERCEL_TOKEN --scope=$VERCEL_ORG_ID)
+OUTPUT=$(yarn -s vercel logs $DEPLOYED_URL --token=$VERCEL_TOKEN --scope=$VERCEL_ORG_ID)
 echo "${OUTPUT}"
 
 # Check the Vercel Build Logs for the postinstal hook"
