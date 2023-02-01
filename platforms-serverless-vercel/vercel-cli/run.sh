88523266ce96119d4aa2a3a09878de3e187beff2
Joël Galeran <Jolg42@users.noreply.github.com>
2023-02-01 22:39:11 +0100

    fix(vercel): rename --confirm to --yes + small changes (#3420)

--- platforms-serverless-vercel/vercel-cli/run.sh
+++ platforms-serverless-vercel/vercel-cli/run.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 set -eu
+# Replace by the following line to debug this script
+# set -eux
 
 export PRISMA_TELEMETRY_INFORMATION='ecosystem-tests platforms vercel-cli build'
-yarn
 
-export VERCEL_ORG_ID=$VERCEL_ORG_ID
-echo "VERCEL_ORG_ID: $VERCEL_ORG_ID"
+yarn
 
 # Note: be aware that Vercel truncates logs, so if you add something like `--build-env DEBUG="prisma:*"` plenty of logs will be missing.
 # That will likely influence the "Postinstall hook" check below, which will result in the CI failing with error code `1`.
@@ -14,16 +14,18 @@ echo "VERCEL_ORG_ID: $VERCEL_ORG_ID"
 if [ "$PRISMA_CLIENT_ENGINE_TYPE" == "binary" ]; then
   echo "Binary"
   export VERCEL_PROJECT_ID=$VERCEL_API_BINARY_PROJECT_ID
-  echo "VERCEL_PROJECT_ID: $VERCEL_PROJECT_ID"
-
-  yarn -s vercel --token=$VERCEL_TOKEN --build-env PRISMA_CLIENT_ENGINE_TYPE="binary" --prod --scope=$VERCEL_ORG_ID --confirm --force 1> deployment-url.txt
 else
  echo "Library (Default)"
   export VERCEL_PROJECT_ID=$VERCEL_API_PROJECT_ID
-  echo "VERCEL_PROJECT_ID: $VERCEL_PROJECT_ID"
-
-  yarn -s vercel --token=$VERCEL_TOKEN --build-env PRISMA_CLIENT_ENGINE_TYPE="library" --prod --scope=$VERCEL_ORG_ID --confirm --force 1> deployment-url.txt
+  # Set local var to `library` for the `vercel deploy` command below
+  PRISMA_CLIENT_ENGINE_TYPE=library
 fi
+
+export VERCEL_ORG_ID=$VERCEL_ORG_ID
+echo "VERCEL_ORG_ID: $VERCEL_ORG_ID"
+echo "VERCEL_PROJECT_ID: $VERCEL_PROJECT_ID"
+echo "PRISMA_CLIENT_ENGINE_TYPE: $PRISMA_CLIENT_ENGINE_TYPE"
+yarn -s vercel deploy --prod --yes --force --token=$VERCEL_TOKEN --build-env PRISMA_CLIENT_ENGINE_TYPE="$PRISMA_CLIENT_ENGINE_TYPE" --scope=$VERCEL_ORG_ID 1> deployment-url.txt
 
 echo ''
 cat deployment-url.txt
