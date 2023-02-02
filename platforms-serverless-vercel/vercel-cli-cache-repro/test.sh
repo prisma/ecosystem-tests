eeec32a9b9dfd203b54d5107e299996d5110b9c6
Joël Galeran <Jolg42@users.noreply.github.com>
2023-02-02 15:37:33 +0100

    fix DEPLOYED_URL_API

--- platforms-serverless-vercel/vercel-cli-cache-repro/test.sh
+++ platforms-serverless-vercel/vercel-cli-cache-repro/test.sh
@@ -11,5 +11,6 @@ else
   files=',"files":["index.js","libquery_engine-rhel-openssl-1.0.x.so.node","package.json","schema.prisma"]'
 fi
 
-DEPLOYED_URL=$( tail -n 1 deployment-url.txt )
-npx ts-node ../../utils/fetch-retry-and-confirm-version.ts --url $DEPLOYED_URL --prisma-version $(sh ../../utils/prisma_version.sh) --binary-string ${files}
+DEPLOYED_URL_API=$( tail -n 1 deployment-url.txt )/api
+
+npx ts-node ../../utils/fetch-retry-and-confirm-version.ts --url "$DEPLOYED_URL_API" --prisma-version "$(sh ../../utils/prisma_version.sh)" --binary-string "${files}"
