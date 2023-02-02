1347ca0f7ab537d7718ff0bc9d5da5c5dca3623a
Joël Galeran <Jolg42@users.noreply.github.com>
2023-02-02 16:49:09 +0100

    fix binary run, ignore logs

--- platforms-serverless-vercel/vercel-cli-cache-repro/finally.sh
+++ platforms-serverless-vercel/vercel-cli-cache-repro/finally.sh
@@ -2,6 +2,12 @@
 
 set -eu
 
-DEPLOYED_URL=$( tail -n 1 deployment-url.txt )
+if [ "$PRISMA_CLIENT_ENGINE_TYPE" == "binary" ]; then
+  echo "Binary"
+  echo "Deployment does not exist yet"
+  exit 0
+else
+  DEPLOYED_URL=$( tail -n 1 deployment-url.txt )
+  yarn vercel logs $DEPLOYED_URL --token=$VERCEL_TOKEN --scope=$VERCEL_ORG_ID
+fi
 
-yarn vercel logs $DEPLOYED_URL --token=$VERCEL_TOKEN --scope=$VERCEL_ORG_ID
