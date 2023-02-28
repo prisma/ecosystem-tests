46c22eb72643b2a662614631dee800391e44c16c
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2023-02-28 13:00:26 -1000

    update upgrade script

--- packagers/yarn3-workspaces-pnp/packages/sub-project-1/upgrade.sh
+++ packagers/yarn3-workspaces-pnp/packages/sub-project-1/upgrade.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
-set -eux
+set -eu
 
-sh run.sh
+# Set Yarn to berry
+yarn set version berry
+
+# To set Yarn to 3.x
+yarn set version 3.x
+
+# To avoid this error 
+# YN0028: │ The lockfile would have been modified by this install, which is explicitly forbidden.
+YARN_ENABLE_IMMUTABLE_INSTALLS=0 yarn install
