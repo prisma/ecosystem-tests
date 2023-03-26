7dc019e881ae0babd33f413a752d49934d66b24b
Jan Piotrowski <piotrowski+github@gmail.com>
2023-03-26 16:12:34 +0000

    what happens if this test does not push to prod?

--- platforms-serverless/netlify-cli/run.sh
+++ platforms-serverless/netlify-cli/run.sh
@@ -22,7 +22,7 @@ mv index.zip ../functions-build/index.zip
 cd ..
 
 # deploy content of `functions-build` to Netlify
-yarn netlify deploy --dir=. --prod --functions=functions-build
+yarn netlify deploy --dir=. --functions=functions-build
 # TODO Use individual deployment URL and store
 
 # TODO Why is this done?
