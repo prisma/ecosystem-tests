05947c2134cd526bb895e0d4b11099ac01bed428
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2023-03-30 12:40:56 -1000

    attempt to fix windows path issues

--- binaries/pkg/run.sh
+++ binaries/pkg/run.sh
@@ -4,13 +4,9 @@ set -eu
 
 pnpm install
 
-# This is a hack to work around a problem that pkg would otherwise have with the Engines binaries missing in the package
-CLI_PACKAGE=$(node -e "console.log(path.dirname(require.resolve('prisma/package.json')).replace('/\\/g', '/'))")
-echo "CLI_PACKAGE: $CLI_PACKAGE"
-
+# This means that we resolve the location of the @prisma/engines package starting to the location of the prisma package
 ENGINES_PACKAGE=$(node -e "console.log(path.dirname(require.resolve('@prisma/engines/package.json', {paths: [path.dirname(require.resolve('prisma/package.json'))]})))")
 
-echo "ENGINES_PACKAGE: $ENGINES_PACKAGE"
-
-mkdir -p node_modules/prisma/node_modules/@prisma/engines
-cp -fR $ENGINES_PACKAGE/* node_modules/prisma/node_modules/@prisma/engines
+# This is a hack to work around a problem that pkg would otherwise have with the Engines binaries missing in the package
+mkdir -p node_modules/prisma/node_modules/@prisma
+cp -fr $ENGINES_PACKAGE node_modules/prisma/node_modules/@prisma
