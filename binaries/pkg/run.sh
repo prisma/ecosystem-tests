0c8c3ca2226128b447afb2ea762388fd2b3a9e8d
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2023-03-30 10:06:15 -1000

    debug pkg on windows

--- binaries/pkg/run.sh
+++ binaries/pkg/run.sh
@@ -6,7 +6,11 @@ pnpm install
 
 # This is a hack to work around a problem that pkg would otherwise have with the Engines binaries missing in the package
 CLI_PACKAGE=$(node -e "console.log(path.dirname(require.resolve('prisma/package.json')))")
+echo "CLI_PACKAGE: $CLI_PACKAGE"
+
 ENGINES_PACKAGE=$(node -e "console.log(path.dirname(require.resolve('@prisma/engines/package.json', {paths: ['$CLI_PACKAGE']})))")
+
+echo "ENGINES_PACKAGE: $ENGINES_PACKAGE"
 
 mkdir -p node_modules/prisma/node_modules/@prisma/engines
 cp -fR $ENGINES_PACKAGE/* node_modules/prisma/node_modules/@prisma/engines
