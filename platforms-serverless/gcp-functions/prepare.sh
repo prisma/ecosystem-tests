3e24d6794c9454ebe8cef1dd84611e9adfbeca30
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2023-03-31 12:27:58 -1000

    install latest google cloud

--- platforms-serverless/gcp-functions/prepare.sh
+++ platforms-serverless/gcp-functions/prepare.sh
@@ -2,6 +2,9 @@
 
 set -eux
 
+curl https://sdk.cloud.google.com > install.sh
+bash install.sh --disable-prompts
+
 printf "%s" "$GCP_FUNCTIONS_SECRET" > key.json
 gcloud auth activate-service-account --key-file key.json
 gcloud config set project "$GCP_FUNCTIONS_PROJECT"
