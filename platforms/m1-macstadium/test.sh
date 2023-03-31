fe763c07301152d97fbf255cf51f13cfb7908d23
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2023-03-30 16:02:39 -1000

    debug

--- platforms/m1-macstadium/test.sh
+++ platforms/m1-macstadium/test.sh
@@ -5,6 +5,8 @@ set -eux
 ssh -i ./server-key.pem administrator@207.254.29.83 -tt "
     cd /Users/administrator/e2e-tests/$GITHUB_JOB/$GITHUB_RUN_ID/$PRISMA_CLIENT_ENGINE_TYPE;
 
+    echo $PATH
+
     # to get around https://serverfault.com/questions/351731/why-does-the-path-of-an-ssh-remote-command-differ-from-that-of-an-interactive-s
     export PATH=/opt/homebrew/bin:/opt/homebrew/sbin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Users/administrator/.cargo/bin:/Users/administrator/.npm-global/bin
 
