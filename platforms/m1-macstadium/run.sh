a069ef66532bf41bf18a8124a6a371f233ec2329
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2023-03-30 15:45:17 -1000

    attempt to make macstad tests work

--- platforms/m1-macstadium/run.sh
+++ platforms/m1-macstadium/run.sh
@@ -7,7 +7,7 @@ sh sync-to-remote.sh
 if [ "$PRISMA_CLIENT_ENGINE_TYPE" == "binary" ]; then
   echo "N-API: Disabled"
   ssh -i ./server-key.pem administrator@207.254.29.83 -tt "
-      cd /Users/administrator/e2e-tests/$GITHUB_JOB/$GITHUB_RUN_ID;
+      cd /Users/administrator/e2e-tests/$GITHUB_JOB/$GITHUB_RUN_ID/$PRISMA_CLIENT_ENGINE_TYPE;
       export PRISMA_CLIENT_ENGINE_TYPE=\"binary\";
 
       # to get around https://serverfault.com/questions/351731/why-does-the-path-of-an-ssh-remote-command-differ-from-that-of-an-interactive-s
@@ -21,7 +21,7 @@ if [ "$PRISMA_CLIENT_ENGINE_TYPE" == "binary" ]; then
 else
   echo "N-API: Enabled"
   ssh -i ./server-key.pem administrator@207.254.29.83 -tt "
-      cd /Users/administrator/e2e-tests/$GITHUB_JOB/$GITHUB_RUN_ID;
+      cd /Users/administrator/e2e-tests/$GITHUB_JOB/$GITHUB_RUN_ID/$PRISMA_CLIENT_ENGINE_TYPE;
       export PRISMA_CLIENT_ENGINE_TYPE=\"library\";
 
       # to get around https://serverfault.com/questions/351731/why-does-the-path-of-an-ssh-remote-command-differ-from-that-of-an-interactive-s
