383d3fa87a15929897dfee110a9aa3cbb5fe5c1b
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2023-03-31 17:30:37 -1000

    update snapshots

--- platforms-serverless-vercel/vercel-cli/test.sh
+++ platforms-serverless-vercel/vercel-cli/test.sh
@@ -4,7 +4,7 @@ set -eux
 
 if [ "$PRISMA_CLIENT_ENGINE_TYPE" == "binary" ]; then
   echo "Binary"
-  files=',"files":["index.js","query-engine-rhel-openssl-1.0.x","package.json","schema.prisma"]'
+  files=',"files":["index.js","package.json","query-engine-rhel-openssl-1.0.x","schema.prisma"]'
   # TODO Use deployment, not production
   pnpm ts-node ../../utils/fetch-retry-and-confirm-version.ts --url https://e2e-vercel-api-binary.vercel.app/api --prisma-version $(sh ../../utils/prisma_version.sh) --binary-string $files
 else
