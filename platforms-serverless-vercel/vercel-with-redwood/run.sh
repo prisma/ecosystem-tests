43a37e5e805a09375c997326a7e9d79234d48ce4
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2023-03-31 17:18:30 -1000

    fix redwood

--- platforms-serverless-vercel/vercel-with-redwood/run.sh
+++ platforms-serverless-vercel/vercel-with-redwood/run.sh
@@ -2,8 +2,6 @@
 
 set -eu
 
-rm pnpm-lock.yaml
-
 # Yarn 1 has the following issue https://github.com/yarnpkg/yarn/issues/7807
 # which shows with the following error in our `check-for-update` job":
 # error An unexpected error occurred: "expected workspace package to exist for \"@babel/core\"".
