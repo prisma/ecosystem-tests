05e1d80f88e9962c52c2d1d18ee29eec97db59d1
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2023-03-31 16:30:04 -1000

    attempt to hide require from bundler

--- platforms-serverless-vercel/vercel-with-nextjs/pages/api/files.js
+++ platforms-serverless-vercel/vercel-with-nextjs/pages/api/files.js
@@ -1,13 +1,15 @@
 const path = require('path')
 
+const include = eval('require')
+
 export default async (req, res) => {
   res.statusCode = 200
   res.setHeader('Content-Type', 'application/json')
   const fs = require('fs')
   let files
   try {
-    const generatedClientDir = path.dirname(require.resolve('.prisma/client', {
-      paths: [path.dirname(require.resolve('@prisma/client'))]
+    const generatedClientDir = path.dirname(include.resolve('.prisma/client', {
+      paths: [path.dirname(include.resolve('@prisma/client'))]
     }))
     files = fs.readdirSync(generatedClientDir)
   } catch (e) {
