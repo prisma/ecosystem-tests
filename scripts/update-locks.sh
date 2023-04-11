ad7a7baf70dd048d42e098675db355dd6b1a4850
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2023-04-11 11:09:35 -1000

    remove npmrc files and use ni for detection

--- scripts/update-locks.sh
+++ scripts/update-locks.sh
@@ -1,27 +1,25 @@
 #! /bin/sh
 
-IS_YARN_SUBPROJECT=$(node -e "console.log('$(pwd)'.includes('yarn') || '$(pwd)'.includes('redwood'))")
+rm -f .pnp.cjs # remove pnp file to prevent nodejs from using it
+
+PROJECT_PACKAGE_MANAGER=$(node -e "require('@antfu/ni').detect({ autoinstall: false }).then(console.log)")
 IS_GENERATED_CLIENT=$(node -e "const pkg = require('./package.json'); console.log(pkg.name === '.prisma/client')")
 
 if [ "$IS_GENERATED_CLIENT" = "true" ]; then
     exit 0
 fi
 
-if [ ! -f "yarn.lock" ] && [ "$IS_YARN_SUBPROJECT" = "true" ]; then
-    exit 0
-fi
-
-if [ -f "yarn.lock" ]; then
+if [ "$PROJECT_PACKAGE_MANAGER" = "yarn" ] || [ "$PROJECT_PACKAGE_MANAGER" = "yarn@berry" ]; then
     YARN_ENABLE_SCRIPTS=0 yarn
     exit 0
 fi
 
-if [ -f "package-lock.json" ]; then
+if [ "$PROJECT_PACKAGE_MANAGER" = "npm" ]; then
     npm install --package-lock-only
     exit 0
 fi
 
-if [ -f "pnpm-lock.yaml" ]; then
+if [ "$PROJECT_PACKAGE_MANAGER" = "pnpm" ]; then
     pnpm install --lockfile-only
     exit 0
 fi
