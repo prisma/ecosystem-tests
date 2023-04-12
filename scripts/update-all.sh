7105881a2dcf231f290cfc606b8fdb612bc5b56d
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2023-04-12 11:47:43 -1000

    attempt: do not run lock update in parallel

--- scripts/update-all.sh
+++ scripts/update-all.sh
@@ -7,6 +7,6 @@ echo "$NEW_VERSION" > .github/prisma-version.txt
 # first update all the versions in all the projects for perf gains
 pnpm -rc --parallel exec "$(pwd)/scripts/update-version.sh $NEW_VERSION"
 # then update all the lockfiles by running the package managers
-pnpm -rc --parallel exec "$(pwd)/scripts/update-locks.sh $NEW_VERSION"
+pnpm -rc exec "$(pwd)/scripts/update-locks.sh $NEW_VERSION"
 # doing the two separately is important as it also allows us to update the
 # lockfiles in parallel, while also handling monorepo/workspaces correctly
