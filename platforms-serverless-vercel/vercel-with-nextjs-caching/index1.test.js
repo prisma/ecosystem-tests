a37d579a674afdf0f73324506bca170b988e1004
pierre <pierreantoine.urvoy@gmail.com>
2023-04-17 06:31:04 -1000

    test(vercel, netlify): schema caching error messages (#3454)

---
+++ platforms-serverless-vercel/vercel-with-nextjs-caching/index1.test.js
@@ -0,0 +1,26 @@
+const fetch = require('node-fetch')
+const fs = require('fs')
+
+function getDeploymentURL() {
+  const data = fs.readFileSync('./deployment-url.txt', { encoding: 'utf8' })
+  return data.trim()
+}
+
+const endpoint = getDeploymentURL()
+
+const DELAY = 5000
+let backOff = -DELAY
+
+test('simple query', async () => {
+  await new Promise((r) => setTimeout(r, backOff += DELAY))
+
+  const r = await fetch(endpoint + '/api')
+  const data = await r.json().catch((e) => e)
+
+  expect(data).toMatchObject({
+    value: expect.stringContaining(`We have detected that you've built your project on Vercel, which caches dependencies.`),
+  })
+})
+
+jest.retryTimes(10, { logErrorsBeforeRetry: true })
+jest.setTimeout(10 * DELAY)
