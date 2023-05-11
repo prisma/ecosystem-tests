241b8fa065a7288c27c979d45b9aa294a914003e
Joël Galeran <Jolg42@users.noreply.github.com>
2023-05-11 08:20:59 +0000

    rename deprecated env var names

--- dataproxy/cloudflare-workers/prepare.sh
+++ dataproxy/cloudflare-workers/prepare.sh
@@ -2,8 +2,8 @@
 
 export PRISMA_TELEMETRY_INFORMATION='ecosystem-tests dataproxy cloudflare-workers build'
 
-source ../../utils/crypto/setEnv.sh CF_ACCOUNT_ID CF_API_TOKEN CF_DATA_PROXY_URL
+source ../../utils/crypto/setEnv.sh CLOUDFLARE_ACCOUNT_ID CLOUDFLARE_API_TOKEN CLOUDFLARE_DATA_PROXY_URL
 
 # we add the data proxy URL into the configuration file directly
 cp -fr wrangler.base.toml wrangler.toml # needed for retries
-echo "CF_DATA_PROXY_URL=\"$CF_DATA_PROXY_URL\"" >> wrangler.toml
+echo "CLOUDFLARE_DATA_PROXY_URL=\"$CLOUDFLARE_DATA_PROXY_URL\"" >> wrangler.toml
