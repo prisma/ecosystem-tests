8355e278a7cc105cd72a3d575bea4c618ccc2687
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2023-05-11 05:31:26 -1000

    fix(notify): add retries for slack notifications

--- .github/slack/post.js
+++ .github/slack/post.js
@@ -1,11 +1,20 @@
 const { IncomingWebhook } = require('@slack/webhook')
 
-async function post(url, message) {
-  const webhook = new IncomingWebhook(url)
-  await webhook.send({
-    text: message,
-    icon_emoji: ':microscope:',
-  })
+async function post(url, message, retries = 0) {
+  try {
+    const webhook = new IncomingWebhook(url)
+    await webhook.send({
+      text: message,
+      icon_emoji: ':microscope:',
+    })
+  } catch (err) {
+    if (retries <= 3) {
+      await new Promise((r) => setTimeout(r, retries ** 2 * 1000))
+      await post(url, message, retries + 1)
+    } else {
+      throw err
+    }
+  }
 }
 
 module.exports = post
