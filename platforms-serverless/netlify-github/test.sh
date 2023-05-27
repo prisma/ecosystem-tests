72d15f97e26af31672f8c24914b6e43ca942fc2c
Jan Piotrowski <piotrowski+github@gmail.com>
2023-05-27 07:30:07 +0000

    fix(esm): Adapt list of expected files

--- platforms-serverless/netlify-github/test.sh
+++ platforms-serverless/netlify-github/test.sh
@@ -5,9 +5,9 @@ set -eux
 ID=$( tail -n 1 id.txt )
 
 if [ "$PRISMA_CLIENT_ENGINE_TYPE" == "binary" ]; then
-  files=',"files":["deno","edge.d.ts","edge.js","index-browser.js","index.d.ts","index.js","package.json","query-engine-rhel-openssl-1.0.x","schema.prisma"]'
+  files=',"files":["deno","edge.d.ts","edge.js","edge.mjs","index-browser.js","index-browser.mjs","index.d.ts","index.js","package.json","query-engine-rhel-openssl-1.0.x","schema.prisma"]'
 else
-  files=',"files":["deno","edge.d.ts","edge.js","index-browser.js","index.d.ts","index.js","libquery_engine-rhel-openssl-1.0.x.so.node","package.json","schema.prisma"]'
+  files=',"files":["deno","edge.d.ts","edge.js","edge.mjs","index-browser.js","index-browser.mjs","index.d.ts","index.js","libquery_engine-rhel-openssl-1.0.x.so.node","package.json","schema.prisma"]'
 fi
 
 pnpm ts-node ../../utils/fetch-retry-and-confirm-version.ts --url https://$ID--prisma-ecosystem-tests-netlify-github.netlify.app/.netlify/functions/index --prisma-version $(sh ../../utils/prisma_version.sh) --binary-string $files
