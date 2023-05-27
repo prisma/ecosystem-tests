20dbc3c554b7a1d0a7db68038c4d707581be148e
Jan Piotrowski <piotrowski+github@gmail.com>
2023-05-27 08:37:10 +0000

    fix heroku binary

--- platforms/heroku/test.sh
+++ platforms/heroku/test.sh
@@ -3,7 +3,7 @@
 set -eux
 
 if [ "$PRISMA_CLIENT_ENGINE_TYPE" == "binary" ]; then
-  files=',"files":["deno","edge.d.ts","edge.js","edge.mjs","index-browser.js","index-browser.mjs","index.d.ts","index.js","index.js","package.json","query-engine-debian-openssl-3.0.x","schema.prisma"]'
+  files=',"files":["deno","edge.d.ts","edge.js","edge.mjs","index-browser.js","index-browser.mjs","index.d.ts","index.js","index.mjs","package.json","query-engine-debian-openssl-3.0.x","schema.prisma"]'
   pnpm ts-node ../../utils/fetch-retry-and-confirm-version.ts --url https://e2e-platforms-heroku-binary.herokuapp.com --prisma-version $(sh ../../utils/prisma_version.sh) --binary-string $files
 else
   files=',"files":["deno","edge.d.ts","edge.js","edge.mjs","index-browser.js","index-browser.mjs","index.d.ts","index.js","index.mjs","libquery_engine-debian-openssl-3.0.x.so.node","package.json","schema.prisma"]'
