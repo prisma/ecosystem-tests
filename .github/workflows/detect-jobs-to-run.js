bd2ffe7c925461ea8c0e8b9bf1f3c75a231a60af
Joël Galeran <Jolg42@users.noreply.github.com>
2023-05-31 14:01:48 +0200

    chore: switch from ::set-output to GITHUB_OUTPUT (#3571)

--- .github/workflows/detect-jobs-to-run.js
+++ .github/workflows/detect-jobs-to-run.js
@@ -132,7 +132,11 @@ async function main() {
     filesChanged,
     GITHUB_REF,
   })
-  console.log('::set-output name=jobs::' + JSON.stringify(jobsToRun))
+
+  if (typeof process.env.GITHUB_OUTPUT == 'string' && process.env.GITHUB_OUTPUT.length > 0) {
+    fs.appendFileSync(process.env.GITHUB_OUTPUT, `jobs=${JSON.stringify(jobsToRun)}\n`)
+    console.debug('jobsToRun added to GITHUB_OUTPUT')
+  }
 }
 
 // Only run if from command line
