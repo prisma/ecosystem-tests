cd30caf1b5864269f5c948e35c50d9579fcbd8b5
Joël Galeran <Jolg42@users.noreply.github.com>
2023-06-02 14:14:19 +0200

    chore(cloudflare-workers): use types (#3584)

--- dataproxy/cloudflare-workers/run.sh
+++ dataproxy/cloudflare-workers/run.sh
@@ -6,4 +6,4 @@ pnpm install
 
 pnpm prisma generate --data-proxy
 
-pnpm wrangler publish | tee deployment-url.txt
+pnpm wrangler deploy | tee deployment-url.txt
--- dataproxy/cloudflare-workers/src/index.ts
+++ dataproxy/cloudflare-workers/src/index.ts
@@ -1,29 +1,27 @@
-// We should use https://www.npmjs.com/package/@cloudflare/workers-types
+// Note: see wrangler.toml and https://www.npmjs.com/package/@cloudflare/workers-types for the version
+/// <reference types="@cloudflare/workers-types/2022-08-04" />
 import { PrismaClient } from '@prisma/client/edge'
 
 const prisma = new PrismaClient({})
 
 async function getUsers() {
-  console.debug(new Date(), "Start await prisma.$transaction")
-  console.time('transactionTook');
-  const data = await prisma.$transaction([
-    prisma.user.findFirst(),
-    prisma.user.findMany()
-  ])
-  console.timeEnd('transactionTook');
-  console.debug(new Date(), "End await prisma.$transaction")
+  console.debug(new Date(), 'Start await prisma.$transaction')
+  console.time('transactionTook')
+  const data = await prisma.$transaction([prisma.user.findFirst(), prisma.user.findMany()])
+  console.timeEnd('transactionTook')
+  console.debug(new Date(), 'End await prisma.$transaction')
 
   const json = JSON.stringify({ data })
 
   return new Response(json, {
     status: 200,
     headers: {
-      "content-type": "application/json;charset=UTF-8"
-    }
+      'content-type': 'application/json;charset=UTF-8',
+    },
   })
 }
 
-addEventListener("fetch", (event: FetchEvent) => {
+addEventListener('fetch', (event: FetchEvent) => {
   if (new URL(event.request.url).pathname === '/') {
     return event.respondWith(getUsers())
   }
