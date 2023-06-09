821513a9beeed5328de11c391d546cb57ce9a001
Joël Galeran <Jolg42@users.noreply.github.com>
2023-06-09 17:02:45 +0200

    Update run.sh

--- platforms/heroku/run.sh
+++ platforms/heroku/run.sh
@@ -4,7 +4,6 @@ set -eux
 
 export PRISMA_TELEMETRY_INFORMATION='ecosystem-tests platforms heroku build'
 
-git init
 git config --global user.email "prismabots@gmail.com"
 git config --global user.name "Prismo"
 
@@ -12,26 +11,23 @@ git config --global user.name "Prismo"
 if [ "$PRISMA_CLIENT_ENGINE_TYPE" == "binary" ]; then
   echo "Using Binary enabled schema"
   cp ./prisma/schema-with-binary.prisma ./prisma/schema.prisma
-  git remote add heroku "https://ignored_user:`heroku auth:token`@git.heroku.com/e2e-platforms-heroku-binary.git" || git remote set-url heroku "https://ignored_user:`heroku auth:token`@git.heroku.com/e2e-platforms-heroku-binary.git" || true
 else
   echo "Using Node-API enabled schema"
   cp ./prisma/schema-with-node-api.prisma ./prisma/schema.prisma
-  git remote add heroku "https://ignored_user:`heroku auth:token`@git.heroku.com/e2e-platforms-heroku.git" || git remote set-url heroku "https://ignored_user:`heroku auth:token`@git.heroku.com/e2e-platforms-heroku.git" || true
 fi
 
 pnpm install
-
-git pull heroku master
-# Remove all files from git
-# So we don't keep deleted filed
-git rm -r '*'
-
 pnpm prisma generate
 
+git clone "https://ignored_user:`heroku auth:token`@git.heroku.com/e2e-platforms-heroku-binary.git" heroku_clone
+cd heroku_clone
+# Remove all files from git, so we don't keep deleted files
+git rm -r '*'
+cp ../prisma .gitignore ../index.js ../package.json ../pnpm-lock.yaml ./
 # Add files to git
 git add .
-
 git commit -m "push to heroku"
 git push heroku master --force
+cd ../
 
-rm -rf .git
+rm -rf heroku_clone
