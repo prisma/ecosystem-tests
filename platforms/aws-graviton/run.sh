313162738d9e2d54e811baeaf63d32e8456c26b0
Joël Galeran <Jolg42@users.noreply.github.com>
2023-07-06 20:10:26 +0200

    test(graviton): add export CI="true" (#3667)

--- platforms/aws-graviton/run.sh
+++ platforms/aws-graviton/run.sh
@@ -8,10 +8,11 @@ if [ "$PRISMA_CLIENT_ENGINE_TYPE" == "binary" ]; then
   echo "Node-API: Disabled"
   ssh -i ./server-key.pem ec2-user@ec2-54-209-135-27.compute-1.amazonaws.com -tt "
       cd /home/ec2-user/aws-graviton/$GITHUB_JOB/$GITHUB_RUN_ID/$PRISMA_CLIENT_ENGINE_TYPE;
+      export CI=\"true\"
       export PRISMA_CLIENT_ENGINE_TYPE=\"binary\"
       rm -rf ./node_modules;
       npm i -g pnpm@7;
-      pnpm install --reporter=append-only;
+      pnpm install;
       pnpm prisma generate;
       pnpm prisma -v;
   "
@@ -19,10 +20,11 @@ else
   echo "Node-API: Enabled"
   ssh -i ./server-key.pem ec2-user@ec2-54-209-135-27.compute-1.amazonaws.com -tt "
       cd /home/ec2-user/aws-graviton/$GITHUB_JOB/$GITHUB_RUN_ID/$PRISMA_CLIENT_ENGINE_TYPE;
+      export CI=\"true\"
       export PRISMA_CLIENT_ENGINE_TYPE=\"library\"
       rm -rf ./node_modules;
       npm i -g pnpm@7;
-      pnpm install --reporter=append-only;
+      pnpm install;
       pnpm prisma generate;
       pnpm prisma -v;
   "
