1753bdde93e4c01204ab56e4d541d9e018bb1d8d
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2023-07-07 07:30:37 -1000

    enable dp2+extension in matrix

--- dataproxy/vercel-edge-functions/run.sh
+++ dataproxy/vercel-edge-functions/run.sh
@@ -4,4 +4,8 @@ set -eu
 
 pnpm install
 
-pnpm vercel deploy --prod --yes --force --token=$VERCEL_TOKEN --env DATAPROXY_COMMON_URL="$DATAPROXY_COMMON_URL" --env PRISMA_GENERATE_FLAG="$PRISMA_GENERATE_FLAG" --scope=$VERCEL_ORG_ID 1> deployment-url.txt
+pnpm vercel deploy --prod --yes --force \
+--token=$VERCEL_TOKEN \
+--env DATAPROXY_COMMON_URL="$DATAPROXY_COMMON_URL" \
+--env PRISMA_GENERATE_FLAG="$PRISMA_GENERATE_FLAG" \
+--scope=$VERCEL_ORG_ID 1> deployment-url.txt
