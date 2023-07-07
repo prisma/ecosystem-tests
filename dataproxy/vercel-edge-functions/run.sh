62d79eb5ff7a52e0f960c88a5db158889542509e
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2023-07-07 10:06:52 -1000

    pass generate blag to buildenv

--- dataproxy/vercel-edge-functions/run.sh
+++ dataproxy/vercel-edge-functions/run.sh
@@ -8,5 +8,5 @@ pnpm vercel deploy --prod --yes --force \
 --token=$VERCEL_TOKEN \
 --scope=$VERCEL_ORG_ID \
 --env DATAPROXY_COMMON_URL="$DATAPROXY_COMMON_URL" \
---env PRISMA_GENERATE_FLAG="$PRISMA_GENERATE_FLAG" \
+--build-env PRISMA_GENERATE_FLAG="$PRISMA_GENERATE_FLAG" \
 1> deployment-url.txt
