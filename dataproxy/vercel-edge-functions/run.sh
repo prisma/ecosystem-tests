c5d2d81f8a411e7a18303b6dbefc8a9192ff5717
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2023-07-07 07:58:39 -1000

    attempt to fix concurrency

--- dataproxy/vercel-edge-functions/run.sh
+++ dataproxy/vercel-edge-functions/run.sh
@@ -6,6 +6,7 @@ pnpm install
 
 pnpm vercel deploy --prod --yes --force \
 --token=$VERCEL_TOKEN \
+--scope=$VERCEL_ORG_ID \
 --env DATAPROXY_COMMON_URL="$DATAPROXY_COMMON_URL" \
 --env PRISMA_GENERATE_FLAG="$PRISMA_GENERATE_FLAG" \
---scope=$VERCEL_ORG_ID 1> deployment-url.txt
+1> deployment-url.txt
