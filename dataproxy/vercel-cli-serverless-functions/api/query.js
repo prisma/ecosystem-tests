1e09c7f9e33f67b0bf4172846d05f404ddd532cc
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2023-07-07 15:30:10 -1000

    change import to require

--- dataproxy/vercel-cli-serverless-functions/api/query.js
+++ dataproxy/vercel-cli-serverless-functions/api/query.js
@@ -1,5 +1,5 @@
-import { PrismaClient } from '@prisma/client'
-import { withAccelerate } from '@prisma/extension-accelerate'
+const { PrismaClient } = require('@prisma/client')
+const { withAccelerate } = require('@prisma/extension-accelerate')
 
 let prisma = new PrismaClient()
 
