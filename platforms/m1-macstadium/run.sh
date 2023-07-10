724931d5f8f1faf66cee7b3a7fb3ca0f978eb66f
Joël Galeran <Jolg42@users.noreply.github.com>
2023-07-10 16:06:58 +0200

    test(m1-macstadium): add export CI=\"true\"

--- platforms/m1-macstadium/run.sh
+++ platforms/m1-macstadium/run.sh
@@ -9,7 +9,8 @@ if [ "$PRISMA_CLIENT_ENGINE_TYPE" == "binary" ]; then
   ssh -i ./server-key.pem administrator@207.254.29.83 -tt "
       cd /Users/administrator/e2e-tests/$GITHUB_JOB/$GITHUB_RUN_ID/$PRISMA_CLIENT_ENGINE_TYPE;
       export PRISMA_CLIENT_ENGINE_TYPE=\"binary\";
-
+      export CI=\"true\";
+      
       # to get around https://serverfault.com/questions/351731/why-does-the-path-of-an-ssh-remote-command-differ-from-that-of-an-interactive-s
       export PATH=/Users/administrator/.nvm/versions/node/v16.10.0/bin:/opt/homebrew/bin:/opt/homebrew/sbin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Users/administrator/.cargo/bin
 
@@ -23,7 +24,8 @@ else
   ssh -i ./server-key.pem administrator@207.254.29.83 -tt "
       cd /Users/administrator/e2e-tests/$GITHUB_JOB/$GITHUB_RUN_ID/$PRISMA_CLIENT_ENGINE_TYPE;
       export PRISMA_CLIENT_ENGINE_TYPE=\"library\";
-
+      export CI=\"true\";
+      
       # to get around https://serverfault.com/questions/351731/why-does-the-path-of-an-ssh-remote-command-differ-from-that-of-an-interactive-s
       export PATH=/Users/administrator/.nvm/versions/node/v16.10.0/bin:/opt/homebrew/bin:/opt/homebrew/sbin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Users/administrator/.cargo/bin
 
