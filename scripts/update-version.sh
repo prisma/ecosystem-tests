572521cbcfe0882ad16d61901d3c59c1a60865de
Joël Galeran <Jolg42@users.noreply.github.com>
2023-08-07 15:18:13 +0200

    ci(@prisma/instrumentation): update pkg like CLI/Client
    Goodbye Renovate PRs like https://github.com/prisma/ecosystem-tests/pull/3771

--- scripts/update-version.sh
+++ scripts/update-version.sh
@@ -1,7 +1,9 @@
 #! /bin/sh
 
-PRISMA_VERSION=$(node -e "const pkg = require('./package.json'); console.log(pkg?.devDependencies?.['prisma'] || pkg?.dependencies?.['prisma'] || pkg?.resolutions?.['prisma'] || '')")
-CLIENT_VERSION=$(node -e "const pkg = require('./package.json'); console.log(pkg?.devDependencies?.['@prisma/client'] || pkg?.dependencies?.['@prisma/client'] || pkg?.resolutions?.['@prisma/client'] || '')")
+CLI_PKG_VERSION=$(node -e "const pkg = require('./package.json'); console.log(pkg?.devDependencies?.['prisma'] || pkg?.dependencies?.['prisma'] || pkg?.resolutions?.['prisma'] || '')")
+CLIENT_PKG_VERSION=$(node -e "const pkg = require('./package.json'); console.log(pkg?.devDependencies?.['@prisma/client'] || pkg?.dependencies?.['@prisma/client'] || pkg?.resolutions?.['@prisma/client'] || '')")
+INSTRUMENTATION_PKG_VERSION=$(node -e "const pkg = require('./package.json'); console.log(pkg?.devDependencies?.['@prisma/instrumentation'] || pkg?.dependencies?.['@prisma/instrumentation'] || pkg?.resolutions?.['@prisma/instrumentation'] || '')")
 
-if [ -n "$PRISMA_VERSION" ]; then sed -i "s/$PRISMA_VERSION/$1/g" package.json; fi
-if [ -n "$CLIENT_VERSION" ]; then sed -i "s/$CLIENT_VERSION/$1/g" package.json; fi
+if [ -n "$CLI_PKG_VERSION" ]; then sed -i "s/$CLI_PKG_VERSION/$1/g" package.json; fi
+if [ -n "$CLIENT_PKG_VERSION" ]; then sed -i "s/$CLIENT_PKG_VERSION/$1/g" package.json; fi
+if [ -n "$INSTRUMENTATION_PKG_VERSION" ]; then sed -i "s/$INSTRUMENTATION_PKG_VERSION/$1/g" package.json; fi
