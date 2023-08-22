b865d90a6d4cb54de3a186f6839a88290e4085bd
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2023-08-21 16:47:06 -1000

    fix(bundlers): adapt webpack browser test

--- bundlers/webpack-browser-custom-output/index.test.js
+++ bundlers/webpack-browser-custom-output/index.test.js
@@ -1,5 +1,9 @@
 const { PrismaClient } = require('./dist/prismaTest')
 
-test('correctly generates browser bundle for prisma at custom path', () => {
-  expect(() => new PrismaClient()).toThrow('PrismaClient is unable to be run in the browser')
+test('correctly generates browser bundle for prisma at custom path', async () => {
+  await expect(new PrismaClient().user.findFirst()).rejects.toThrow(
+    expect.objectContaining({
+      message: expect.stringContaining('PrismaClient is unable to run in'),
+    }),
+  )
 })
