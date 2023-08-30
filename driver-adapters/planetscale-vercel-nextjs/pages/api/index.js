c9dcae6dc5c4f0b0c617cde2f31c5f002a7241c1
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2023-08-30 07:58:03 -1000

    test(client): driver adapters (neon, planetscale, pg) on vercel via next.js

---
+++ driver-adapters/planetscale-vercel-nextjs/pages/api/index.js
@@ -0,0 +1,21 @@
+import { Prisma, PrismaClient } from '@prisma/client'
+import { createPlanetScaleConnector } from '@jkomyno/prisma-planetscale-js-connector'
+
+const connectionString = process.env.DRIVER_ADAPTERS_PLANETSCALE_VERCEL_NEXTJS_DATABASE_URL
+
+const jsConnector = createPlanetScaleConnector({
+  url: connectionString,
+})
+
+const prisma = new PrismaClient({ jsConnector })
+
+export default async (req, res) => {
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'application/json')
+  const users = await prisma.user.findMany()
+
+  res.status(200).json({
+    prismaVersion: Prisma.prismaVersion.client,
+    users
+  })
+}
