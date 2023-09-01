c768a454af70539f4528dd68b3b55b87bdaa4019
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2023-09-01 11:00:23 -1000

    upload jkomyno to lambda

--- driver-adapters/postgresql-lambda-basic/run.sh
+++ driver-adapters/postgresql-lambda-basic/run.sh
@@ -6,7 +6,7 @@ pnpm install
 pnpm prisma generate
 
 rm -rf lambda.zip
-zip --symlinks -r lambda.zip index.js prisma/schema.prisma node_modules/@prisma/client node_modules/.pnpm/@prisma+client*
+zip --symlinks -r lambda.zip index.js prisma/schema.prisma node_modules/@prisma/client node_modules/@jkomyno node_modules/.pnpm/@prisma+client* node_modules/.pnpm/@jkomyno*
 
 aws lambda update-function-configuration --function-name driver-adapters-postgresql-lambda-basic --runtime nodejs18.x --environment "Variables={DATABASE_URL=$DATABASE_URL}" --timeout 10
 aws lambda update-function-code --function-name driver-adapters-postgresql-lambda-basic --zip-file "fileb://lambda.zip"
