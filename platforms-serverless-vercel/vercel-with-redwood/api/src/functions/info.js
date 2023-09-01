f5ea97b8c975631b50adc3edc45838caa36a3ded
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2023-08-31 14:22:01 -1000

    simple function

--- platforms-serverless-vercel/vercel-with-redwood/api/src/functions/info.js
+++ platforms-serverless-vercel/vercel-with-redwood/api/src/functions/info.js
@@ -1,34 +1,21 @@
-import { Prisma as PrismaNamespace } from '@prisma/client'
 import { logger } from 'src/lib/logger'
-import { db } from 'src/lib/db'
 
-export const users = () => {
-  return db.user.findMany()
-}
-
-export const user = ({ id }) => {
-  return db.user.findUnique({
-    where: { id },
-  })
-}
-
-export const prismaVersion = () => {
-  return PrismaNamespace.prismaVersion.client
-}
-export const files = () => {
-  // list all files in node_modules/.prisma/client
-  const fs = require('fs')
-  const path = require('path')
-  let files
-  try {
-    files = fs.readdirSync(path.dirname(require.resolve('.prisma/client')))
-  } catch (e) {
-    files = [e.message]
-  }
-  return files
-}
-
-
+/**
+ * The handler function is your code that processes http request events.
+ * You can use return and throw to send a response or error, respectively.
+ *
+ * Important: When deployed, a custom serverless function is an open API endpoint and
+ * is your responsibility to secure appropriately.
+ *
+ * @see {@link https://redwoodjs.com/docs/serverless-functions#security-considerations|Serverless Function Considerations}
+ * in the RedwoodJS documentation for more information.
+ *
+ * @typedef { import('aws-lambda').APIGatewayEvent } APIGatewayEvent
+ * @typedef { import('aws-lambda').Context } Context
+ * @param { APIGatewayEvent } event - an object which contains information from the invoker.
+ * @param { Context } context - contains information about the invocation,
+ * function, and execution environment.
+ */
 export const handler = async (event, _context) => {
   logger.info(`${event.httpMethod} ${event.path}: info function`)
 
@@ -38,9 +25,7 @@ export const handler = async (event, _context) => {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({
-      users: await users(),
-      files: files(),
-      prismaVersion: prismaVersion(),
+      data: 'info function',
     }),
   }
 }
