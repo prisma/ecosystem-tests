7a8806dc924720ac6f1fcdecce8f1ba82aba4254
pierre <pierreantoine.urvoy@gmail.com>
2023-09-01 09:15:02 -1000

    test(client): driver adapters on vercel via next.js (neon, planetscale, pg) (#3875)

---
+++ driver-adapters/postgresql-vercel-nextjs/run.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+set -eu
+
+export PRISMA_TELEMETRY_INFORMATION='ecosystem-tests driver-adapters postgresql-vercel-nextjs build'
+
+pnpm install
+
+export VERCEL_ORG_ID=$VERCEL_ORG_ID
+export VERCEL_PROJECT_ID=$DRIVER_ADAPTERS_POSTGRESQL_VERCEL_NEXTJS_PROJECT_ID
+
+echo "VERCEL_ORG_ID: $VERCEL_ORG_ID"
+echo "VERCEL_PROJECT_ID: $VERCEL_PROJECT_ID"
+echo "PRISMA_CLIENT_ENGINE_TYPE: $PRISMA_CLIENT_ENGINE_TYPE"
+
+pnpm vercel deploy \
+--prod --yes --force \
+--token=$VERCEL_TOKEN \
+--build-env DEBUG="prisma:*" \
+--build-env PRISMA_CLIENT_ENGINE_TYPE="$PRISMA_CLIENT_ENGINE_TYPE" \
+--env DATABASE_URL=$DATABASE_URL \
+--scope=$VERCEL_ORG_ID 1> deployment-url.txt
+
+echo ''
+cat deployment-url.txt
+DEPLOYED_URL=$( tail -n 1 deployment-url.txt )
+echo ''
+echo "Deployed to ${DEPLOYED_URL}"
+
+sleep 15
