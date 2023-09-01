bd0756f60103871b964218629c1d9786c210d0d0
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2023-09-01 10:49:50 -1000

    fix flakiness wrt ordering in results

--- driver-adapters/postgresql-lambda-basic/index.test.js
+++ driver-adapters/postgresql-lambda-basic/index.test.js
@@ -30,7 +30,7 @@ Object {
 }
 `)
   expect(regResult.createMany.count).toBe(2)
-  expect(regResult.findMany).toMatchInlineSnapshot(`
+  expect(regResult.findMany.sort((a, b) => a.email > b.email ? 1 : -1)).toMatchInlineSnapshot(`
 Array [
   Object {
     "age": 27,
---
+++ driver-adapters/postgresql-lambda-basic/test.js
@@ -0,0 +1,19 @@
+const users = [
+{
+  "age": 29,
+  "email": "test-2@prisma.io",
+  "name": "Test 2",
+},
+{
+  "age": 27,
+  "email": "test-1@prisma.io",
+  "name": "Test 1",
+},
+{
+  "age": 29,
+  "email": "test-3@prisma.io",
+  "name": "Test 3",
+}
+]
+
+console.log(users)
