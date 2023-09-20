740e98f3ef22eeac5bd1c251c01f10ec8abfdd88
Jan Piotrowski <piotrowski+github@gmail.com>
2023-09-20 16:18:16 +0000

    try to catch, so on error we have some info

--- driver-adapters/neon-http-lambda-basic/index.test.js
+++ driver-adapters/neon-http-lambda-basic/index.test.js
@@ -14,102 +14,108 @@ test('prisma version and output', async () => {
     Payload: '""',
   })
 
-  const { regResult, itxResult } = JSON.parse(JSON.parse(new TextDecoder().decode(response.Payload)).body)
-
-  expect(regResult).toEqual(itxResult)
-  expect(regResult.prismaVersion).toMatch(dependencies['@prisma/client'])
-  expect(regResult.deleteMany.count).toBe(0)
-  expect(regResult.create).toMatchInlineSnapshot(`
-Object {
-  "age": 27,
-  "email": "test-1@prisma.io",
-  "name": "Test 1",
-}
-`)
-  expect(regResult.createMany.count).toBe(2)
-  expect(regResult.findMany).toMatchInlineSnapshot(`
-Array [
+  try {
+    const { regResult, itxResult } = JSON.parse(JSON.parse(new TextDecoder().decode(response.Payload)).body)
+  
+    expect(regResult).toEqual(itxResult)
+    expect(regResult.prismaVersion).toMatch(dependencies['@prisma/client'])
+    expect(regResult.deleteMany.count).toBe(0)
+    expect(regResult.create).toMatchInlineSnapshot(`
   Object {
     "age": 27,
     "email": "test-1@prisma.io",
     "name": "Test 1",
-  },
+  }
+  `)
+    expect(regResult.createMany.count).toBe(2)
+    expect(regResult.findMany).toMatchInlineSnapshot(`
+  Array [
+    Object {
+      "age": 27,
+      "email": "test-1@prisma.io",
+      "name": "Test 1",
+    },
+    Object {
+      "age": 29,
+      "email": "test-2@prisma.io",
+      "name": "Test 2",
+    },
+    Object {
+      "age": 29,
+      "email": "test-3@prisma.io",
+      "name": "Test 3",
+    },
+  ]
+  `)
+    expect(regResult.findUnique).toMatchInlineSnapshot(`
+  Object {
+    "age": 27,
+    "email": "test-1@prisma.io",
+    "name": "Test 1",
+  }
+  `)
+    expect(regResult.update).toMatchInlineSnapshot(`
+  Object {
+    "age": 26,
+    "email": "test-1@prisma.io",
+    "name": "Test 1",
+  }
+  `)
+    expect(regResult.updateMany.count).toBe(1)
+    expect(regResult.findFirst).toMatchInlineSnapshot(`
+  Object {
+    "age": 27,
+    "email": "test-1@prisma.io",
+    "name": "Test 1",
+  }
+  `)
+    expect(regResult.delete).toMatchInlineSnapshot(`
+  Object {
+    "age": 27,
+    "email": "test-1@prisma.io",
+    "name": "Test 1",
+  }
+  `)
+    expect(regResult.count).toBe(2)
+    expect(regResult.aggregate).toMatchInlineSnapshot(`
+  Object {
+    "age": 29,
+  }
+  `)
+    expect(regResult.groupBy).toMatchInlineSnapshot(`
+  Array [
+    Object {
+      "_count": Object {
+        "age": 2,
+      },
+      "age": 29,
+    },
+  ]
+  `)
+    expect(regResult.findFirstOrThrow).toMatchInlineSnapshot(`
   Object {
     "age": 29,
     "email": "test-2@prisma.io",
     "name": "Test 2",
-  },
+  }
+  `)
+    expect(regResult.findUniqueOrThrow).toMatchInlineSnapshot(`
   Object {
     "age": 29,
-    "email": "test-3@prisma.io",
-    "name": "Test 3",
-  },
-]
-`)
-  expect(regResult.findUnique).toMatchInlineSnapshot(`
-Object {
-  "age": 27,
-  "email": "test-1@prisma.io",
-  "name": "Test 1",
-}
-`)
-  expect(regResult.update).toMatchInlineSnapshot(`
-Object {
-  "age": 26,
-  "email": "test-1@prisma.io",
-  "name": "Test 1",
-}
-`)
-  expect(regResult.updateMany.count).toBe(1)
-  expect(regResult.findFirst).toMatchInlineSnapshot(`
-Object {
-  "age": 27,
-  "email": "test-1@prisma.io",
-  "name": "Test 1",
-}
-`)
-  expect(regResult.delete).toMatchInlineSnapshot(`
-Object {
-  "age": 27,
-  "email": "test-1@prisma.io",
-  "name": "Test 1",
-}
-`)
-  expect(regResult.count).toBe(2)
-  expect(regResult.aggregate).toMatchInlineSnapshot(`
-Object {
-  "age": 29,
-}
-`)
-  expect(regResult.groupBy).toMatchInlineSnapshot(`
-Array [
+    "email": "test-2@prisma.io",
+    "name": "Test 2",
+  }
+  `)
+    expect(regResult.upsert).toMatchInlineSnapshot(`
   Object {
-    "_count": Object {
-      "age": 2,
-    },
-    "age": 29,
-  },
-]
-`)
-  expect(regResult.findFirstOrThrow).toMatchInlineSnapshot(`
-Object {
-  "age": 29,
-  "email": "test-2@prisma.io",
-  "name": "Test 2",
-}
-`)
-  expect(regResult.findUniqueOrThrow).toMatchInlineSnapshot(`
-Object {
-  "age": 29,
-  "email": "test-2@prisma.io",
-  "name": "Test 2",
-}
-`)
-  expect(regResult.upsert).toMatchInlineSnapshot(`
-Object {
-  "age": 30,
-  "email": "test-4@prisma.io",
-  "name": "Test 4",
-}
-`)
+    "age": 30,
+    "email": "test-4@prisma.io",
+    "name": "Test 4",
+  }
+  `)
+  } catch(e) {
+    console.log({ e })
+    console.log(response.Payload)
+  }
+
 })
