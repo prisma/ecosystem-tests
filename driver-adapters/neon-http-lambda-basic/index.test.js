1bb0cc0bc8956705f8fd120103191375b0db7b67
Jan Piotrowski <piotrowski+github@gmail.com>
2023-09-26 16:46:36 +0200

    feat: Driver Adapter Neon HTTP tests (#3981)
    Co-authored-by: Joël Galeran <Jolg42@users.noreply.github.com>

---
+++ driver-adapters/neon-http-lambda-basic/index.test.js
@@ -0,0 +1,121 @@
+const { Lambda } = require("@aws-sdk/client-lambda")
+const { dependencies } = require('./package.json')
+
+const lambda = new Lambda({
+  region: process.env.AWS_DEFAULT_REGION,
+})
+
+jest.setTimeout(30000)
+
+test('prisma version and output', async () => {
+  const response = await lambda.invoke({
+    FunctionName: 'driver-adapters-neon-http-lambda-basic',
+    InvocationType: 'RequestResponse',
+    Payload: '""',
+  })
+
+  try {
+    const { regResult, itxResult } = JSON.parse(JSON.parse(new TextDecoder().decode(response.Payload)).body)
+  
+    expect(regResult).toEqual(itxResult)
+    expect(regResult.prismaVersion).toMatch(dependencies['@prisma/client'])
+    expect(regResult.deleteMany.count).toBe(0)
+    expect(regResult.create).toMatchInlineSnapshot(`
+  Object {
+    "age": 27,
+    "email": "test-1@prisma.io",
+    "name": "Test 1",
+  }
+  `)
+    expect(regResult.createMany.count).toBe(2)
+    expect(regResult.findMany).toMatchInlineSnapshot(`
+  Array [
+    Object {
+      "age": 27,
+      "email": "test-1@prisma.io",
+      "name": "Test 1",
+    },
+    Object {
+      "age": 29,
+      "email": "test-2@prisma.io",
+      "name": "Test 2",
+    },
+    Object {
+      "age": 29,
+      "email": "test-3@prisma.io",
+      "name": "Test 3",
+    },
+  ]
+  `)
+    expect(regResult.findUnique).toMatchInlineSnapshot(`
+  Object {
+    "age": 27,
+    "email": "test-1@prisma.io",
+    "name": "Test 1",
+  }
+  `)
+    expect(regResult.update).toMatchInlineSnapshot(`
+  Object {
+    "age": 26,
+    "email": "test-1@prisma.io",
+    "name": "Test 1",
+  }
+  `)
+    expect(regResult.updateMany.count).toBe(1)
+    expect(regResult.findFirst).toMatchInlineSnapshot(`
+  Object {
+    "age": 27,
+    "email": "test-1@prisma.io",
+    "name": "Test 1",
+  }
+  `)
+    expect(regResult.delete).toMatchInlineSnapshot(`
+  Object {
+    "age": 27,
+    "email": "test-1@prisma.io",
+    "name": "Test 1",
+  }
+  `)
+    expect(regResult.count).toBe(2)
+    expect(regResult.aggregate).toMatchInlineSnapshot(`
+  Object {
+    "age": 29,
+  }
+  `)
+    expect(regResult.groupBy).toMatchInlineSnapshot(`
+  Array [
+    Object {
+      "_count": Object {
+        "age": 2,
+      },
+      "age": 29,
+    },
+  ]
+  `)
+    expect(regResult.findFirstOrThrow).toMatchInlineSnapshot(`
+  Object {
+    "age": 29,
+    "email": "test-2@prisma.io",
+    "name": "Test 2",
+  }
+  `)
+    expect(regResult.findUniqueOrThrow).toMatchInlineSnapshot(`
+  Object {
+    "age": 29,
+    "email": "test-2@prisma.io",
+    "name": "Test 2",
+  }
+  `)
+    expect(regResult.upsert).toMatchInlineSnapshot(`
+  Object {
+    "age": 30,
+    "email": "test-4@prisma.io",
+    "name": "Test 4",
+  }
+  `)
+  } catch(e) {
+    console.log(new TextDecoder().decode(response.Payload))
+    throw e
+  }
+
+})
