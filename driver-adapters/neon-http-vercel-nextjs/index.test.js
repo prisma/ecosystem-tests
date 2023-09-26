96ce0ffe8116d48198c522defe241c6a5abc7e0a
Jan Piotrowski <piotrowski+github@gmail.com>
2023-09-26 20:51:34 +0000

    fix

--- driver-adapters/neon-http-vercel-nextjs/index.test.js
+++ driver-adapters/neon-http-vercel-nextjs/index.test.js
@@ -29,7 +29,7 @@ Object {
   "name": "Test 1",
 }
 `)
-  // expect(regResult.createMany.count).toBe(2)
+  expect(regResult.createMany.count).toBe(2)
   expect(regResult.findMany).toMatchInlineSnapshot(`
 Array [
   Object {
