4c45b83ecc517aa3be4bf2ad3d8e26ce204493a7
Joël Galeran <Jolg42@users.noreply.github.com>
2023-10-24 10:49:36 +0200

    Update run.sh

--- packagers/yarn3-workspaces-pnp/run.sh
+++ packagers/yarn3-workspaces-pnp/run.sh
@@ -2,6 +2,16 @@
 
 set -eu
 
+# YARN_IGNORE_NODE=1 is needed to ignore the Node.js version 
+# since the latest version needs v18+
+# TODO: remove once we switched to v18
+export YARN_IGNORE_NODE=1 
+
+# We must set yarn to berry first
+# if not this error will show up later
+# error An unexpected error occurred: "Release not found: 3.x".
+yarn set version berry
+
 # To set Yarn to 3.x
 yarn set version 3.x
 
