f22a03a35d7aea6556f1a2775c392d6ab6c09fcd
Sergey Tatarintsev <tatarintsev@prisma.io>
2023-11-10 12:25:58 +0100

    fix: New planetscale initialization
    See prisma/prisma#21883

--- driver-adapters/planetscale-node-basic/index.js
+++ driver-adapters/planetscale-node-basic/index.js
@@ -1,12 +1,12 @@
 // @ts-check
 const { Prisma, PrismaClient } = require('@prisma/client')
-const { connect } = require('@planetscale/database')
+const { Client } = require('@planetscale/database')
 const { PrismaPlanetScale } = require('@prisma/adapter-planetscale')
 
 const connectionString = process.env.DRIVER_ADAPTERS_PLANETSCALE_NODE_BASIC_DATABASE_URL
 
-const connection = connect({ url: connectionString })
-const adapter = new PrismaPlanetScale(connection)
+const client = new Client({ url: connectionString })
+const adapter = new PrismaPlanetScale(client)
 const prisma = new PrismaClient({ adapter })
 
 exports.handler = async () => {
