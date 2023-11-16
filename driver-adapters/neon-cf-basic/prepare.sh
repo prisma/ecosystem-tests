11c8c698bae0c1437d9450b53c9b5e948d14dc6f
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2023-11-15 15:07:01 -1000

    test(driver-adapters): cloudflare

---
+++ driver-adapters/neon-cf-basic/prepare.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+export PRISMA_TELEMETRY_INFORMATION='ecosystem-tests driver-adapters neon-cf-basic build'
+
+# we add the data proxy URL into the configuration file directly
+cp -fr wrangler.base.toml wrangler.toml # needed for retries
+echo "DRIVER_ADAPTERS_NEON_CF_BASIC_DATABASE_URL=\"$DRIVER_ADAPTERS_NEON_CF_BASIC_DATABASE_URL\"" >> wrangler.toml
+
