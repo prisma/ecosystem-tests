549f41a0a1d34ea66f2bfd4574a5517397ff384b
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2023-11-16 12:02:42 -1000

    remove custom folder

--- driver-adapters/neon-cf-basic/prepare.sh
+++ driver-adapters/neon-cf-basic/prepare.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 export PRISMA_TELEMETRY_INFORMATION='ecosystem-tests driver-adapters neon-cf-basic build'
-export PRISMA_CLIENT_ENGINE_TYPE='wasm'
+export PRISMA_CLIENT_ENGINE_TYPE='wasm' # because setup otherwise makes it library/binary
 
 # we add the data proxy URL into the configuration file directly
 cp -fr wrangler.base.toml wrangler.toml # needed for retries
