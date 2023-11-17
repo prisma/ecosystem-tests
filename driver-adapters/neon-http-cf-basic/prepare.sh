8b8c410ed726ac72611c2fe11dc0573a01170e75
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2023-11-16 15:33:06 -1000

    add neon http cloudflare test

---
+++ driver-adapters/neon-http-cf-basic/prepare.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+export PRISMA_TELEMETRY_INFORMATION='ecosystem-tests driver-adapters neon-cf-basic build'
+export PRISMA_CLIENT_ENGINE_TYPE='wasm' # because setup otherwise makes it library/binary
+
+# we add the data proxy URL into the configuration file directly
+cp -fr wrangler.base.toml wrangler.toml # needed for retries
+echo "DRIVER_ADAPTERS_NEON_HTTP_CF_BASIC_DATABASE_URL=\"$DRIVER_ADAPTERS_NEON_HTTP_CF_BASIC_DATABASE_URL\"" >> wrangler.toml
+
