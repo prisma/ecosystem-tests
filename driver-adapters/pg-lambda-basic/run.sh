0f8f962198297820d1f0cb5bbd2bee7f0c66b686
pierre <pierreantoine.urvoy@gmail.com>
2023-11-21 08:45:38 -1000

    chore: cleanup tasks team-orm/#607 (#4284)

--- driver-adapters/pg-lambda-basic/run.sh
+++ driver-adapters/pg-lambda-basic/run.sh
@@ -20,5 +20,5 @@ cp "$GENERATED_CLIENT"/libquery_engine-rhel-openssl-1.0.x.so.node dist
 cp "$GENERATED_CLIENT"/schema.prisma dist
 zip -rj lambda.zip dist
 
-aws lambda update-function-configuration --function-name driver-adapters-postgresql-lambda-basic --runtime nodejs18.x --environment "Variables={DATABASE_URL=$DATABASE_URL}" --timeout 30
-aws lambda update-function-code --function-name driver-adapters-postgresql-lambda-basic --zip-file "fileb://lambda.zip"
+aws lambda update-function-configuration --function-name driver-adapters-pg-lambda-basic --runtime nodejs18.x --environment "Variables={DATABASE_URL=$DATABASE_URL}" --timeout 30
+aws lambda update-function-code --function-name driver-adapters-pg-lambda-basic --zip-file "fileb://lambda.zip"
