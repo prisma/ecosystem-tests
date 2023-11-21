607df0cf9f6be42617ae8a31e7905f2d41d7aa75
pierre <pierreantoine.urvoy@gmail.com>
2023-11-21 05:52:06 -1000

    test(driver-adapters): wasm engine with cloudflare (#4253)
    * test(driver-adapters): cloudflare
    
    * add entry to yaml file
    
    * attempt to generate in folder
    
    * skip other tests for now
    
    * debug
    
    * properly set engine type
    
    * remove custom folder
    
    * remove debug
    
    * add planetscale cloudflare test
    
    * add entry to yml
    
    * add postgresql cloudflare test
    
    * add pg-cloudflare and node_compat=true
    
    * fix planetscale setup
    
    * add turso cloudflare test
    
    * add nodejs compat
    
    * restore old node_compat
    
    * remove node compat for turso
    
    * add neon http cloudflare test
    
    * Apply suggestions from code review
    
    Co-authored-by: Joël Galeran <Jolg42@users.noreply.github.com>
    
    * bump packages
    
    * remove wrong comment
    
    * unskip tests
    
    ---------
    
    Co-authored-by: Joël Galeran <Jolg42@users.noreply.github.com>

---
+++ driver-adapters/planetscale-cf-basic/prepare.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+export PRISMA_TELEMETRY_INFORMATION='ecosystem-tests driver-adapters planetscale-cf-basic build'
+export PRISMA_CLIENT_ENGINE_TYPE='wasm' # because setup otherwise makes it library/binary
+
+cp -fr wrangler.base.toml wrangler.toml # needed for retries
+echo "DRIVER_ADAPTERS_PLANETSCALE_CF_BASIC_DATABASE_URL=\"$DRIVER_ADAPTERS_PLANETSCALE_CF_BASIC_DATABASE_URL\"" >> wrangler.toml
+
