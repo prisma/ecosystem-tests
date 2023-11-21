dbab2a537507b2a425889a6fb2338233bb6bfdf1
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2023-11-21 05:28:22 -1000

    remove wrong comment

--- driver-adapters/neon-http-cf-basic/prepare.sh
+++ driver-adapters/neon-http-cf-basic/prepare.sh
@@ -3,7 +3,6 @@
 export PRISMA_TELEMETRY_INFORMATION='ecosystem-tests driver-adapters neon-http-cf-basic build'
 export PRISMA_CLIENT_ENGINE_TYPE='wasm' # because setup otherwise makes it library/binary
 
-# we add the data proxy URL into the configuration file directly
 cp -fr wrangler.base.toml wrangler.toml # needed for retries
 echo "DRIVER_ADAPTERS_NEON_HTTP_CF_BASIC_DATABASE_URL=\"$DRIVER_ADAPTERS_NEON_HTTP_CF_BASIC_DATABASE_URL\"" >> wrangler.toml
 
--- driver-adapters/postgresql-cf-basic/prepare.sh
+++ driver-adapters/postgresql-cf-basic/prepare.sh
@@ -3,7 +3,6 @@
 export PRISMA_TELEMETRY_INFORMATION='ecosystem-tests driver-adapters pg-cf-basic build'
 export PRISMA_CLIENT_ENGINE_TYPE='wasm' # because setup otherwise makes it library/binary
 
-# we add the data proxy URL into the configuration file directly
 cp -fr wrangler.base.toml wrangler.toml # needed for retries
 echo "DATABASE_URL=\"$DATABASE_URL\"" >> wrangler.toml
 
