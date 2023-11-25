f4e57ab2e17862f16c151a8aeec09bb263109054
Jan Piotrowski <piotrowski+github@gmail.com>
2023-11-25 10:46:50 +0000

    another try

--- driver-adapters/neon-cf-basic/index.test.js
+++ driver-adapters/neon-cf-basic/index.test.js
@@ -9,21 +9,21 @@ jest.setTimeout(30_000)
 
 test.skip('prisma version and output', async () => {
   const response = await fetch(process.env.DEPLOYMENT_URL + '/')
-  try {
-    const { regResult, itxResult } = await response.json()
 
-    expect(regResult).toEqual(itxResult)
-    expect(regResult.prismaVersion).toMatch(pjson.dependencies['@prisma/client'])
-    expect(regResult.deleteMany.count).toBe(0)
-    expect(regResult.create).toMatchInlineSnapshot(`
+  const { regResult, itxResult } = await response.json()
+
+  expect(regResult).toEqual(itxResult)
+  expect(regResult.prismaVersion).toMatch(pjson.dependencies['@prisma/client'])
+  expect(regResult.deleteMany.count).toBe(0)
+  expect(regResult.create).toMatchInlineSnapshot(`
 Object {
   "age": 27,
   "email": "test-1@prisma.io",
   "name": "Test 1",
 }
 `)
-    expect(regResult.createMany.count).toBe(2)
-    expect(regResult.findMany).toMatchInlineSnapshot(`
+  expect(regResult.createMany.count).toBe(2)
+  expect(regResult.findMany).toMatchInlineSnapshot(`
 Array [
   Object {
     "age": 27,
@@ -42,42 +42,42 @@ Array [
   },
 ]
 `)
-    expect(regResult.findUnique).toMatchInlineSnapshot(`
+  expect(regResult.findUnique).toMatchInlineSnapshot(`
 Object {
   "age": 27,
   "email": "test-1@prisma.io",
   "name": "Test 1",
 }
 `)
-    expect(regResult.update).toMatchInlineSnapshot(`
+  expect(regResult.update).toMatchInlineSnapshot(`
 Object {
   "age": 26,
   "email": "test-1@prisma.io",
   "name": "Test 1",
 }
 `)
-    expect(regResult.updateMany.count).toBe(1)
-    expect(regResult.findFirst).toMatchInlineSnapshot(`
+  expect(regResult.updateMany.count).toBe(1)
+  expect(regResult.findFirst).toMatchInlineSnapshot(`
 Object {
   "age": 27,
   "email": "test-1@prisma.io",
   "name": "Test 1",
 }
 `)
-    expect(regResult.delete).toMatchInlineSnapshot(`
+  expect(regResult.delete).toMatchInlineSnapshot(`
 Object {
   "age": 27,
   "email": "test-1@prisma.io",
   "name": "Test 1",
 }
 `)
-    expect(regResult.count).toBe(2)
-    expect(regResult.aggregate).toMatchInlineSnapshot(`
+  expect(regResult.count).toBe(2)
+  expect(regResult.aggregate).toMatchInlineSnapshot(`
 Object {
   "age": 29,
 }
 `)
-    expect(regResult.groupBy).toMatchInlineSnapshot(`
+  expect(regResult.groupBy).toMatchInlineSnapshot(`
 Array [
   Object {
     "_count": Object {
@@ -87,48 +87,45 @@ Array [
   },
 ]
 `)
-    expect(regResult.findFirstOrThrow).toMatchInlineSnapshot(`
+  expect(regResult.findFirstOrThrow).toMatchInlineSnapshot(`
 Object {
   "age": 29,
   "email": "test-2@prisma.io",
   "name": "Test 2",
 }
 `)
-    expect(regResult.findUniqueOrThrow).toMatchInlineSnapshot(`
+  expect(regResult.findUniqueOrThrow).toMatchInlineSnapshot(`
 Object {
   "age": 29,
   "email": "test-2@prisma.io",
   "name": "Test 2",
 }
 `)
-    expect(regResult.upsert).toMatchInlineSnapshot(`
+  expect(regResult.upsert).toMatchInlineSnapshot(`
 Object {
   "age": 30,
   "email": "test-4@prisma.io",
   "name": "Test 4",
 }
 `)
-  } catch (error) {
-    console.log(error, response)
-  }
+
 })
 
 test('failure snapshot until wasm engine works', async () => {
   const response = await fetch(process.env.DEPLOYMENT_URL + '/')
-  try {
-    const { regResult, itxResult } = await response.json()
+  const { regResult, itxResult } = await response.json()
 
-    expect(regResult).toMatchInlineSnapshot(`
+  if (!response.ok) {
+    throw new Error(`HTTP error! Status: ${response.status}, ${response.text}`);
+  }
+  expect(regResult).toMatchInlineSnapshot(`
   Object {
     "error": "Can't use \`query\` until \`request_handlers\` is Wasm-compatible.",
   }
   `)
-    expect(itxResult).toMatchInlineSnapshot(`
+  expect(itxResult).toMatchInlineSnapshot(`
   Object {
     "error": "Can't use \`start_transaction\` until \`query_core\` is Wasm-compatible.",
   }
   `)
-  } catch (error) {
-    console.log(error, response)
-  }
 })
