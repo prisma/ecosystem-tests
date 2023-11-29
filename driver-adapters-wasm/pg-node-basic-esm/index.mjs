6a6286f2a6d0b22630ee6fa0e6e26b694e870f3a
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2023-11-28 15:06:51 -1000

    fix import

--- driver-adapters-wasm/pg-node-basic-esm/index.mjs
+++ driver-adapters-wasm/pg-node-basic-esm/index.mjs
@@ -1,11 +1,11 @@
 // @ts-check
 import { Prisma, PrismaClient } from '@prisma/client'
 import { PrismaPg } from '@prisma/adapter-pg'
-import { Pool } from 'pg'
+import pg from 'pg'
 
 const connectionString = process.env.DATABASE_URL
 
-const pool = new Pool({ connectionString })
+const pool = new pg.Pool({ connectionString })
 const adapter = new PrismaPg(pool)
 const prisma = new PrismaClient({ adapter })
 
