d18b707aad35b31d14b0a0c9b1f80159dfa757d5
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2023-11-29 10:21:13 -1000

    update neon-http test

--- driver-adapters-wasm/neon-http-cf-basic/index.test.js
+++ driver-adapters-wasm/neon-http-cf-basic/index.test.js
@@ -1,9 +1,7 @@
 // @ts-check
 const { test, expect } = require('@jest/globals')
+const { dependencies } = require('./package.json')
 const fetch = require('node-fetch')
-const fs = require('fs')
-
-const pjson = require('./package.json')
 
 jest.setTimeout(30_000)
 
@@ -11,8 +9,12 @@ test('prisma version and output', async () => {
   const response = await fetch(process.env.DEPLOYMENT_URL + '/')
   const { regResult, itxResult } = await response.json()
 
-  expect(regResult).toEqual(itxResult)
-  expect(regResult.prismaVersion).toMatch(pjson.dependencies['@prisma/client'])
+  // Error: Transactions are not supported in HTTP mode
+  // The expectations that need transactions behind the scene are commented out
+  // Except the interactive transaction (itxResult) at the end
+
+  // expect(regResult).toEqual(itxResult)
+  expect(regResult.prismaVersion).toMatch(dependencies['@prisma/client'])
   expect(regResult.deleteMany.count).toBe(0)
   expect(regResult.create).toMatchInlineSnapshot(`
 Object {
@@ -21,23 +23,13 @@ Object {
   "name": "Test 1",
 }
 `)
-  expect(regResult.createMany.count).toBe(2)
+  // expect(regResult.createMany.count).toBe(2)
   expect(regResult.findMany).toMatchInlineSnapshot(`
 Array [
   Object {
     "age": 27,
     "email": "test-1@prisma.io",
     "name": "Test 1",
-  },
-  Object {
-    "age": 29,
-    "email": "test-2@prisma.io",
-    "name": "Test 2",
-  },
-  Object {
-    "age": 29,
-    "email": "test-3@prisma.io",
-    "name": "Test 3",
   },
 ]
 `)
@@ -55,56 +47,50 @@ Object {
   "name": "Test 1",
 }
 `)
-  expect(regResult.updateMany.count).toBe(1)
-  expect(regResult.findFirst).toMatchInlineSnapshot(`
-Object {
-  "age": 27,
-  "email": "test-1@prisma.io",
-  "name": "Test 1",
-}
-`)
-  expect(regResult.delete).toMatchInlineSnapshot(`
-Object {
-  "age": 27,
-  "email": "test-1@prisma.io",
-  "name": "Test 1",
-}
-`)
-  expect(regResult.count).toBe(2)
+  // expect(regResult.updateMany.count).toBe(1)
+  expect(regResult.findFirst).toMatchInlineSnapshot(`null`)
+  //   expect(regResult.delete).toMatchInlineSnapshot(`
+  // Object {
+  //   "age": 27,
+  //   "email": "test-1@prisma.io",
+  //   "name": "Test 1",
+  // }
+  // `)
+  expect(regResult.count).toBe(1)
   expect(regResult.aggregate).toMatchInlineSnapshot(`
 Object {
-  "age": 29,
+  "age": 26,
 }
 `)
   expect(regResult.groupBy).toMatchInlineSnapshot(`
 Array [
   Object {
     "_count": Object {
-      "age": 2,
+      "age": 1,
     },
-    "age": 29,
+    "age": 26,
   },
 ]
 `)
   expect(regResult.findFirstOrThrow).toMatchInlineSnapshot(`
 Object {
-  "age": 29,
-  "email": "test-2@prisma.io",
-  "name": "Test 2",
+  "age": 26,
+  "email": "test-1@prisma.io",
+  "name": "Test 1",
 }
 `)
   expect(regResult.findUniqueOrThrow).toMatchInlineSnapshot(`
 Object {
-  "age": 29,
-  "email": "test-2@prisma.io",
-  "name": "Test 2",
+  "age": 26,
+  "email": "test-1@prisma.io",
+  "name": "Test 1",
 }
 `)
-  expect(regResult.upsert).toMatchInlineSnapshot(`
-Object {
-  "age": 30,
-  "email": "test-4@prisma.io",
-  "name": "Test 4",
-}
-`)
+  //   expect(regResult.upsert).toMatchInlineSnapshot(`
+  // Object {
+  //   "age": 30,
+  //   "email": "test-4@prisma.io",
+  //   "name": "Test 4",
+  // }
+  // `)
 })
