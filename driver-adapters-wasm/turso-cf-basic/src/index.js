662562a5c6e6bd1be3ccc9c592c3b0abd1dd0f58
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2023-11-30 10:40:40 -1000

    fix missing create

--- driver-adapters-wasm/turso-cf-basic/src/index.js
+++ driver-adapters-wasm/turso-cf-basic/src/index.js
@@ -42,6 +42,30 @@ export default {
         //     },
         //   ],
         // }),
+        create2: await prisma.user.create({
+          data: {
+            email: `test-2@prisma.io`,
+            age: 29,
+            name: 'Test 2',
+          },
+          select: {
+            email: true,
+            age: true,
+            name: true,
+          },
+        }),
+        create3: await prisma.user.create({
+          data: {
+            email: `test-3@prisma.io`,
+            age: 29,
+            name: 'Test 3',
+          },
+          select: {
+            email: true,
+            age: true,
+            name: true,
+          },
+        }),
         findMany: await prisma.user.findMany({
           select: {
             email: true,
