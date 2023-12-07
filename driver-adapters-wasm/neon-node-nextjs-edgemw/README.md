fa7ee5d0aa19a22df366d4f0827c93996b2f8b83
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2023-12-07 12:48:28 -1000

    test(driver-adapters-wasm): neon next edgemw local dev

---
+++ driver-adapters-wasm/neon-node-nextjs-edgemw/README.md
@@ -0,0 +1,12 @@
+# neon-node-nextjs-edgemw
+
+Deploys a Prisma Client using the Neon Adapter on Vercel Edge Middleware locally in `dev`` mode.
+
+## How to run this
+
+Set the necessary env vars first.
+
+```sh
+source ./prepare.sh && ./run.sh && ./test.sh && ./finally.sh
+```
+
---
+++ driver-adapters-wasm/neon-node-nextjs-edgemw/run.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -eu
+
+export PRISMA_TELEMETRY_INFORMATION='ecosystem-tests driver-adapters-wasm neon-node-nextjs-edgemw'
+
+wait_for_host() {
+  while ! nc -z $1 $2; do
+    sleep 0.1
+  done
+}
+
+pnpm install
+pnpm next dev --port 3000 &
+echo "http://localhost:3000" > deployment-url.txt
+wait_for_host localhost 3000
