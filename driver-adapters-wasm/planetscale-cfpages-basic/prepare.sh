7b4a103bb49e102edc6b98a3fff92cd5b7602a0d
Jan Piotrowski <piotrowski+github@gmail.com>
2023-12-09 17:52:44 +0000

    fix

--- driver-adapters-wasm/planetscale-cfpages-basic/prepare.sh
+++ driver-adapters-wasm/planetscale-cfpages-basic/prepare.sh
@@ -3,6 +3,6 @@
 export PRISMA_TELEMETRY_INFORMATION='ecosystem-tests driver-adapters-wasm planetscale-cfpages-basic build'
 export PRISMA_CLIENT_ENGINE_TYPE='wasm' # because setup otherwise makes it library/binary
 
-cp -fr wrangler.base.toml wrangler.toml # needed for retries
-echo "DRIVER_ADAPTERS_PLANETSCALE_CFPAGES_BASIC_DATABASE_URL=\"$DRIVER_ADAPTERS_PLANETSCALE_CFPAGES_BASIC_DATABASE_URL\"" >> wrangler.toml
+# cp -fr wrangler.base.toml wrangler.toml # needed for retries
+# echo "DRIVER_ADAPTERS_PLANETSCALE_CFPAGES_BASIC_DATABASE_URL=\"$DRIVER_ADAPTERS_PLANETSCALE_CFPAGES_BASIC_DATABASE_URL\"" >> wrangler.toml
 
