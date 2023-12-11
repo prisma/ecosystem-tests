3c6c025343d19f1354f23333dd67c635bab1cbce
Jan Piotrowski <piotrowski+github@gmail.com>
2023-12-11 19:39:29 +0100

    feat(driver-adapters-wasm): Cloudflare Pages projects (`cfpages-basic`) (#4368)
    Co-authored-by: pierre <pierreantoine.urvoy@gmail.com>

---
+++ driver-adapters-wasm/pg-cfpages-basic/run.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -eu
+
+pnpm install
+
+pnpm prisma generate
+
+pnpm wrangler pages deploy . --project-name pg-cfpages-basic --node-compat | tee deployment-logs.txt
+sleep 5
