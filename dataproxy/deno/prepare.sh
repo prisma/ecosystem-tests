96fcb3bd4fb28652d1a59fe2ee9853c0da3869d5
Jan Piotrowski <piotrowski+github@gmail.com>
2023-12-18 11:18:43 +0100

    chore: Output Deno version

--- dataproxy/deno/prepare.sh
+++ dataproxy/deno/prepare.sh
@@ -5,3 +5,5 @@ export PRISMA_TELEMETRY_INFORMATION='ecosystem-tests dataproxy deno build'
 # Install Deno CLI (not needed on GH Actions, only here for reference for local testing)
 # curl -fsSL https://deno.land/x/install/install.sh | sh
 # alias deno='/home/gitpod/.deno/bin/deno'
+
+deno -v
