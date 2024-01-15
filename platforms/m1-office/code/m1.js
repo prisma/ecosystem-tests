133fa54ec96570c0b3d36e29353d3f0662a11b80
Joël Galeran <Jolg42@users.noreply.github.com>
2024-01-15 17:17:23 +0100

    throw if running different CPU arch than expected

--- platforms/m1-office/code/m1.js
+++ platforms/m1-office/code/m1.js
@@ -1,18 +1,20 @@
 // adapted from https://gregmfoster.medium.com/using-m1-mac-minis-to-power-our-github-actions-ios-ci-540c55af13ea
 
-const { execSync } = require('child_process');
+const { execSync } = require('child_process')
 
 const uname = execSync(`uname -m`).toString().trim()
-console.log("uname", uname)
-const isArm = uname === "arm64";
+console.log('uname', uname)
+const isArm = uname === 'arm64'
 
-let isM1 = false;
+let isM1 = false
 try {
-    // this command will only succeed on m1 macs.
-    execSync(`arch -arm64e echo hi`);
-    isM1 = true;
+  // this command will only succeed on m1 macs.
+  execSync(`arch -arm64e echo hi`)
+  isM1 = true
 } catch (err) {
-    // Must not be an m1 mac
+  throw new Error(
+    'This test is expected to run on Apple Silicon (e.g. M1/M2) only. Please run it on a machine with an Apple Silicon CPU.',
+  )
 }
 
 console.log('arm', isArm)
