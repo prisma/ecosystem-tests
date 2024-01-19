2766e05b22664eb6058e75447a7212feb1926eaa
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2024-01-18 17:41:23 -1000

    update snaps

--- platforms-serverless-vercel/vercel-node-builder/test.sh
+++ platforms-serverless-vercel/vercel-node-builder/test.sh
@@ -4,7 +4,7 @@ set -eux
 DEPLOYED_URL=$( tail -n 1 deployment-url.txt )
 
 if [ "$PRISMA_CLIENT_ENGINE_TYPE" == "binary" ]; then
-  files=',"files":["default.js","index.js","query-engine-rhel-openssl-1.0.x","package.json","schema.prisma"]'
+  files=',"files":["default.js","index.js","package.json",,"query-engine-rhel-openssl-1.0.x""schema.prisma"]'
 else
   files=',"files":["default.js","index.js","libquery_engine-rhel-openssl-1.0.x.so.node","package.json","schema.prisma"]'
 fi
