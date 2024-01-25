190b01ae60316892e6ccb7d392467b028a704d57
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2024-01-25 10:18:02 -1000

    attempt to pass env var via cli

--- driver-adapters-wasm/pg-cfpages-basic/run.sh
+++ driver-adapters-wasm/pg-cfpages-basic/run.sh
@@ -6,5 +6,5 @@ pnpm install
 
 pnpm prisma generate
 
-pnpm wrangler pages deploy . --project-name pg-cfpages-basic --node-compat | tee deployment-logs.txt
+pnpm wrangler pages deploy . --project-name pg-cfpages-basic --node-compat --var "$DATABASE_URL" | tee deployment-logs.txt
 sleep 15
