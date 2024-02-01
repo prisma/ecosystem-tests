beee2f1640b2ac71ad90ae7fd59af994ef7e6b65
Joël Galeran <Jolg42@users.noreply.github.com>
2024-02-01 15:44:32 +0100

    fix

--- driver-adapters-wasm/pg-cfpages-basic/run.sh
+++ driver-adapters-wasm/pg-cfpages-basic/run.sh
@@ -7,6 +7,6 @@ pnpm install
 pnpm prisma generate
 
 pnpm wrangler pages functions build --node-compat --outdir build/
-pnpm wrangler publish build/index.js --name pg-cfpages-basic public
+pnpm wrangler publish build/index.js --name pg-cfpages-basic
 
 sleep 15
