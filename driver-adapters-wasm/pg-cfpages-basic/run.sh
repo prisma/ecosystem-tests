eaf1c664155db973b19058e4ac16a20829e1d7bb
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2024-02-01 06:12:16 -1000

    remove flag into the ui

--- driver-adapters-wasm/pg-cfpages-basic/run.sh
+++ driver-adapters-wasm/pg-cfpages-basic/run.sh
@@ -6,5 +6,5 @@ pnpm install
 
 pnpm prisma generate
 
-pnpm wrangler pages deploy . --project-name pg-cfpages-basic --compatibility-flags nodejs_compat | tee deployment-logs.txt
+pnpm wrangler pages deploy . --project-name pg-cfpages-basic | tee deployment-logs.txt
 sleep 15
