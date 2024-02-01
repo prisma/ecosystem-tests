06289fa28b13b55540669984d36f3774841ae499
Joël Galeran <Jolg42@users.noreply.github.com>
2024-02-01 17:33:51 +0100

    add m1 check

--- generic/basic-m1/run.sh
+++ generic/basic-m1/run.sh
@@ -5,5 +5,8 @@ set -eux
 export DEBUG="*"
 export RUST_BACKTRACE=full
 
+# We want to make sure this runs on M1, so we check the architecture
+node m1.js
+
 pnpm install
 pnpm prisma generate
--- generic/basic-m1/test.sh
+++ generic/basic-m1/test.sh
@@ -2,4 +2,7 @@
 
 set -eux
 
+# We want to make sure this runs on M1, so we check the architecture
+node m1.js
+
 pnpm cmd
