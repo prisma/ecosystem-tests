dddc7b5e052d590f53324e5457ea24d2dfbeb708
Sergey Tatarintsev <tatarintsev@prisma.io>
2024-02-14 14:47:25 +0100

    Fix things

--- driver-adapters-wasm/pg-cfpages-basic/run.sh
+++ driver-adapters-wasm/pg-cfpages-basic/run.sh
@@ -3,9 +3,9 @@
 set -eu
 
 echo "Temporary disabled, see https://github.com/cloudflare/workers-sdk/pull/2541"
-# pnpm install
+pnpm install
 
-# pnpm prisma generate
+pnpm prisma generate
 
 # pnpm wrangler pages deploy . --project-name pg-cfpages-basic --node-compat | tee deployment-logs.txt
 # sleep 15
