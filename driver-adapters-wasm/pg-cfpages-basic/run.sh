6adadc726c90f4428f2859c039338163245ea535
Jan Piotrowski <piotrowski+github@gmail.com>
2024-02-19 16:18:59 +0000

    comment

--- driver-adapters-wasm/pg-cfpages-basic/run.sh
+++ driver-adapters-wasm/pg-cfpages-basic/run.sh
@@ -8,5 +8,6 @@ pnpm prisma generate
 
 pnpm wrangler pages functions build --node-compat --outdir build/
 pnpm wrangler publish build/index.js --name pg-cfpages-basic --compatibility-date 2024-02-01 | tee deployment-logs.txt
+# maybe build to another folder and also copy over index.html to make a full "pages build" instead?
 
 sleep 15
