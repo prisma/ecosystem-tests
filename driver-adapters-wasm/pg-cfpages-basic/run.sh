9c23237ae38b9b16b3cf5121ac5dcc29445d5133
Jan Piotrowski <piotrowski+github@gmail.com>
2024-02-19 15:51:14 +0000

    switch to suggested workaround, unfortuantely only deploys the worker bit and not pages

--- driver-adapters-wasm/pg-cfpages-basic/run.sh
+++ driver-adapters-wasm/pg-cfpages-basic/run.sh
@@ -2,12 +2,11 @@
 
 set -eu
 
-echo "Temporary disabled, because wrangler does not support "--node-compat" flag for pages commands yet. See https://github.com/cloudflare/workers-sdk/pull/2541"
 pnpm install
 
 pnpm prisma generate
 
 pnpm wrangler pages functions build --node-compat --outdir build/
-pnpm wrangler publish build/index.js --name pg-cfpages-basic --compatibility-date 2024-02-01
+pnpm wrangler publish build/index.js --name pg-cfpages-basic --compatibility-date 2024-02-01 | tee deployment-logs.txt
 
 sleep 15
--- driver-adapters-wasm/pg-cfpages-basic/test.sh
+++ driver-adapters-wasm/pg-cfpages-basic/test.sh
@@ -2,9 +2,7 @@
 
 set -eu
 
-echo "Temporary disabled, because wrangler does not support "--node-compat" flag for pages commands yet. See https://github.com/cloudflare/workers-sdk/pull/2541"
+export DEPLOYMENT_URL=$(cat deployment-logs.txt | grep -Eo "(https.*)\.pages\.dev$" --color=never)
+echo $DEPLOYMENT_URL
 
-# export DEPLOYMENT_URL=$(cat deployment-logs.txt | grep -Eo "(https.*)\.pages\.dev$" --color=never)
-# echo $DEPLOYMENT_URL
-
-# pnpm test
+pnpm test
