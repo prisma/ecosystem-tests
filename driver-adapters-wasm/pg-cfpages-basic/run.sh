d718e60b01f32473d7f3d2a986257ba804ac6147
Peter Bacon Darwin <pbacondarwin@cloudflare.com>
2024-02-19 16:33:07 +0000

    fix Cloudflare Pages integration

--- driver-adapters-wasm/pg-cfpages-basic/run.sh
+++ driver-adapters-wasm/pg-cfpages-basic/run.sh
@@ -7,5 +7,9 @@ pnpm install
 
 pnpm prisma generate
 
-# pnpm wrangler pages deploy . --project-name pg-cfpages-basic --node-compat | tee deployment-logs.txt
-# sleep 15
+# First build the functions using the `--node-compat` flag into the `_worker.js` directory
+# (Note that the functions are in the non-standard `fns` directory so that they are not confused with the output `_worker.js` directory.)
+pnpm wrangler pages functions build fns --node-compat --outdir _worker.js
+# Now deploy the _worker.js alongside the index.html asset to Pages
+pnpm wrangler pages deploy . --project-name pg-cfpages-basic -- | tee deployment-logs.txt
+sleep 15
