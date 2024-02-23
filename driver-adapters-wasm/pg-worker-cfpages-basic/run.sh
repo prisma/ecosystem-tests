24ce3917bd27fc7a10ec6f5fb213e164a1ac3c78
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2024-02-23 13:15:58 -1000

    update to latest

--- driver-adapters-wasm/pg-worker-cfpages-basic/run.sh
+++ driver-adapters-wasm/pg-worker-cfpages-basic/run.sh
@@ -2,19 +2,9 @@
 
 set -eu
 
-echo "Temporary disabled, because wrangler does not support "--node-compat" flag for pages commands yet. See https://github.com/cloudflare/workers-sdk/pull/2541"
 pnpm install
 
 pnpm prisma generate
 
-# First build the functions using the `--node-compat` flag into the `_worker.js` directory
-# (Note that the functions are in the non-standard `fns` directory so that they are not confused with the output `_worker.js` directory.)
-# Make sure build folder does not exist
-rm -rf build
-# Build pages function to build/_worker.js
-pnpm wrangler pages functions build fns --outdir build/_worker.js # nodejs_compat enabled in the UI
-# Copy index.html to build folder
-cp index.html build/index.html
-# Now deploy the _worker.js alongside the index.html asset to Pages
-pnpm wrangler pages deploy ./build --project-name pg-cfpages-basic -- | tee deployment-logs.txt
+pnpm wrangler pages deploy . --project-name pg-worker-cfpages-basic | tee deployment-logs.txt
 sleep 15
