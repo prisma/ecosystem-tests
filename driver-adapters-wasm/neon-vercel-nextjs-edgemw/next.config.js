cd9474d98b8e722d6fcdfd4e8a96b98c261a562a
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2024-02-29 18:00:18 -1000

    remove next.js wasm config

--- driver-adapters-wasm/neon-vercel-nextjs-edgemw/next.config.js
+++ driver-adapters-wasm/neon-vercel-nextjs-edgemw/next.config.js
@@ -1,14 +1,6 @@
 /**
  * @type {import('next').NextConfig}
  */
-const nextConfig = {
-  webpack(config) {
-    config.experiments ??= {}
-
-    config.experiments.asyncWebAssembly = true
-
-    return config
-  }
-}
+const nextConfig = {}
 
 module.exports = nextConfig
