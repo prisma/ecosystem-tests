571bcad89a489780dc24afb5a59a4a8e80a8dcb8
Joël Galeran <Jolg42@users.noreply.github.com>
2024-05-07 15:15:28 +0200

    Not implemented for MySQL

--- driver-adapters-wasm/planetscale-cf-basic/index.test.js
+++ driver-adapters-wasm/planetscale-cf-basic/index.test.js
@@ -20,20 +20,21 @@ test('prisma version and output', async () => {
 }
 `)
   expect(regResult.createMany.count).toBe(2)
-  expect(regResult.createManyAndReturn).toMatchInlineSnapshot(`
-  [
-    {
-      "age": 30,
-      "email": "test-4@prisma.io",
-      "name": "Test 4",
-    },
-    {
-      "age": 31,
-      "email": "test-5@prisma.io",
-      "name": "Test 5",
-    },
-  ]
-  `)
+  // Not implemented for MySQL
+  // expect(regResult.createManyAndReturn).toMatchInlineSnapshot(`
+  // [
+  //   {
+  //     "age": 30,
+  //     "email": "test-4@prisma.io",
+  //     "name": "Test 4",
+  //   },
+  //   {
+  //     "age": 31,
+  //     "email": "test-5@prisma.io",
+  //     "name": "Test 5",
+  //   },
+  // ]
+  // `)
   expect(regResult.findMany).toMatchInlineSnapshot(`
 [
   {
--- driver-adapters-wasm/planetscale-cfpages-basic/functions/function.js
+++ driver-adapters-wasm/planetscale-cfpages-basic/functions/function.js
@@ -44,25 +44,25 @@ export async function onRequest(context) {
           },
         ],
       }),
-      createManyAndReturn: await prisma.user.createManyAndReturn({
-        select: {
-          age: true,
-          email: true,
-          name: true,
-        },
-        data: [
-          {
-            email: `test-4@prisma.io`,
-            age: 30,
-            name: 'Test 4',
-          },
-          {
-            email: `test-5@prisma.io`,
-            age: 31,
-            name: 'Test 5',
-          },
-        ],
-      }),
+      // Not implemented for MySQL
+      // createManyAndReturn: await prisma.user.createManyAndReturn({
+      //   select: {
+      //     email: true,
+      //     name: true,
+      //   },
+      //   data: [
+      //     {
+      //       email: `test-4@prisma.io`,
+      //       age: 30,
+      //       name: 'Test 4',
+      //     },
+      //     {
+      //       email: `test-5@prisma.io`,
+      //       age: 31,
+      //       name: 'Test 5',
+      //     },
+      //   ],
+      // }),
       findMany: await prisma.user.findMany({
         select: {
           email: true,
