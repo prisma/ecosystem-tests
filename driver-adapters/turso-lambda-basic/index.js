b28b9b63a3dac85a25946874bba9fca50155a32c
Joël Galeran <Jolg42@users.noreply.github.com>
2024-05-07 12:07:55 +0200

    add 1 createManyAndReturn

--- driver-adapters/turso-lambda-basic/index.js
+++ driver-adapters/turso-lambda-basic/index.js
@@ -40,6 +40,20 @@ exports.handler = async () => {
         },
       ],
     }),
+    createManyAndReturn: await prisma.user.createManyAndReturn({
+      data: [
+        {
+          email: `test-4@prisma.io`,
+          age: 30,
+          name: 'Test 4',
+        },
+        {
+          email: `test-5@prisma.io`,
+          age: 31,
+          name: 'Test 5',
+        },
+      ],
+    }),
     findMany: await prisma.user.findMany({
       select: {
         email: true,
