f35260c6098517c2ce35af1a9dc005d5430ab941
jkomyno <skiabo97@gmail.com>
2024-06-04 16:31:46 +0200

    chore: manual Cloudflare Pages setting, fix run.sh

--- driver-adapters-wasm/d1-cfpages-nuxt/run.sh
+++ driver-adapters-wasm/d1-cfpages-nuxt/run.sh
@@ -3,8 +3,8 @@
 set -eu
 
 pnpm install
-
 pnpm prisma generate
+pnpm build
 
-pnpm wrangler pages deploy . --project-name d1-cfpages-nuxt | tee deployment-logs.txt
+pnpm wrangler pages deploy dist --project-name d1-cfpages-nuxt | tee deployment-logs.txt
 sleep 15
