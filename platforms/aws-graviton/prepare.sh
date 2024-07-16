4a5259f4568e3d976f6145822c476a6754325d32
Serhii Tatarintsev <sergey@tatarintsev.me>
2024-07-16 09:52:07 +0200

    test

--- platforms/aws-graviton/prepare.sh
+++ platforms/aws-graviton/prepare.sh
@@ -11,4 +11,4 @@ if test -f "./server-key.pem"; then
     chmod 600 ./server-key.pem
 fi
 
-ssh -tt -i ./server-key.pem ec2-user@ec2-54-209-135-27.compute-1.amazonaws.com "ls -la /home/ec2-user/aws-graviton"
+ssh -tt -i ./server-key.pem ec2-user@ec2-54-209-135-27.compute-1.amazonaws.com "ls -la /home/ec2-user/aws-graviton/platforms"
