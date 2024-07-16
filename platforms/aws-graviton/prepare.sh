fdc1ae70776ad0ef1b47b34d2c4cb3935a38d4d8
Serhii Tatarintsev <sergey@tatarintsev.me>
2024-07-16 09:58:14 +0200

    Revert

--- platforms/aws-graviton/prepare.sh
+++ platforms/aws-graviton/prepare.sh
@@ -10,5 +10,3 @@ echo "$SSH_KEY_GRAVITON" > ./server-key.pem
 if test -f "./server-key.pem"; then
     chmod 600 ./server-key.pem
 fi
-
-ssh -tt -i ./server-key.pem ec2-user@ec2-54-209-135-27.compute-1.amazonaws.com "rm -rf /home/ec2-user/aws-graviton/platforms"
