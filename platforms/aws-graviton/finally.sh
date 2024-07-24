2a6ca5378afb8c5b4cd3d00842ab8acbe58efd94
Serhii Tatarintsev <sergey@tatarintsev.me>
2024-07-24 17:25:19 +0200

    Store prune

--- platforms/aws-graviton/finally.sh
+++ platforms/aws-graviton/finally.sh
@@ -2,5 +2,5 @@
 
 set -eux
 
-ssh -tt -i ./server-key.pem ec2-user@ec2-54-209-135-27.compute-1.amazonaws.com "pnpm store prune && rm -rf /home/ec2-user/aws-graviton/$GITHUB_JOB/$GITHUB_RUN_ID/$PRISMA_CLIENT_ENGINE_TYPE"
+ssh -tt -i ./server-key.pem ec2-user@ec2-54-209-135-27.compute-1.amazonaws.com "rm -rf /home/ec2-user/aws-graviton/$GITHUB_JOB/$GITHUB_RUN_ID/$PRISMA_CLIENT_ENGINE_TYPE && pnpm store prune"
 
