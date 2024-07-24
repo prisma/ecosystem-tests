f65c29e971fb3ef24049bbe73ab508eef17a0b72
Serhii Tatarintsev <sergey@tatarintsev.me>
2024-07-24 17:23:26 +0200

    Cleunup pnpm cache

--- platforms/aws-graviton/finally.sh
+++ platforms/aws-graviton/finally.sh
@@ -2,6 +2,6 @@
 
 set -eux
 
-echo "In finally: /home/ec2-user/aws-graviton/$GITHUB_JOB/$GITHUB_RUN_ID/$PRISMA_CLIENT_ENGINE_TYPE"
 ssh -tt -i ./server-key.pem ec2-user@ec2-54-209-135-27.compute-1.amazonaws.com "rm -rf /home/ec2-user/aws-graviton/$GITHUB_JOB/$GITHUB_RUN_ID/$PRISMA_CLIENT_ENGINE_TYPE"
+ssh -tt -i ./server-key.pem ec2-user@ec2-54-209-135-27.compute-1.amazonaws.com "rm -rf $(pnpm store path)"
 
