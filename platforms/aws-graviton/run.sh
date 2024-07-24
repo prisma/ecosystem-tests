ccb72cd87e46ca658db85df288681f1f4de7b22e
Serhii Tatarintsev <sergey@tatarintsev.me>
2024-07-24 15:37:06 +0200

    Add more logs

--- platforms/aws-graviton/run.sh
+++ platforms/aws-graviton/run.sh
@@ -5,6 +5,7 @@ set -eux
 sh sync-to-remote.sh
 
 echo "PRISMA_CLIENT_ENGINE_TYPE = $PRISMA_CLIENT_ENGINE_TYPE"
+echo "IN RUN: /home/ec2-user/aws-graviton/$GITHUB_JOB/$GITHUB_RUN_ID/$PRISMA_CLIENT_ENGINE_TYPE"
 
 ssh -i ./server-key.pem ec2-user@ec2-54-209-135-27.compute-1.amazonaws.com -tt "
     cd /home/ec2-user/aws-graviton/$GITHUB_JOB/$GITHUB_RUN_ID/$PRISMA_CLIENT_ENGINE_TYPE;
