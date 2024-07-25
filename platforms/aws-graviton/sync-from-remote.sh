d6fdf67c4d6fc46a55ddd54a150e1420b1e3fe46
Serhii Tatarintsev <sergey@tatarintsev.me>
2024-07-25 15:23:31 +0200

    Use long options

--- platforms/aws-graviton/sync-from-remote.sh
+++ platforms/aws-graviton/sync-from-remote.sh
@@ -10,7 +10,7 @@ echo "Sync: Removing existing local code"
 rm -rf ./code
 
 echo "Sync: Copying remote code from EC2 to local"
-rysnc -Pav -e "ssh -i ./server-key.pem" --exclude "node_modules" ec2-user@ec2-54-209-135-27.compute-1.amazonaws.com:/home/ec2-user/aws-graviton/ .
+rysnc --progress --verbose --archive --rsh="ssh -i ./server-key.pem" --exclude "node_modules" ec2-user@ec2-54-209-135-27.compute-1.amazonaws.com:/home/ec2-user/aws-graviton/ .
 
 echo "Sync: Renaming local aws-graviton/ to code/"
 mv aws-graviton/ code/
