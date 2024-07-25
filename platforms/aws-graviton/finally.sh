b23da3d40464f0884dbb919f91d3b64f0f2a051d
Serhii Tatarintsev <sergey@tatarintsev.me>
2024-07-25 13:02:41 +0200

    Fix

--- platforms/aws-graviton/finally.sh
+++ platforms/aws-graviton/finally.sh
@@ -4,7 +4,9 @@ set -eux
 
 ssh -tt -i ./server-key.pem ec2-user@ec2-54-209-135-27.compute-1.amazonaws.com "
     rm -rf /home/ec2-user/aws-graviton/$GITHUB_JOB/$GITHUB_RUN_ID/$PRISMA_CLIENT_ENGINE_TYPE
-    pnpm cache prune
+    # delete parent folder if empty
+    find /home/ec2-user/aws-graviton/$GITHUB_JOB/$GITHUB_RUN_ID -maxdepth 0 -empty -delete
+    pnpm store prune
 "
 
 
