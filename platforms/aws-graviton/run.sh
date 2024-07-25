0b04db5d27885f4dda51ec294479b9538bbbda39
Serhii Tatarintsev <sergey@tatarintsev.me>
2024-07-25 15:25:43 +0200

    fix(aws-graviton): Cleanup pnpm packages after the test (#5264)
    * fix: Cleanup npm packages after install
    
    * Fix
    
    * Use long options

--- platforms/aws-graviton/run.sh
+++ platforms/aws-graviton/run.sh
@@ -12,8 +12,6 @@ ssh -i ./server-key.pem ec2-user@ec2-54-209-135-27.compute-1.amazonaws.com -tt "
     export CI=\"true\"
     export PRISMA_CLIENT_ENGINE_TYPE=\"$PRISMA_CLIENT_ENGINE_TYPE\"
 
-    rm -rf ./node_modules;
-
     npm i -g pnpm@8;
     pnpm -v;
 
