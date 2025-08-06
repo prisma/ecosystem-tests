e6d3655a885c7ae68c77da9dfc115683d69d7452
Florian Goessler <florian.goessler@hey.com>
2025-08-06 10:07:42 +0200

    fix: use esbuild for serverless tests for small bundle size

--- platforms-serverless/serverless-framework-lambda/run.sh
+++ platforms-serverless/serverless-framework-lambda/run.sh
@@ -4,11 +4,13 @@ set -eux
 
 npm install
 npx prisma generate
-npx tsc
+npx tsc --noEmit
 
 # Turn serverless CLI logs to verbose
 # Useful for debugging aws permissions errors (and probably more)
 export SLS_DEBUG=*
 
 # npx serverless remove --region "$AWS_DEFAULT_REGION"
+
+# Build and deploy
 npx serverless deploy --region "$AWS_DEFAULT_REGION"
