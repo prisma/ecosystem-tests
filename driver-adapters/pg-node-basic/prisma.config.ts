a1083d8633e294225d0b5cc21706b817747b0cb3
Jacek Malec <malec@prisma.io>
2025-10-31 12:25:51 +0000

    ci: config and workflow update

--- driver-adapters/pg-node-basic/prisma.config.ts
+++ driver-adapters/pg-node-basic/prisma.config.ts
@@ -4,10 +4,7 @@ import { defineConfig, env } from 'prisma/config'
 export default defineConfig({
   engine: 'classic',
   datasource: {
-    // not used by the client, just to setup the db
-    directUrl: env('DATABASE_URL'),
-    // fake url to ensure the driver adapter is used
-    url: env('INVALID_ENV_VAR'),
+    url: env('DATABASE_URL'),
   },
   schema: path.join('prisma', 'schema.prisma'),
 })
