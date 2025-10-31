393050d8db5f866b102e4d18e21a66d52c859438
Jacek Malec <malec@prisma.io>
2025-10-31 12:25:51 +0000

    ci: config and workflow update

--- frameworks/nestjs/src/prisma.service.ts
+++ frameworks/nestjs/src/prisma.service.ts
@@ -8,7 +8,9 @@ export class PrismaService
   implements OnModuleInit, OnModuleDestroy
 {
   constructor() {
-    super(new PrismaPg({ connectionString: process.env.DATABASE_URL }));
+    super({
+      adapter: new PrismaPg({ connectionString: process.env.DATABASE_URL }),
+    });
   }
 
   async onModuleInit() {
