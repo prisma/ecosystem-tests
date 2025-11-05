586ed829ce1e2422ec04a8701020e82811279b18
Jacek Malec <malec@prisma.io>
2025-11-05 13:31:06 +0000

    test: update accelerate tests

--- accelerate/nodejs/src/index.ts
+++ accelerate/nodejs/src/index.ts
@@ -1,7 +1,9 @@
 import { PrismaClient } from '@prisma/client'
-import { withAccelerate } from '@prisma/extension-accelerate';
+import { withAccelerate } from '@prisma/extension-accelerate'
 
-let prisma = new PrismaClient({})
+let prisma = new PrismaClient({
+  accelerateUrl: process.env.DATAPROXY_COMMON_URL,
+})
 
 if (process.env.DATAPROXY_FLAVOR === 'DP2+Extension') {
   prisma = prisma.$extends(withAccelerate()) as any
@@ -10,23 +12,19 @@ if (process.env.DATAPROXY_FLAVOR === 'DP2+Extension') {
 export async function getUsers() {
   // Do a query and disconnect
   // So we also test that connection is re-established in next query below
-  console.debug(new Date(), "Start await prisma.user.findFirst()")
-  console.time('findFirstTook');
+  console.debug(new Date(), 'Start await prisma.user.findFirst()')
+  console.time('findFirstTook')
   await prisma.user.findFirst()
-  console.timeEnd('findFirstTook');
-  console.debug(new Date(), "End await prisma.user.findFirst()")
+  console.timeEnd('findFirstTook')
+  console.debug(new Date(), 'End await prisma.user.findFirst()')
 
   await prisma.$disconnect()
 
-  console.debug(new Date(), "Start await prisma.$transaction")
-  console.time('transactionTook');
+  console.debug(new Date(), 'Start await prisma.$transaction')
+  console.time('transactionTook')
   // query should re-connect automatically
-  const data = await prisma.$transaction([
-    prisma.user.findFirst(),
-    prisma.user.findMany()
-  ])
-  console.timeEnd('transactionTook');
-  console.debug(new Date(), "End await prisma.$transaction")
+  const data = await prisma.$transaction([prisma.user.findFirst(), prisma.user.findMany()])
+  console.timeEnd('transactionTook')
+  console.debug(new Date(), 'End await prisma.$transaction')
   return data
 }
-
