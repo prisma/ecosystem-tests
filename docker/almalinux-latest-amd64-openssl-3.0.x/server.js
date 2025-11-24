4e9dc41bfee8a795e410865379fef91312bb785c
Jacek Malec <malec@prisma.io>
2025-11-24 21:57:12 +0000

    fix: update docker test snippets

--- docker/almalinux-latest-amd64-openssl-3.0.x/server.js
+++ docker/almalinux-latest-amd64-openssl-3.0.x/server.js
@@ -5,8 +5,11 @@ const app = express()
 const port = 3000
 
 const { PrismaClient } = require('@prisma/client')
+const { PrismaPg } = require('@prisma/adapter-pg')
 
-const client = new PrismaClient()
+const connectionString = process.env.DATABASE_URL
+const adapter = new PrismaPg({ connectionString })
+const client = new PrismaClient({ adapter })
 
 app.get('/', async (req, res) => {
   const data = await client.user.findMany()
