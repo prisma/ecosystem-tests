701ff4f5f73fc28283c95744a70d79b745aba59b
Jacek Malec <malec@prisma.io>
2025-11-24 22:15:04 +0000

    fix: more portable version check

--- docker/_utils/version.sh
+++ docker/_utils/version.sh
@@ -11,7 +11,7 @@ PRISMA_VERSION=$(pnpm prisma -v)
 echo "pnpm prisma -v\n${PRISMA_VERSION}\n"
 
 # extract the binaryTarget from the version command output, e.g., "debian-openssl-1.1.x"
-PRISMA_TARGET_PLATFORM=$(echo "${PRISMA_VERSION}" | awk -F'engines/schema-engine-' '{split($2, a, ")"); print a[1]}' | tr -d ' \t\n')
+PRISMA_TARGET_PLATFORM=$(echo "${PRISMA_VERSION}" | grep 'engines/schema-engine-' | sed 's/.*engines\/schema-engine-//; s/).*//')
 
 if [ $PRISMA_TARGET_PLATFORM = $EXPECTED_PRISMA_TARGET_PLATFORM ]; then
   echo "Current binaryTarget matches expected binaryTarget \"${PRISMA_TARGET_PLATFORM}\""
