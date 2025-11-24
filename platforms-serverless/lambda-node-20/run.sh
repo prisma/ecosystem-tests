f3fe6dcff07d94b7d371003eded73cf80b644d88
Jacek Malec <malec@prisma.io>
2025-11-24 17:34:51 +0000

    fix: misc fixes

--- platforms-serverless/lambda-node-20/run.sh
+++ platforms-serverless/lambda-node-20/run.sh
@@ -13,7 +13,7 @@ pnpm prisma generate
 pnpm tsc
 
 rm -rf lambda.zip
-zip --symlinks -r lambda.zip index.js prisma/schema.prisma node_modules/@prisma/* node_modules/.pnpm/@prisma* node_modules/.pnpm/pg* node_modules/.pnpm/postgres*
+zip --symlinks -r lambda.zip index.js prisma/schema.prisma node_modules/@prisma/* node_modules/.pnpm/{@prisma,pg,postgres,xtend,split2}*
 du -b ./lambda.zip
 
 AWS_RUNTIME=nodejs20.x
