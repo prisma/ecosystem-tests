b43e29bfa37248b584a60e1936786313fc3dfdc5
Jacek Malec <malec@prisma.io>
2026-02-04 19:01:47 +0000

    chore: bump to node 22

--- driver-adapters/neon-lambda-basic/run.sh
+++ driver-adapters/neon-lambda-basic/run.sh
@@ -23,5 +23,5 @@ fi
 cp "$GENERATED_CLIENT/schema.prisma" dist
 zip -rj lambda.zip dist
 
-aws lambda update-function-configuration --function-name driver-adapters-neon-lambda-basic --runtime nodejs20.x --environment "Variables={DRIVER_ADAPTERS_NEON_LAMBDA_BASIC_DATABASE_URL=$DRIVER_ADAPTERS_NEON_LAMBDA_BASIC_DATABASE_URL}" --timeout 30
+aws lambda update-function-configuration --function-name driver-adapters-neon-lambda-basic --runtime nodejs22.x --environment "Variables={DRIVER_ADAPTERS_NEON_LAMBDA_BASIC_DATABASE_URL=$DRIVER_ADAPTERS_NEON_LAMBDA_BASIC_DATABASE_URL}" --timeout 30
 aws lambda update-function-code --function-name driver-adapters-neon-lambda-basic --zip-file "fileb://lambda.zip"
--- driver-adapters/pg-lambda-basic/run.sh
+++ driver-adapters/pg-lambda-basic/run.sh
@@ -23,5 +23,5 @@ fi
 cp "$GENERATED_CLIENT/schema.prisma" dist
 zip -rj lambda.zip dist
 
-aws lambda update-function-configuration --function-name driver-adapters-pg-lambda-basic --runtime nodejs20.x --environment "Variables={DATABASE_URL=$DATABASE_URL}" --timeout 30
+aws lambda update-function-configuration --function-name driver-adapters-pg-lambda-basic --runtime nodejs22.x --environment "Variables={DATABASE_URL=$DATABASE_URL}" --timeout 30
 aws lambda update-function-code --function-name driver-adapters-pg-lambda-basic --zip-file "fileb://lambda.zip"
