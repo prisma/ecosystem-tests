f13ca80c0e0f8e09d9d24e8fb455cbd68c153c5d
jacek-prisma <malec@prisma.io>
2026-03-11 11:14:23 +0000

    chore: run functions:secrets:set with --force to replace old versions (#5995)
    When running `functions:secrets:set` with `--force` the CLI should
    update the affected functions and disable old secret version. This
    should save us some GCP costs, since disabled secrets cost nothing.
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    
    ## Summary by CodeRabbit
    
    * **Chores**
    * Enhanced Firebase deployment script for more reliable secret
    configuration handling.
    
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- platforms-serverless/firebase-functions/run.sh
+++ platforms-serverless/firebase-functions/run.sh
@@ -11,6 +11,6 @@ echo "$FIREBASE_PRIVATE_KEY" > "./privateKey.json"
 cd functions/ && sh prepare_in_project.sh "$func" && cd ..
 
 echo "$DATABASE_URL" > db_credentials.txt
-GOOGLE_APPLICATION_CREDENTIALS="./privateKey.json" pnpm firebase functions:secrets:set PRISMA_DB --data-file db_credentials.txt
+GOOGLE_APPLICATION_CREDENTIALS="./privateKey.json" pnpm firebase functions:secrets:set PRISMA_DB --data-file db_credentials.txt --force
 
 GOOGLE_APPLICATION_CREDENTIALS="./privateKey.json" pnpm firebase deploy --only "functions:firebase-functions:$func"
